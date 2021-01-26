7ea913fb834f71ea04a36759030448ea04d59123
William Fish <william.michael.fish@gmail.com>
2021-01-26 17:23:15 +0000

    Fix more test noise

--- spec/services/geographical_area_memberships_import_service_spec.rb
+++ spec/services/geographical_area_memberships_import_service_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe GeographicalAreaMembershipsImportService do
   subject(:importer) { described_class.new }
 
   let(:hjid_mappings_file) { Rails.root.join('spec', 'fixtures', 'files', 'hjid_member_map.csv').to_s }
-  
-  let!(:area) {
+
+  let!(:area) do
     create(:geographical_area_membership, geographical_area_sid: 256,
-    geographical_area_group_sid: 114,
-    validity_start_date: '2004-05-01T00:00:00'
-    )
-  }
+                                          geographical_area_group_sid: 114,
+                                          validity_start_date: '2004-05-01T00:00:00')
+  end
 
   before do
     create(:geographical_area_membership)
@@ -18,19 +17,19 @@ RSpec.describe GeographicalAreaMembershipsImportService do
 
   describe '#import_hjids' do
     it 'raises error if invalid filename' do
-      expect { importer.import_hjids('foo') }.to raise_exception
+      expect { importer.import_hjids('foo') }.to raise_error(Errno::ENOENT)
     end
 
     it 'does not raise error with valid filename' do
-      expect { importer.import_hjids(hjid_mappings_file) }.not_to raise_exception
+      expect { importer.import_hjids(hjid_mappings_file) }.not_to raise_error
     end
 
     it 'updates matching geographical areas' do
       importer.import_hjids(hjid_mappings_file)
       expect(area.reload).to have_attributes(
-        hjid: 25654,
-        geographical_area_hjid: 23590,
-        geographical_area_group_hjid: 23501,
+        hjid: 25_654,
+        geographical_area_hjid: 23_590,
+        geographical_area_group_hjid: 23_501,
       )
     end
   end
@@ -42,7 +41,7 @@ RSpec.describe GeographicalAreaMembershipsImportService do
       expect(importer.import_hjids_stats).to eq(
         geographical_areas_total: 2,
         geographical_areas_with_hjid_total: 1,
-        errors: 1
+        errors: 1,
       )
     end
   end
