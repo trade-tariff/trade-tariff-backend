357a43fff0071c21ecf9e1367fd81e5d8eeb6e4c
William Fish <william.michael.fish@gmail.com>
2021-03-18 09:31:43 +0000

    Cache geographical areas for 24 hours

--- app/controllers/api/v2/geographical_areas_controller.rb
+++ app/controllers/api/v2/geographical_areas_controller.rb
@@ -1,20 +1,35 @@
 module Api
   module V2
     class GeographicalAreasController < ApiController
-      def index
-        @geographical_areas = GeographicalArea.eager(:geographical_area_descriptions).actual.areas.all
+      TTL = 24.hours
 
-        render json: Api::V2::GeographicalAreaTreeSerializer.new(@geographical_areas, options).serializable_hash
+      def index
+        render json: cached_serialized_geographical_areas
       end
 
       def countries
-        @geographical_areas = GeographicalArea.eager(:geographical_area_descriptions).actual.countries.all
-
-        render json: Api::V2::GeographicalAreaTreeSerializer.new(@geographical_areas, options).serializable_hash
+        render json: cached_serialized_geographical_areas
       end
 
-      def options
-        { include: [:contained_geographical_areas] }
+      private
+
+      def cached_serialized_geographical_areas(countries: false)
+        @serialized = Rails.cache.fetch(cache_key, expires_in: TTL) do
+          geographical_areas = if countries
+                                 GeographicalArea.eager(:geographical_area_descriptions).actual.countries.all
+                               else
+                                 GeographicalArea.eager(:geographical_area_descriptions).actual.areas.all
+                               end
+
+          Api::V2::GeographicalAreaTreeSerializer.new(
+            geographical_areas,
+            include: [:contained_geographical_areas],
+          ).serializable_hash
+        end
+      end
+
+      def cache_key
+        "_geographical_areas-#{action}-#{actual_date}"
       end
     end
   end
