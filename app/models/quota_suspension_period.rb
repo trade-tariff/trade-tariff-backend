343fc354d1d935455f1d1b02fb1c13f3abfab78a
cacquani <cacquani@mail.com>
2021-05-04 12:30:03 +0100

    HOTT-428 - fix Rubocop errors
    Using the ticket as an excuse to fix some styling issues related to the
    files involved in the investigation and development

--- app/models/quota_suspension_period.rb
+++ app/models/quota_suspension_period.rb
@@ -1,9 +1,8 @@
 class QuotaSuspensionPeriod < Sequel::Model
   plugin :time_machine, period_start_column: :suspension_start_date,
-         period_end_column: :suspension_end_date
+                        period_end_column: :suspension_end_date
   plugin :oplog, primary_key: :quota_suspension_period_sid
   plugin :conformance_validator
 
   set_primary_key [:quota_suspension_period_sid]
-
 end
