b8b0fea401a7a7c45a338610cbe638b0c0322499
William Fish <william.michael.fish@gmail.com>
2021-06-07 13:09:11 +0100

    Adds ApplicableVatMeasuresService

---
+++ app/services/applicable_vat_measures_service.rb
@@ -0,0 +1,22 @@
+class ApplicableVatMeasuresService
+  def initialize(measures)
+    @measures = measures
+  end
+
+  def call
+    measures.each_with_object({}) do |measure, acc|
+      vat_key = measure.additional_code_id.present? ? "#{measure.additional_code_type_id}#{measure.additional_code_id}" : 'VAT'
+      vat_duty_amount = measure.measure_components.first&.duty_amount
+      vat_description = measure.additional_code_id.present? ? measure.additional_code.description : measure.measure_type.description
+      vat_description = "#{vat_description} (#{vat_duty_amount})"
+
+      acc[vat_key] = vat_description
+    end
+  end
+
+  private
+
+  def measures
+    @measures.select(&:vat?)
+  end
+end
