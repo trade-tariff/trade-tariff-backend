0adf29f6928ce2ea6d15730c4e97c98c424921b4
William Fish <william.michael.fish@gmail.com>
2021-06-07 15:43:39 +0100

    Fix presenter wrapping additional_code_id

--- app/services/applicable_vat_options_service.rb
+++ app/services/applicable_vat_options_service.rb
@@ -5,7 +5,7 @@ class ApplicableVatOptionsService
 
   def call
     measures.each_with_object({}) do |measure, acc|
-      vat_key = measure.additional_code_id.present? ? "#{measure.additional_code_type_id}#{measure.additional_code_id}" : 'VAT'
+      vat_key = measure.additional_code.present? ? "#{measure.additional_code.additional_code_type_id}#{measure.additional_code.additional_code}" : 'VAT'
       vat_duty_amount = measure.measure_components.first&.duty_amount
       vat_description = measure.additional_code_id.present? ? measure.additional_code.description : measure.measure_type.description
       vat_description = "#{vat_description} (#{vat_duty_amount})"
