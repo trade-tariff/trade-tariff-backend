7cf2ca3c1bef1ec7244c16937931102f51af3a53
William Fish <william.michael.fish@gmail.com>
2021-06-11 12:37:18 +0100

    Fixes failing spec

--- spec/unit/tariff_synchronizer/base_update_spec.rb
+++ spec/unit/tariff_synchronizer/base_update_spec.rb
@@ -23,14 +23,13 @@ describe TariffSynchronizer::BaseUpdate do
 
   describe '.latest_applied_of_both_kinds' do
     it 'Makes the right sql query' do
-      expected_sql = %{SELECT DISTINCT ON ("update_type") "tariff_updates".* FROM "tariff_updates" WHERE ("state" = 'A') ORDER BY "update_type", "issue_date" DESC}
-      result = TariffSynchronizer::BaseUpdate.latest_applied_of_both_kinds.sql
-      expect(result).to eq(expected_sql)
+      expected_sql = %{SELECT DISTINCT ON ("update_type") "tariff_updates".* FROM "tariff_updates" WHERE (("update_type" != 'TariffSynchronizer::ChiefUpdate') AND ("state" = 'A')) ORDER BY "update_type", "issue_date" DESC}
+      expect(described_class.latest_applied_of_both_kinds.sql).to eq(expected_sql)
     end
 
     it 'returns only one record for each update_type' do
       create_list :taric_update, 2, :applied
-      result = TariffSynchronizer::BaseUpdate.latest_applied_of_both_kinds.all
+      result = described_class.latest_applied_of_both_kinds.all
       expect(result.size).to eq(1)
     end
 
@@ -38,7 +37,7 @@ describe TariffSynchronizer::BaseUpdate do
       date = Date.new(2016, 2, 6)
       create :taric_update, :applied, issue_date: date
       create :taric_update, :applied, issue_date: date
-      result = TariffSynchronizer::BaseUpdate.latest_applied_of_both_kinds.all
+      result = described_class.latest_applied_of_both_kinds.all
       expect(result.size).to eq(1)
       expect(result.first.issue_date).to eq(date)
     end
