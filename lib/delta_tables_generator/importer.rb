d5e88504e887db154116a1f35e854f92dcf417da
cacquani <cacquani@mail.com>
2021-06-30 16:27:32 +0100

    HOTT-726: reuse children list

--- lib/delta_tables_generator/importer.rb
+++ lib/delta_tables_generator/importer.rb
@@ -34,12 +34,12 @@ module DeltaTablesGenerator
         'true'
       end
 
-      def integrate_element(row:, day: Date.current, is_end_line: nil)
+      def integrate_element(row:, day: Date.current, is_end_line: nil, siblings: nil)
         [
           row[:goods_nomenclature_item_id],
           row[:goods_nomenclature_sid],
           row[:producline_suffix] || row[:productline_suffix] || '80',
-          is_end_line || end_line?(row: row, day: day),
+          is_end_line || end_line?(row: row, day: day, siblings: siblings),
           delta_type,
           day,
         ]
@@ -49,7 +49,7 @@ module DeltaTablesGenerator
         'commodity'
       end
 
-      def end_line?(row:, day: Date.current)
+      def end_line?(row:, day: Date.current, siblings: [])
         TimeMachine.at(day) do
           item_id = row[:goods_nomenclature_item_id]
           return false if chapter?(item_id) || heading?(item_id)
@@ -57,7 +57,10 @@ module DeltaTablesGenerator
           suffix = row[:producline_suffix] || row[:productline_suffix]
           return false if suffix.present? && suffix != '80'
 
-          find_children(row: row, day: day).none?
+          children = siblings&.any? ?
+                       find_children_from_siblings(row: row, siblings: siblings) :
+                       find_children(row: row, day: day)
+          children.none?
         end
       end
 
@@ -67,7 +70,34 @@ module DeltaTablesGenerator
         is_end_line = suffix.nil? || suffix == '80' ? children.none? : false
 
         [integrate_element(row: row, day: day, is_end_line: is_end_line)]
-          .concat(children.map { |child| integrate_element(row: child, day: day) })
+          .concat(children.map { |child| integrate_element(row: child, day: day, siblings: children) })
+      end
+
+      def find_children_from_siblings(row:, siblings:)
+        item_id = row[:goods_nomenclature_item_id]
+        row_suffix = row[:producline_suffix] || row[:productline_suffix]
+
+        if chapter?(item_id)
+          children = siblings.select do |element|
+            /^#{item_id[0, 2]}/ =~ element[:goods_nomenclature_item_id]
+          end
+        elsif heading?(item_id)
+          children = siblings.select do |element|
+            /^#{item_id[0, 4]}/ =~ element[:goods_nomenclature_item_id]
+          end
+        else
+          goods_nomenclature = GoodsNomenclature.eager(:goods_nomenclature_indents)
+                                                .first(goods_nomenclature_sid: row[:goods_nomenclature_sid])
+          children = siblings.select do |element|
+            suffix = element[:producline_suffix] || element[:productline_suffix]
+            element[:number_indents] > goods_nomenclature.number_indents &&
+              (element[:goods_nomenclature_item_id] > item_id ||
+               (element[:goods_nomenclature_item_id] == item_id &&
+                suffix > row_suffix))
+          end
+        end
+
+        return children
       end
 
       def find_children(row:, day: Date.current)
@@ -78,23 +108,45 @@ module DeltaTablesGenerator
 
           if chapter?(item_id)
             return DB[:goods_nomenclatures]
+              .left_join(:goods_nomenclature_indents,
+                         goods_nomenclatures__goods_nomenclature_sid: :goods_nomenclature_indents__goods_nomenclature_sid)
               .where {
-                (validity_start_date <= day) &
-                ((validity_end_date == nil) | (validity_end_date >= day)) &
-                (like(goods_nomenclature_item_id, chapter_id))
+                (goods_nomenclatures__validity_start_date <= day) &
+                ((goods_nomenclatures__validity_end_date == nil) | (goods_nomenclatures__validity_end_date >= day)) &
+                (like(goods_nomenclatures__goods_nomenclature_item_id, chapter_id))
+              }
+              .distinct(:goods_nomenclatures__goods_nomenclature_sid)
+              .select { |row|
+                [
+                  row.goods_nomenclatures__goods_nomenclature_item_id,
+                  row.goods_nomenclatures__goods_nomenclature_sid,
+                  row.goods_nomenclatures__producline_suffix,
+                  row.number_indents
+                ]
               }
               .all
           elsif heading?(item_id)
             return DB[:goods_nomenclatures]
+              .left_join(:goods_nomenclature_indents,
+                         goods_nomenclatures__goods_nomenclature_sid: :goods_nomenclature_indents__goods_nomenclature_sid)
               .where {
-                (validity_start_date <= day) &
-                ((validity_end_date == nil) | (validity_end_date >= day)) &
-                (like(goods_nomenclature_item_id, heading_id))
+                (goods_nomenclatures__validity_start_date <= day) &
+                ((goods_nomenclatures__validity_end_date == nil) | (goods_nomenclatures__validity_end_date >= day)) &
+                (like(goods_nomenclatures__goods_nomenclature_item_id, heading_id))
+              }
+              .distinct(:goods_nomenclatures__goods_nomenclature_sid)
+              .select { |row|
+                [
+                  row.goods_nomenclatures__goods_nomenclature_item_id,
+                  row.goods_nomenclatures__goods_nomenclature_sid,
+                  row.goods_nomenclatures__producline_suffix,
+                  row.number_indents
+                ]
               }
               .all
           else
-            # or incomincian le dolenti note a farmisi sentire, or son venuta ove molto pianto mi percuote
-            goods_nomenclature = GoodsNomenclature.first(goods_nomenclature_sid: row[:goods_nomenclature_sid])
+            goods_nomenclature = GoodsNomenclature.eager(:goods_nomenclature_indents)
+                                                  .first(goods_nomenclature_sid: row[:goods_nomenclature_sid])
             productline_suffix = goods_nomenclature.producline_suffix
             indent = goods_nomenclature.goods_nomenclature_indent&.number_indents
 
@@ -116,9 +168,11 @@ module DeltaTablesGenerator
                 [
                   row.goods_nomenclatures__goods_nomenclature_item_id,
                   row.goods_nomenclatures__goods_nomenclature_sid,
-                  row.goods_nomenclatures__producline_suffix
+                  row.goods_nomenclatures__producline_suffix,
+                  row.number_indents
                 ]
               }
+              .all
           end
         end
       end
