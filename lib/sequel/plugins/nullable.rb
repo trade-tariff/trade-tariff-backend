12848031953ff3dfa6819559fc247a06d6d4c8c0
William Fish <william.michael.fish@gmail.com>
2021-08-17 21:28:33 +0100

    Remove require failure handling

--- lib/sequel/plugins/nullable.rb
+++ lib/sequel/plugins/nullable.rb
@@ -3,11 +3,7 @@ module Sequel
     module Nullable
       module DatasetMethods
         def first_or_null(*args)
-          null_model = "Null#{model}"
-
-          require null_model.underscore
-
-          first(*args) || null_model.constantize.new
+          first(*args) || "Null#{model}".constantize.new
         end
       end
     end
