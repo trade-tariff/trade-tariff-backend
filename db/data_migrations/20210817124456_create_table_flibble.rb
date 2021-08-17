65450450cd937cb5c9ec7b59b353083cef283abd
William Fish <william.michael.fish@gmail.com>
2021-08-17 14:14:24 +0100

    Adds migration for measure with missing validity end date

---
+++ db/data_migrations/20210817124456_create_table_flibble.rb
@@ -0,0 +1,21 @@
+TradeTariffBackend::DataMigrator.migration do
+  name 'Fixes missing validity_end_date for xi measure 3753097'
+
+  up do
+    applicable { false }
+
+    apply do
+      if TradeTariffBackend.xi?
+        end_date = Time.zone.parse( '2020-02-26')
+        measure = Measure.where(measure_sid:  3753097).last
+
+        return if measure.validity_end_date == end_date
+
+        measure.update(validity_end_date: end_date)
+        measure.save
+      end
+    end
+  end
+
+  down { }
+end
