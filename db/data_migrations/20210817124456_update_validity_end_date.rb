96677e88d4db2a6a3bc9628efe043f21c82dcd06
William Fish <william.michael.fish@gmail.com>
2021-08-17 14:18:19 +0100

    Lint

--- db/data_migrations/20210817124456_update_validity_end_date.rb
+++ db/data_migrations/20210817124456_update_validity_end_date.rb
@@ -6,8 +6,8 @@ TradeTariffBackend::DataMigrator.migration do
 
     apply do
       if TradeTariffBackend.xi?
-        end_date = Time.zone.parse( '2020-02-26')
-        measure = Measure.where(measure_sid:  3753097).last
+        end_date = Time.zone.parse('2020-02-26')
+        measure = Measure.where(measure_sid: 3_753_097).last
 
         return if measure.validity_end_date == end_date
 
@@ -17,5 +17,5 @@ TradeTariffBackend::DataMigrator.migration do
     end
   end
 
-  down { }
+  down {}
 end
