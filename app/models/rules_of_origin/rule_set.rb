144ab8f0a64f5b145a6b069702fc672a96f58d27
jebw <jeb@jdwilkins.co.uk>
2021-08-26 12:42:21 +0100

    HOTT-868: Added RuleSet model for importing rules

---
+++ app/models/rules_of_origin/rule_set.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module RulesOfOrigin
+  class RuleSet
+    include ActiveModel::Model
+
+    def initialize(source_file)
+      @rules = nil
+
+      @source_file = Pathname.new(source_file)
+      unless @source_file.extname == '.csv' && @source_file.file? && @source_file.exist?
+        raise InvalidRulesFile, 'Requires a path to a CSV file'
+      end
+    end
+
+    def import
+      raise AlreadyImported if @rules
+
+      @rules = {}
+
+      CSV.foreach(@source_file, headers: true) do |row|
+        next unless row['scope'] == TradeTariffBackend.service
+
+        @rules[row['id_rule']] = row.to_h.without('scope', 'id_rule')
+      end
+
+      @rules.length
+    end
+
+    def rule(id_rule)
+      rule = @rules[id_rule]
+
+      Rule.new rule.merge(id_rule: id_rule) if rule
+    end
+
+    class InvalidRulesFile < RuntimeError; end
+
+    class AlreadyImported < RuntimeError; end
+  end
+end
---
+++ spec/models/rules_of_origin/rule_set_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe RulesOfOrigin::RuleSet do
+  subject(:rule_set) { described_class.new test_file }
+
+  let(:test_file) { file_fixture 'rules_of_origin/rules_of_origin_210728.csv' }
+  let(:imported_rules) { rule_set.tap(&:import) }
+
+  describe '.new' do
+    context 'with valid file' do
+      it { is_expected.to be_instance_of described_class }
+    end
+
+    context 'with missing file' do
+      let(:test_file) do
+        Rails.root.join(file_fixture_path).join 'rules_of_origin/random.json'
+      end
+
+      it { expect { rule_set }.to raise_exception described_class::InvalidRulesFile }
+    end
+
+    context 'with non-CSV file' do
+      let(:test_file) { file_fixture 'rules_of_origin/invalid.json' }
+
+      it { expect { rule_set }.to raise_exception described_class::InvalidRulesFile }
+    end
+  end
+
+  describe '#import' do
+    subject { rule_set.import }
+
+    it('will return the number of imported rows') { is_expected.to be 55 }
+
+    context 'with rows for different scopes' do
+      before { allow(TradeTariffBackend).to receive(:service).and_return('xi') }
+
+      it('will skip those rows') { is_expected.to be 0 }
+    end
+
+    context 'when already imported' do
+      it 'will raise an exception' do
+        expect { imported_rules.import }.to \
+          raise_exception described_class::AlreadyImported
+      end
+    end
+  end
+
+  describe '#rule' do
+    subject { imported_rules.rule(id_rule) }
+
+    context 'with known rule' do
+      let(:id_rule) { '20000001' }
+
+      it { is_expected.to be_instance_of RulesOfOrigin::Rule }
+      it { is_expected.to have_attributes id_rule: id_rule }
+    end
+
+    context 'with unknown rule' do
+      let(:id_rule) { '1' }
+
+      it { is_expected.to be_nil }
+    end
+  end
+end
