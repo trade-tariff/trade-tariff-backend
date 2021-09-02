d2f168a73b813efa774164edcd763ecd62cfa9f1
jebw <jeb@jdwilkins.co.uk>
2021-09-02 15:43:43 +0100

    HOTT-868: Added RulesOfOrigin::DataSet model

--- config/initializers/rules_of_origin.rb
+++ config/initializers/rules_of_origin.rb
@@ -1,8 +1,6 @@
 if TradeTariffBackend.rules_of_origin_enabled? && !Rails.env.test?
   Rails.application.reloader.to_prepare do
     # trigger loading at boot
-    TradeTariffBackend.rules_of_origin_schemes
-    TradeTariffBackend.rules_of_origin_rules
-    TradeTariffBackend.rules_of_origin_mappings
+    TradeTariffBackend.rules_of_origin
   end
 end
---
+++ spec/models/rules_of_origin/data_set_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe RulesOfOrigin::DataSet do
+  subject { described_class.new scheme_set, rule_set, mappings }
+
+  let(:rules) do
+    build_list :rules_of_origin_rule, 2, scheme_code: scheme_set.schemes.first
+  end
+
+  let(:scheme_set) { build :rules_of_origin_scheme_set }
+  let(:rule_set) { build :rules_of_origin_rule_set, rules: rules }
+  let(:mappings) { build :rules_of_origin_heading_mappings, rule: rules.first }
+
+  it { is_expected.to respond_to :scheme_set }
+  it { is_expected.to respond_to :rule_set }
+  it { is_expected.to respond_to :heading_mappings }
+
+  it { is_expected.to have_attributes scheme_set: scheme_set }
+  it { is_expected.to have_attributes rule_set: rule_set }
+  it { is_expected.to have_attributes heading_mappings: mappings }
+end
