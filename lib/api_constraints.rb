7b612de3b5e4a5dbc4501d61fbb21d834df75cd9
William Fish <william.michael.fish@gmail.com>
2021-09-22 15:31:31 +0100

    Default to the environment default

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -4,24 +4,16 @@ class ApiConstraints
   end
 
   def matches?(req)
-    is_default? || req.headers['Accept'].include?("application/vnd.uktt.v#{@version}")
+    default? || req.headers['Accept'].include?("application/vnd.uktt.v#{@version}")
   end
 
   private
 
-  def is_default?
-    if Rails.env.production?
-      production_default?
-    else
-      development_default?
-    end
+  def default?
+    @version == default
   end
 
-  def development_default?
-    @version == 2
-  end
-
-  def production_default?
-    @version == 1
+  def default
+    ENV.fetch('DEFAULT_VERSION', 1)
   end
 end
