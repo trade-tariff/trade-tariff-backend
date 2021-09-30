d7f1f5e9f3d7a3a4336272fc9b78c5bd41ba1a2b
jebw <jeb@jdwilkins.co.uk>
2021-09-30 11:19:36 +0100

    HOTT-1018: Extend Proof model to read urls

--- app/models/rules_of_origin/proof.rb
+++ app/models/rules_of_origin/proof.rb
@@ -3,5 +3,15 @@ module RulesOfOrigin
     include ActiveModel::Model
 
     attr_accessor :scheme, :summary, :detail, :proof_class, :subtext
+
+    def url
+      all_proof_urls[proof_class] if proof_class.present?
+    end
+
+  private
+
+    def all_proof_urls
+      scheme&.scheme_set&.proof_urls || {}
+    end
   end
 end
--- spec/models/rules_of_origin/proof_spec.rb
+++ spec/models/rules_of_origin/proof_spec.rb
@@ -23,4 +23,48 @@ RSpec.describe RulesOfOrigin::Proof do
     it { is_expected.to have_attributes proof_class: 'origin-declaration' }
     it { is_expected.to have_attributes subtext: 'subtext' }
   end
+
+  describe '#url' do
+    subject do
+      build(:rules_of_origin_proof, proof_class: proof_class, scheme: scheme).url
+    end
+
+    let(:scheme) { build :rules_of_origin_scheme, scheme_set: scheme_set }
+    let(:proof_urls) { { 'origin-declaration' => 'https://www.gov.uk/' } }
+    let(:proof_class) { 'origin-declaration' }
+
+    let(:scheme_set) do
+      instance_double RulesOfOrigin::SchemeSet, proof_urls: proof_urls
+    end
+
+    context 'without proof_class' do
+      let(:proof_class) { nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'with proof_class' do
+      context 'with scheme and scheme_set' do
+        it { is_expected.to eql 'https://www.gov.uk/' }
+      end
+
+      context 'without scheme_set' do
+        let(:scheme) { build(:rules_of_origin_scheme) }
+
+        it { is_expected.to be_nil }
+      end
+
+      context 'without scheme' do
+        let(:scheme) { nil }
+
+        it { is_expected.to be_nil }
+      end
+    end
+
+    context 'with unknown proof_class' do
+      let(:proof_class) { 'unknown' }
+
+      it { is_expected.to be_nil }
+    end
+  end
 end
