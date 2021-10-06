ac7c9bb0821b80495273cefbeb7e7197ca43da98
William Fish <william.michael.fish@gmail.com>
2021-10-06 15:52:56 +0100

    Return the most specific geographical_area meursing measure first (#252)
    * Return the most specific geographical_area meursing measure first
    
    * Fix spec

--- app/services/meursing_measure_component_resolver_service.rb
+++ app/services/meursing_measure_component_resolver_service.rb
@@ -20,14 +20,21 @@ class MeursingMeasureComponentResolverService
   attr_reader :root_measure, :meursing_measures
 
   def meursing_component_for(root_measure_component)
-    measure = meursing_measures.find do |meursing_measure|
+    applicable_measures = meursing_measures.select do |meursing_measure|
       meursing_measure.measure_type_id == root_measure_component.duty_expression.meursing_measure_type_id
     end
 
-    return nil unless measure
+    # Occasionally we get multiple meursing measures that apply to multiple geographical areas based on the contained geographical area logic. We should aim to retrieve the most specific answer if possible
+    most_specific_measure = applicable_measures.find do |meursing_measure|
+      meursing_measure.geographical_area_id == root_measure.geographical_area_id
+    end
+
+    applicable_measure = most_specific_measure.presence || applicable_measures.first
+
+    return nil unless applicable_measure
 
     # Meursing measures only ever have a single component
-    component = measure.measure_components.first
+    component = applicable_measure.measure_components.first
 
     Api::V2::Measures::MeursingMeasureComponentPresenter.new(component)
   end
