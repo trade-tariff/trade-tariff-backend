56168f15ac60b7c916a91dae44bfde5b446d301f
William Fish <william.michael.fish@gmail.com>
2021-10-06 15:21:41 +0100

    Return the most specific geographical_area meursing measure first

--- spec/services/meursing_measure_component_resolver_service_spec.rb
+++ spec/services/meursing_measure_component_resolver_service_spec.rb
@@ -1,22 +1,39 @@
 RSpec.describe MeursingMeasureComponentResolverService do
   subject(:service) do
     described_class.new(
-      root_measure.reload,
-      [meursing_agricultural_measure],
+      root_measure,
+      [meursing_agricultural_measure, more_specific_agricultural_measure],
     )
   end
 
   describe '#call' do
-    before do
-      root_measure_components
-      meursing_agricultural_measure_component
-      meursing_agricultural_measure.reload
+    let(:root_measure) do
+      measure = create(
+        :measure,
+        :third_country,
+        geographical_area_id: 'RO',
+      )
+
+      # Ad valorem measure component
+      create(
+        :measure_component,
+        :with_duty_expression,
+        measure_sid: root_measure.measure_sid,
+        duty_expression_id: '01',
+      )
+      # Placeholder meursing measure component - agricultural component
+      create(
+        :measure_component,
+        :with_duty_expression,
+        measure_sid: root_measure.measure_sid,
+        duty_expression_id: '12',
+      )
+
+      measure.reload
     end
 
-    let(:root_measure) { create(:measure, :third_country) }
-
-    let(:meursing_sugar_measure) { create(:measure, :sugar) }
-    let(:meursing_agricultural_measure) { create(:measure, :agricultural) }
+    let(:meursing_agricultural_measure) { create(:measure, :agricultural, geographical_area_id: '1011') }
+    let(:more_specific_agricultural_measure) { create(:measure, :agricultural, geographical_area_id: 'RO') }
 
     let(:meursing_agricultural_measure_component) do
       create(
@@ -28,27 +45,20 @@ RSpec.describe MeursingMeasureComponentResolverService do
       )
     end
 
-    let(:root_measure_components) do
-      [
-        create(
-          :measure_component,
-          :with_duty_expression,
-          measure_sid: root_measure.measure_sid,
-          duty_expression_id: '01',
-        ), # Ad valorem measure component
-        create(
-          :measure_component,
-          :with_duty_expression,
-          measure_sid: root_measure.measure_sid,
-          duty_expression_id: '12',
-        ), # Placeholder meursing measure component - agricultural component
-      ]
+    let(:more_specific_meursing_agricultural_measure_component) do
+      create(
+        :measure_component,
+        duty_amount: 0.0,
+        monetary_unit_code: 'EUR',
+        measurement_unit_code: 'DTN',
+        measure_sid: more_specific_agricultural_measure.measure_sid,
+      )
     end
 
-    it 'returns a correct set of resolved placeholder measure components' do
+    it 'picks the most specific match' do
       expected_measure_components = [
-        root_measure_components[0],              # Ad valorem
-        meursing_agricultural_measure_component, # Agricultural
+        root_measure.measure_components.first,
+        more_specific_meursing_agricultural_measure_component,
       ].map(&:pk)
 
       actual_measure_components = service.call.map(&:pk)
