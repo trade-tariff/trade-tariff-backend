afe7f4d1b782508b676ce86298967da454263fcb
William Fish <william.michael.fish@gmail.com>
2021-10-25 11:11:48 +0100

    Return false for EPS when on UK service

--- app/presenters/api/v2/commodities/commodity_presenter.rb
+++ app/presenters/api/v2/commodities/commodity_presenter.rb
@@ -63,6 +63,8 @@ module Api
         end
 
         def entry_price_system?
+          return false if TradeTariffBackend.uk?
+
           import_measures.any?(&:entry_price_system?)
         end
 
--- spec/presenters/api/v2/commodities/commodity_presenter_spec.rb
+++ spec/presenters/api/v2/commodities/commodity_presenter_spec.rb
@@ -71,4 +71,35 @@ RSpec.describe Api::V2::Commodities::CommodityPresenter do
       it { is_expected.not_to be_zero_mfn_duty }
     end
   end
+
+  describe '#entry_price_system?' do
+    before do
+      allow(TradeTariffBackend).to receive(:service).and_return(service)
+    end
+
+    let(:entry_price_measure) { create(:measure, :with_measure_conditions, :with_entry_price_system) }
+    let(:non_entry_price_measure) { create(:measure, :with_measure_conditions, :without_entry_price_system) }
+
+    context 'when on the uk service' do
+      let(:service) { 'uk' }
+
+      let(:measures) { [entry_price_measure, non_entry_price_measure] }
+
+      it { is_expected.not_to be_entry_price_system }
+    end
+
+    context 'when one of the measures uses the entry price system and on the xi service' do
+      let(:service) { 'xi' }
+      let(:measures) { [entry_price_measure, non_entry_price_measure] }
+
+      it { is_expected.to be_entry_price_system }
+    end
+
+    context 'when none of the measures uses the entry price system and on the xi service' do
+      let(:service) { 'xi' }
+      let(:measures) { [non_entry_price_measure] }
+
+      it { is_expected.not_to be_entry_price_system }
+    end
+  end
 end
