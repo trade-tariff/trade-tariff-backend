5e0552d86da0142a4c77f43bdccb3f1f862c4bfb
William Fish <william.michael.fish@gmail.com>
2021-11-01 09:41:04 +0000

    Lint measure serializer

--- spec/serializers/api/v2/measures/measure_serializer_spec.rb
+++ spec/serializers/api/v2/measures/measure_serializer_spec.rb
@@ -7,49 +7,49 @@ RSpec.describe Api::V2::Measures::MeasureSerializer do
 
   let(:expected_pattern) do
     {
-      'data' => {
-        'id' => measure.measure_sid.to_s,
-        'type' => 'measure',
-        'attributes' => {
-          'id' => measure.measure_sid,
-          'origin' => 'eu',
-          'effective_start_date' => nil,
-          'effective_end_date' => nil,
-          'import' => true,
-          'excise' => false,
-          'vat' => false,
-          'reduction_indicator' => 1,
+      data: {
+        id: measure.measure_sid.to_s,
+        type: 'measure',
+        attributes: {
+          id: measure.measure_sid,
+          origin: 'eu',
+          effective_start_date: nil,
+          effective_end_date: nil,
+          import: true,
+          excise: false,
+          vat: false,
+          reduction_indicator: 1,
         },
-        'relationships' => {
-          'duty_expression' => {
-            'data' => {
-              'id' => "#{measure.id}-duty_expression",
-              'type' => 'duty_expression',
+        relationships: {
+          duty_expression: {
+            data: {
+              id: "#{measure.id}-duty_expression",
+              type: 'duty_expression',
             },
           },
-          'measure_type' => {
-            'data' => {
-              'id' => measure.measure_type_id,
-              'type' => 'measure_type',
+          measure_type: {
+            data: {
+              id: measure.measure_type_id,
+              type: 'measure_type',
             },
           },
-          'legal_acts' => { 'data' => [] },
-          'measure_conditions' => { 'data' => [] },
-          'measure_components' => { 'data' => [] },
-          'national_measurement_units' => { 'data' => [] },
-          'geographical_area' => {
-            'data' => {
-              'id' => measure.geographical_area_id,
-              'type' => 'geographical_area',
+          legal_acts: { data: [] },
+          measure_conditions: { data: [] },
+          measure_components: { data: [] },
+          national_measurement_units: { data: [] },
+          geographical_area: {
+            data: {
+              id: measure.geographical_area_id,
+              type: 'geographical_area',
             },
           },
-          'excluded_countries' => { 'data' => [] },
-          'footnotes' => { 'data' => [] },
-          'order_number' => { 'data' => nil },
+          excluded_countries: { data: [] },
+          footnotes: { data: [] },
+          order_number: { data: nil },
         },
-        'meta' => {
-          'duty_calculator' => {
-            'source' => 'uk',
+        meta: {
+          duty_calculator: {
+            source: 'uk',
           },
         },
       },
@@ -60,93 +60,54 @@ RSpec.describe Api::V2::Measures::MeasureSerializer do
     it { is_expected.to include_json(expected_pattern) }
 
     context 'when passing a meursing additional code' do
-      let(:measure) { create(:measure, :third_country, reduction_indicator: 1) }
-
-      let(:root_measure_components) do
-        [
-          create(
-            :measure_component,
-            :with_duty_expression,
-            measure_sid: measure.measure_sid,
-            duty_expression_id: '01',
-          ), # Ad valorem measure component
-          create(
-            :measure_component,
-            :with_duty_expression,
-            measure_sid: measure.measure_sid,
-            duty_expression_id: '12',
-          ), # Placeholder meursing measure component - agricultural component
-        ]
-      end
-      let(:options) { { params: { meursing_additional_code_id: '000' }, include: ['measure_components'] } }
-      let(:finder_service) do
-        instance_double(
-          'MeursingMeasureFinderService',
-          call: [build(:measure_component)],
-        )
-      end
-      let(:resolver_service) do
-        instance_double(
-          'MeursingMeasureComponentResolverService',
-          call: [],
-        )
-      end
+      let(:options) { { params: { meursing_additional_code_id: '000' }, include: %w[measure_components] } }
       let(:meursing_agricultural_measure) { create(:measure, :agricultural) }
-      let(:meursing_agricultural_measure_component) do
-        create(
-          :measure_component,
-          duty_amount: 0.0,
-          monetary_unit_code: 'EUR',
-          measurement_unit_code: 'DTN',
-          measure_sid: meursing_agricultural_measure.measure_sid,
-        )
-      end
 
       let(:expected_pattern) do
         {
-          'data' => {
-            'id' => measure.measure_sid.to_s,
-            'type' => 'measure',
-            'attributes' => {
-              'id' => measure.measure_sid,
-              'origin' => 'eu',
-              'effective_start_date' => nil,
-              'effective_end_date' => nil,
-              'import' => true,
-              'excise' => false,
-              'vat' => false,
-              'reduction_indicator' => 1,
+          data: {
+            id: measure.measure_sid.to_s,
+            type: 'measure',
+            attributes: {
+              id: measure.measure_sid,
+              origin: 'eu',
+              effective_start_date: nil,
+              effective_end_date: nil,
+              import: true,
+              excise: false,
+              vat: false,
+              reduction_indicator: 1,
             },
-            'relationships' => {
-              'duty_expression' => {
-                'data' => {
-                  'id' => "#{measure.id}-duty_expression",
-                  'type' => 'duty_expression',
+            relationships: {
+              duty_expression: {
+                data: {
+                  id: "#{measure.id}-duty_expression",
+                  type: 'duty_expression',
                 },
               },
-              'measure_type' => {
-                'data' => {
-                  'id' => measure.measure_type_id,
-                  'type' => 'measure_type',
+              measure_type: {
+                data: {
+                  id: measure.measure_type_id,
+                  type: 'measure_type',
                 },
               },
-              'legal_acts' => { 'data' => [] },
-              'measure_conditions' => { 'data' => [] },
-              'measure_components' => { 'data' => [] },
-              'national_measurement_units' => { 'data' => [] },
-              'geographical_area' => {
-                'data' => {
-                  'id' => measure.geographical_area_id,
-                  'type' => 'geographical_area',
+              legal_acts: { data: [] },
+              measure_conditions: { data: [] },
+              measure_components: { data: [] },
+              national_measurement_units: { data: [] },
+              geographical_area: {
+                data: {
+                  id: measure.geographical_area_id,
+                  type: 'geographical_area',
                 },
               },
-              'excluded_countries' => { 'data' => [] },
-              'footnotes' => { 'data' => [] },
-              'order_number' => { 'data' => nil },
+              excluded_countries: { data: [] },
+              footnotes: { data: [] },
+              order_number: { data: nil },
             },
-            'meta' => {
-              'duty_calculator' => {
-                'source' => 'uk',
+            meta: {
+              duty_calculator: {
+                source: 'uk',
               },
             },
           },
@@ -154,8 +115,38 @@ RSpec.describe Api::V2::Measures::MeasureSerializer do
       end
 
       before do
-        root_measure_components
-        meursing_agricultural_measure_component
+        # Ad valorem measure component
+        create(
+          :measure_component,
+          :with_duty_expression,
+          measure_sid: measure.measure_sid,
+          duty_expression_id: '01',
+        )
+        # Placeholder meursing measure component - agricultural component
+        create(
+          :measure_component,
+          :with_duty_expression,
+          measure_sid: measure.measure_sid,
+          duty_expression_id: '12',
+        )
+
+        create(
+          :measure_component,
+          duty_amount: 0.0,
+          monetary_unit_code: 'EUR',
+          measurement_unit_code: 'DTN',
+          measure_sid: meursing_agricultural_measure.measure_sid,
+        )
+
+        finder_service = instance_double(
+          'MeursingMeasureFinderService',
+          call: [build(:measure_component)],
+        )
+        resolver_service = instance_double(
+          'MeursingMeasureComponentResolverService',
+          call: [],
+        )
+
         allow(MeursingMeasureFinderService).to receive(:new).and_return(finder_service)
         allow(MeursingMeasureComponentResolverService).to receive(:new).and_return(resolver_service)
       end
