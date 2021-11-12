711611fd78469d796e8f6a4a74b5c7702efd04cb
William Fish <william.michael.fish@gmail.com>
2021-11-12 14:06:30 +0000

    Stop using rake for rails tasks

--- config/initializers/rules_of_origin.rb
+++ config/initializers/rules_of_origin.rb
@@ -1,6 +1,7 @@
 unless Rails.env.test?
   Rails.application.reloader.to_prepare do
     # trigger loading at boot
+    require_relative '../../app/lib/trade_tariff_backend'
     TradeTariffBackend.rules_of_origin
   end
 end
