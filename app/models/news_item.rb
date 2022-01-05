70df5fd58ae2e3dda93d09ddb51f01610fb27dfe
William Fish <william.michael.fish@gmail.com>
2022-01-05 11:38:54 +0000

    Use active_model sequel plugin in news_items sequel model
    We want to be consistent with the approach we're generally taking in
    this application rather than special snowflaking this specific model

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,9 +1,7 @@
 class NewsItem < Sequel::Model
   plugin :timestamps
   plugin :auto_validations
-
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
+  plugin :active_model
 
   DISPLAY_REGULAR = 0
 
@@ -45,9 +43,5 @@ class NewsItem < Sequel::Model
 
     validates_presence :title if title
     validates_presence :content if content
-  end
-
-  def persisted?
-    true
   end
 end
