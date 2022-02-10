236f6245a550973f7f8b6c021272d3eac95b69b1
Alexdesi <alexdesi@gmail.com>
2022-02-10 09:36:57 +0000

    HOTT-1297 Add support for ?as_of param, and fix eagerloading on measure_type_series_description.

--- spec/controllers/api/v2/measure_types_controller_spec.rb
+++ spec/controllers/api/v2/measure_types_controller_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe Api::V2::MeasureTypesController, type: :controller do
   describe '#index' do
     before do
       create(:measure_type, :with_measure_type_series_description)
+
+      allow(TimeMachine).to receive(:at).and_call_original
     end
 
     it 'returns success' do
@@ -16,6 +18,12 @@ RSpec.describe Api::V2::MeasureTypesController, type: :controller do
       data = JSON.parse(response.body)['data']
 
       expect(data.length).to eq 1
+    end
+
+    it 'the TimeMachine receives the correct Date' do
+      get :index, format: :json
+
+      expect(TimeMachine).to have_received(:at).with(Time.zone.today)
     end
   end
 end
