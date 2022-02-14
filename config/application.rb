e5bb8adede436d5f03badcc0a0c9f0a469eada5a
William Fish <william.michael.fish@gmail.com>
2022-02-14 16:12:50 +0000

    HOTT-1295: Default to 6.1 config prior to 7.0

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 
 module TradeTariffBackend
   class Application < Rails::Application
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.generators do |g|
       g.view_specs     false
