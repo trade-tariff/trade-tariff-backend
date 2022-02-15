f5d4700ce8ef564e7cf13a985f99592fc5f5d428
chriscoffee <7100370+chriscoffee@users.noreply.github.com>
2022-02-15 14:13:44 +0000

    [HOTT-1312] Elasticsearch migration
    * Autocorrect rubocop issues
    * Change elasticsearch to opensearch within paas_config
    * Update docker-compose
    * Disable rubocop for 3.1 hash literal checks
    
    Signed-off-by: Chris Mills <7100370+chriscoffee@users.noreply.github.com>

--- app/lib/paas_config.rb
+++ app/lib/paas_config.rb
@@ -6,28 +6,28 @@ module PaasConfig
       # TODO: !Important
       # need to fetch by service name if we use multiple redis services
       JSON.parse(ENV['VCAP_SERVICES'])['redis'][0]['credentials']['uri']
-    rescue
+    rescue StandardError
       ENV['REDIS_URL']
     end
 
-    { url: url, db: 0, id: nil }
+    { url: url, db: 0, id: nil } # rubocop:disable Style/HashSyntax
   end
 
   def elasticsearch
     url = begin
       # TODO: !Important
       # need to fetch by service name if we use multiple elasticsearch services
-      JSON.parse(ENV['VCAP_SERVICES'])['elasticsearch'][0]['credentials']['uri']
-    rescue
+      JSON.parse(ENV['VCAP_SERVICES'])['opensearch'][0]['credentials']['uri']
+    rescue StandardError
       ENV['ELASTICSEARCH_URL']
     end
 
-    { url: url }
+    { url: url } # rubocop:disable Style/HashSyntax
   end
 
   def space
     JSON.parse(ENV['VCAP_APPLICATION'])['space_name']
-  rescue
+  rescue StandardError
     Rails.env
   end
 end
