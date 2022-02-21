5b2497c29020cd0a26962d83ba6c62b5dea153b3
William Fish <william.michael.fish@gmail.com>
2022-02-21 15:02:35 +0000

    HOTT-1298: Adds caching of the retrieved quota order numbers

--- app/controllers/api/v2/quota_order_numbers_controller.rb
+++ app/controllers/api/v2/quota_order_numbers_controller.rb
@@ -1,11 +1,26 @@
 module Api
   module V2
     class QuotaOrderNumbersController < ApiController
+      TTL = 1.day
+
       def index
-        render json: Api::V2::QuotaOrderNumberSerializer.new(quota_order_numbers, include: [:quota_definition]).serializable_hash
+        render json: serialized_quota_order_numbers
       end
 
       private
+
+      def serialized_quota_order_numbers
+        Rails.cache.fetch(cache_key, expires_in: TTL) do
+          Api::V2::QuotaOrderNumberSerializer.new(
+            quota_order_numbers,
+            include: [:quota_definition],
+          ).serializable_hash
+        end
+      end
+
+      def cache_key
+        "_quota-order-numbers-#{actual_date.iso8601}"
+      end
 
       def quota_order_numbers
         QuotaOrderNumber.with_quota_definitions
