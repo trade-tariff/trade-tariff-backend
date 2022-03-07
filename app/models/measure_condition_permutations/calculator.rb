0ce6c53a04a2effc69f66943831fad7b5b88ae15
jebw <jeb@jdwilkins.co.uk>
2022-03-07 14:31:52 +0000

    HOTT-1361 Refactor of Calculator class
    Hopefully this should now be readable

--- app/models/measure_condition_permutations/calculator.rb
+++ app/models/measure_condition_permutations/calculator.rb
@@ -2,14 +2,12 @@ module MeasureConditionPermutations
   class Calculator
     # There are 2 rules for calculating permutations
     #
-    # When there are conditions with the same permutation_key, but with different
-    # condition codes, then a single permutation group is provided with all
-    # permutations calculated
+    # When there are conditions with matching permutation_keys, then a single
+    # permutation group is provided with all permutations
     #
-    # If there are no conditions with the same permutation key but different
-    # condition codes, then each code gets its own group, with each group
-    # having a separate permutation per condition, with a reference to just that
-    # measure condition
+    # If there are no conditions with matching permutation key, then generate
+    # one group per condition code, with a separate permutation per condition
+    # within it
 
     delegate :measure_conditions, to: :@measure
 
@@ -18,76 +16,81 @@ module MeasureConditionPermutations
     end
 
     def permutation_groups
-      if any_matched_measure_conditions?
-        matched_conditions_in_single_permutation_group
+      if matched_measure_conditions_in_permutation_key_groups.any?
+        groups_when_there_are_matched_conditions
       else
-        unmatched_conditions_in_multiple_permutation_groups
+        groups_when_no_matched_conditions
       end
     end
 
   private
 
-    def group_for_code(condition_code, permutations)
-      Group.new(@measure.measure_sid, condition_code, permutations)
+    def measure_conditions_by_permutation_key
+      @measure_conditions_by_permutation_key =
+        measure_conditions
+          .select(&:permutation_key) # ignore those we can't calculate key for
+          .group_by(&:permutation_key)
     end
 
-    def conditions_grouped_by_code
-      measure_conditions.group_by(&:condition_code)
-    end
-
-    def unmatched_conditions_in_multiple_permutation_groups
-      conditions_grouped_by_code.map do |condition_code, conditions|
-        permutations = conditions.map(&Permutation.method(:new))
-
-        group_for_code condition_code, permutations
-      end
-    end
-
-    def any_matched_measure_conditions?
-      matched_measure_conditions.values.any?(&:many?)
-    end
-
-    def matched_measure_conditions
-      @matched_measure_conditions =
-        measure_conditions.select(&:permutation_key).group_by(&:permutation_key)
-    end
-
-    def matched_conditions_in_single_permutation_group
+    def groups_when_there_are_matched_conditions
       [
         Group.new(@measure.measure_sid,
                   'n/a',
-                  repeated_condition_permutations + unrepeated_condition_permutations),
+                  permutations_for_matched_conditions + permutations_for_unmatched_conditions),
       ]
     end
 
-    def repeated_condition_permutations
-      matched_measure_conditions.values # grab the conditions themselves
-                                .select(&:many?) # fetch conditions with repeated permutation keys
-                                .map(&:first) # grab just the first for any key since they're the same
-                                .map(&Permutation.method(:new)) # Create permutation for each one
+    def matched_measure_conditions_in_permutation_key_groups
+      measure_conditions_by_permutation_key.values.select(&:many?)
     end
 
-    def unrepeated_condition_permutations
-      # FIXME: Needs refactor
+    def unmatched_measure_conditions
+      measure_conditions_by_permutation_key
+        .values
+        .reject(&:many?) # reject any with more than one condition matching a key
+        .flatten # any remaining only have 1 condition in each group
+    end
 
-      unrepeated = matched_measure_conditions.values # grab the conditions themselves
-                                             .reject(&:many?) # fetch conditions without repeated permutation keys
-                                             .map(&:first)
-                                             .group_by(&:condition_code)
+    def permutations_for_matched_conditions
+      matched_measure_conditions_in_permutation_key_groups
+        .map(&:first) # conditions match, so only include the first
+        .map(&Permutation.method(:new)) # Create permutation for each condition
+    end
 
-      reversed_condition_groups = unrepeated.sort_by(&:first).reverse.map(&:last)
+    def permutations_for_unmatched_conditions
+      groups_of_conditions_by_code
+        .inject([], &method(:combine_conditions_in_group_with_existing_combinations)) # Compute all combinations
+        .map(&Permutation.method(:new))
+    end
 
-      reversed_condition_groups.inject([]) do |combinations, conditions|
-        if combinations.any?
-          conditions.flat_map do |condition|
-            combinations.map do |combination|
-              [condition] + combination
-            end
-          end
-        else
-          conditions.to_a.map(&Array.method(:wrap))
+    def groups_of_conditions_by_code
+      unmatched_measure_conditions
+        .group_by(&:condition_code)
+        .sort_by(&:first) # sort on condition code, converts from Hash to Array
+        .map(&:last)
+    end
+
+    def combine_conditions_in_group_with_existing_combinations(combinations, conditions_for_code)
+      # first iteration will have no existin combinations to build upon
+      return conditions_for_code.map(&Array.method(:wrap)) if combinations.none?
+
+      conditions_for_code.flat_map do |condition|
+        combinations.map do |combination|
+          combination + [condition]
         end
-      end.map(&Permutation.method(:new))
+      end
+    end
+
+    def groups_when_no_matched_conditions
+      measure_conditions
+        .group_by(&:condition_code)
+        .map(&method(:group_with_permutation_per_condition))
+    end
+
+    def group_with_permutation_per_condition(condition_code, conditions)
+      Group.new(@measure.measure_sid,
+                condition_code,
+                conditions.map(&Permutation.method(:new)))
     end
   end
 end
