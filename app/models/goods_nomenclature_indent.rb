16639ea6bfb589e4865b3d7ea1104b99f174301b
William Fish <william.michael.fish@gmail.com>
2022-03-24 12:11:48 +0000

    HOTT-1414: Tidies

--- app/models/goods_nomenclature_indent.rb
+++ app/models/goods_nomenclature_indent.rb
@@ -2,8 +2,7 @@ class GoodsNomenclatureIndent < Sequel::Model
   set_dataset order(Sequel.desc(:goods_nomenclature_indents__validity_end_date))
 
   plugin :oplog, primary_key: :goods_nomenclature_indent_sid
-  plugin :time_machine, period_start_column: :goods_nomenclature_indents__validity_start_date,
-                        period_end_column: :goods_nomenclature_indents__validity_end_date
+  plugin :time_machine
 
   set_primary_key [:goods_nomenclature_indent_sid]
 end
