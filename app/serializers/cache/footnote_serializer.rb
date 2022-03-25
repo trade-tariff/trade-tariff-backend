3cb358821d54c422ee6f2c563fd2d86a87adc661
William Fish <william.michael.fish@gmail.com>
2022-03-25 11:02:52 +0000

    Handle when a measure has no matching goods nomenclature

--- app/serializers/cache/footnote_serializer.rb
+++ app/serializers/cache/footnote_serializer.rb
@@ -42,7 +42,7 @@ module Cache
             goods_nomenclature_item_id: measure.goods_nomenclature_item_id,
             goods_nomenclature_sid: measure.goods_nomenclature_sid,
             goods_nomenclature: goods_nomenclature_attributes(measure.goods_nomenclature),
-            goods_nomenclature_id: measure.goods_nomenclature.goods_nomenclature_sid,
+            goods_nomenclature_id: measure.goods_nomenclature_sid,
             geographical_area_id: measure.geographical_area_id,
             geographical_area: geographical_area_attributes(measure.geographical_area),
           }
