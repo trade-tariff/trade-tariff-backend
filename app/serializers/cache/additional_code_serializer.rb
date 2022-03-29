39c12fc53ca64fb51a06f2f331cd4bdab4b5b5a7
William Fish <william.michael.fish@gmail.com>
2022-03-29 10:59:24 +0100

    HOTT-1414: Exclude measures without goods nomenclature code search

--- app/serializers/cache/additional_code_serializer.rb
+++ app/serializers/cache/additional_code_serializer.rb
@@ -10,12 +10,6 @@ module Cache
     end
 
     def as_json
-      measures = additional_code.measures_dataset
-                                .exclude(goods_nomenclature_item_id: nil)
-                                .to_a
-                                .select do |measure|
-        has_valid_dates(measure)
-      end
       {
         additional_code_sid: additional_code.additional_code_sid,
         code: additional_code.code,
@@ -26,21 +20,35 @@ module Cache
         validity_start_date: additional_code.validity_start_date,
         validity_end_date: additional_code.validity_end_date,
         measure_ids: measures.map(&:measure_sid),
-        measures: measures.map do |measure|
-          {
-            id: measure.measure_sid,
-            measure_sid: measure.measure_sid,
-            validity_start_date: measure.validity_start_date,
-            validity_end_date: measure.validity_end_date,
-            goods_nomenclature_item_id: measure.goods_nomenclature_item_id,
-            goods_nomenclature_sid: measure.goods_nomenclature_sid,
-            goods_nomenclature_id: measure.goods_nomenclature_sid,
-            goods_nomenclature: goods_nomenclature_attributes(measure.goods_nomenclature),
-            geographical_area_id: measure.geographical_area_id,
-            geographical_area: geographical_area_attributes(measure.geographical_area),
-          }
-        end,
+        measures: serialized_measures,
       }
+    end
+
+    private
+
+    def serialized_measures
+      measures.map do |measure|
+        {
+          id: measure.measure_sid,
+          measure_sid: measure.measure_sid,
+          validity_start_date: measure.validity_start_date,
+          validity_end_date: measure.validity_end_date,
+          goods_nomenclature_item_id: measure.goods_nomenclature_item_id,
+          goods_nomenclature_sid: measure.goods_nomenclature_sid,
+          goods_nomenclature_id: measure.goods_nomenclature_sid,
+          goods_nomenclature: goods_nomenclature_attributes(measure.goods_nomenclature),
+          geographical_area_id: measure.geographical_area_id,
+          geographical_area: geographical_area_attributes(measure.geographical_area),
+        }
+      end
+    end
+
+    def measures
+      additional_code
+        .measures_dataset.eager(:goods_nomenclature)
+        .exclude(goods_nomenclature_item_id: nil)
+        .all
+        .select { |measure| has_valid_dates(measure) && measure.goods_nomenclature.present? }
     end
   end
 end
