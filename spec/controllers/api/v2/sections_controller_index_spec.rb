17ec932c3dd0f3cab8f626b74cc743fb45b1aca0
William Fish <william.michael.fish@gmail.com>
2022-04-11 18:02:24 +0100

    HOTT-1487: Lint

--- spec/controllers/api/v2/sections_controller_index_spec.rb
+++ spec/controllers/api/v2/sections_controller_index_spec.rb
@@ -1,48 +1,50 @@
-RSpec.describe Api::V2::SectionsController, 'GET #index' do
-  before { section }
+RSpec.describe Api::V2::SectionsController do
+  describe 'GET #index' do
+    before { section }
 
-  let(:section) do
-    create(
-      :section,
-      id: 18,
-      position: 18,
-      numeral: 'XVIII',
-      title: 'Optical, photographic, cinematographic, measuring',
-    )
-  end
-
-  context 'when the format is json' do
-    subject(:do_response) { get :index, format: :json }
-
-    let(:pattern) do
-      {
-        data: [
-          {
-            id: section.id.to_s,
-            type: 'section',
-            attributes: {
-              id: section.id,
-              position: section.position,
-              title: section.title,
-              numeral: section.numeral,
-              chapter_from: section.chapter_from,
-              chapter_to: section.chapter_to,
-            },
-          },
-        ],
-      }
+    let(:section) do
+      create(
+        :section,
+        id: 18,
+        position: 18,
+        numeral: 'XVIII',
+        title: 'Optical, photographic, cinematographic, measuring',
+      )
     end
 
-    it { expect(do_response.body).to match_json_expression pattern }
-  end
+    context 'when the format is json' do
+      subject(:do_response) { get :index, format: :json }
 
-  context 'when the format is csv' do
-    subject(:do_response) { get :index, format: :csv }
+      let(:pattern) do
+        {
+          data: [
+            {
+              id: section.id.to_s,
+              type: 'section',
+              attributes: {
+                id: section.id,
+                position: section.position,
+                title: section.title,
+                numeral: section.numeral,
+                chapter_from: section.chapter_from,
+                chapter_to: section.chapter_to,
+              },
+            },
+          ],
+        }
+      end
 
-    render_views
+      it { expect(do_response.body).to match_json_expression pattern }
+    end
 
-    let(:expected_csv) { "id,numeral,title,position,chapter_from,chapter_to\n18,XVIII,\"Optical, photographic, cinematographic, measuring\",18,,\n" }
+    context 'when the format is csv' do
+      subject(:do_response) { get :index, format: :csv }
 
-    it { expect(do_response.body).to eq(expected_csv) }
+      render_views
+
+      let(:expected_csv) { "id,numeral,title,position,chapter_from,chapter_to\n18,XVIII,\"Optical, photographic, cinematographic, measuring\",18,,\n" }
+
+      it { expect(do_response.body).to eq(expected_csv) }
+    end
   end
 end
