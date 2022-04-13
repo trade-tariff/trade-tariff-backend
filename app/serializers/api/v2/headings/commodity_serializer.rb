634d9058794dd02f542bddd53a9267ddad956770
William Fish <william.michael.fish@gmail.com>
2022-04-13 11:53:43 +0100

    HOTT-1493: Just check the present attributes for declarability

--- app/serializers/api/v2/headings/commodity_serializer.rb
+++ app/serializers/api/v2/headings/commodity_serializer.rb
@@ -20,7 +20,7 @@ module Api
 
         attribute :declarable do |commodity|
           # TODO: Once we've got the ES cache populated with declarable we can use a simple attribute to pull this out
-          commodity.try(:declarable) || commodity.leaf && commodity.producline_suffix == GoodsNomenclatureIndent::NON_GROUPING_PRODUCTLINE_SUFFIX
+          commodity.leaf && commodity.producline_suffix == GoodsNomenclatureIndent::NON_GROUPING_PRODUCTLINE_SUFFIX
         end
 
         has_many :overview_measures, record_type: :measure,
