54e551daa8281abe66ac4ed9f8c75ae09e921972
William Fish <william.michael.fish@gmail.com>
2022-04-19 13:30:37 +0100

    HOTT-1518: Code review

--- spec/requests/api/v2/chapters_controller_spec.rb
+++ spec/requests/api/v2/chapters_controller_spec.rb
@@ -1,21 +1,9 @@
 RSpec.describe Api::V2::ChaptersController do
   describe 'GET #index' do
-    subject(:do_request) { make_request && response }
-
-    before do
-      create(:chapter)
-    end
-
-    context 'when using the mime suffix to configure the mime type' do
-      let(:make_request) { get '/chapters.csv' }
-
-      it_behaves_like 'a successful csv response'
-    end
-
-    context 'when using Accept header to configure the mime type' do
-      let(:make_request) { get '/chapters', headers: { 'ACCEPT' => 'text/csv' } }
-
-      it_behaves_like 'a successful csv response'
+    it_behaves_like 'a successful csv response', '/sections' do
+      before do
+        create(:chapter)
+      end
     end
   end
 end
--- spec/requests/api/v2/sections_controller_spec.rb
+++ spec/requests/api/v2/sections_controller_spec.rb
@@ -1,27 +1,15 @@
 RSpec.describe Api::V2::SectionsController do
   describe 'GET #index' do
-    subject(:do_request) { make_request && response }
-
-    before do
-      create(
-        :section,
-        id: 18,
-        position: 18,
-        numeral: 'XVIII',
-        title: 'Optical, photographic, cinematographic, measuring',
-      )
-    end
-
-    context 'when using the mime suffix to configure the mime type' do
-      let(:make_request) { get '/sections.csv' }
-
-      it_behaves_like 'a successful csv response'
-    end
-
-    context 'when using Accept header to configure the mime type' do
-      let(:make_request) { get '/sections', headers: { 'ACCEPT' => 'text/csv' } }
-
-      it_behaves_like 'a successful csv response'
+    it_behaves_like 'a successful csv response', '/sections' do
+      before do
+        create(
+          :section,
+          id: 18,
+          position: 18,
+          numeral: 'XVIII',
+          title: 'Optical, photographic, cinematographic, measuring',
+        )
+      end
     end
   end
 end
