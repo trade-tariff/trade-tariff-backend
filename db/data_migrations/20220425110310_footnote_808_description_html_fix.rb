eab913aa0d30a5d1fc5fd270a92750aa0077c8f8
jebw <jeb@jdwilkins.co.uk>
2022-05-12 14:11:35 +0100

    HOTT-1413 Fix running migration against XI
    caused by `return` triggering longjump in migration but only triggered
    on XI code path

--- db/data_migrations/20220425110310_footnote_808_description_html_fix.rb
+++ db/data_migrations/20220425110310_footnote_808_description_html_fix.rb
@@ -1,16 +1,16 @@
 Sequel.migration do
   up do
-    return unless TradeTariffBackend.uk?
-
-    Sequel::Model.db[:footnote_descriptions_oplog]
-      .where {
-        (footnote_id =~ '808') &
-          (footnote_type_id =~ 'CD') &
-          (footnote_description_period_sid =~ 200_705) &
-          description.like("%'>aRegulation (EC)%") &
-          (operation_date =~ '2022-04-25')
-      }
-      .update(description: %(Where goods are not accompanied by a Certificate of Inspection attesting that they conform to the requirements of <a href='https://www.legislation.gov.uk/eur/2007/834/introduction'>Regulation (EC) No 834/2007</a> and <a href='https://www.legislation.gov.uk/eur/2008/889/contents'>Regulation (EC) No 889/2008</a> as retained in UK law, they will not be released for free circulation unless references to organic production are removed from the labelling, advertising and accompanying documents.))
+    if TradeTariffBackend.uk?
+      Sequel::Model.db[:footnote_descriptions_oplog]
+        .where {
+          (footnote_id =~ '808') &
+            (footnote_type_id =~ 'CD') &
+            (footnote_description_period_sid =~ 200_705) &
+            description.like("%'>aRegulation (EC)%") &
+            (operation_date =~ '2022-04-25')
+        }
+        .update(description: %(Where goods are not accompanied by a Certificate of Inspection attesting that they conform to the requirements of <a href='https://www.legislation.gov.uk/eur/2007/834/introduction'>Regulation (EC) No 834/2007</a> and <a href='https://www.legislation.gov.uk/eur/2008/889/contents'>Regulation (EC) No 889/2008</a> as retained in UK law, they will not be released for free circulation unless references to organic production are removed from the labelling, advertising and accompanying documents.))
+    end
   end
 
   down {}
