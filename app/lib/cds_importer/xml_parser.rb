a5c7732cdd6f32a9bbeae6aef8d6fc44edd8f020
William Fish <william.michael.fish@gmail.com>
2022-05-17 11:45:41 +0100

    HOTT-1582: Lint

--- app/lib/cds_importer/xml_parser.rb
+++ app/lib/cds_importer/xml_parser.rb
@@ -6,7 +6,7 @@ class CdsImporter
   module XmlParser
     class Reader < ::Ox::Sax
       STRIP_NAMESPACE = '*'.freeze
-      CONTENT_KEY = :__content__.freeze
+      CONTENT_KEY = :__content__
 
       def initialize(stringio, target_handler)
         @stringio = stringio
@@ -16,6 +16,8 @@ class CdsImporter
         @in_target = false
         @stack = []
         @depth = 0
+
+        super()
       end
 
       def parse
@@ -55,11 +57,11 @@ class CdsImporter
         when Hash
           @node[key] = [@node[key], child]
         else
-          if child.size == 1 && child.key?(CONTENT_KEY)
-            @node[key] = child[:__content__]
-          else
-            @node[key] = child
-          end
+          @node[key] = if child.size == 1 && child.key?(CONTENT_KEY)
+                         child[:__content__]
+                       else
+                         child
+                       end
         end
       end
     end
