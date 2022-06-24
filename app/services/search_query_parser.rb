2ffbdb736d629a7668ca4d9f3d8d28ad90fb9f41
Alexdesi <alexdesi@gmail.com>
2022-06-24 15:55:48 +0100

    HOTT-1694: Refacor SearchQueryParser - Set Faraday to raise error when response is not 200 - Add support for query params instead of query terms on URL - Add a no-happy path spec test

--- app/services/search_query_parser.rb
+++ app/services/search_query_parser.rb
@@ -1,22 +1,19 @@
+require 'uri'
+
 class SearchQueryParser
   API_URL = ENV['TARIFF_QUERY_SEARCH_PARSER_URL']
 
   def initialize
-    @client = Faraday.new(
-      url: API_URL,
-      headers: {'Content-Type' => 'application/json'}
-      )
+    @client = Faraday.new(API_URL) do |conn|
+      conn.request :json
+
+      conn.response :raise_error
+      conn.response :json
+    end
   end
 
   def get_tokens(search_query_term)
-    api_response = @client.get("tokens/#{search_query_term}")
-
-    response_data = api_response.success? ? JSON.parse(api_response.body) : {}
-
-    {
-      success: api_response.success?,
-      status: api_response.status,
-      data: response_data
-    }
+    @client.get('tokens', q: search_query_term)
+           .body
   end
 end
--- spec/services/search_query_parser_spec.rb
+++ spec/services/search_query_parser_spec.rb
@@ -1,45 +1,56 @@
 RSpec.describe SearchQueryParser do
-  subject(:search_query_parser){ described_class.new }
+  subject(:search_query_parser) { described_class.new }
+
+  let(:search_query_parser_service_url) { 'http://localhost:5000/api/search' }
+
+  before do
+    stub_request(:get, "#{search_query_parser_service_url}/tokens?q=aaa+bbb")
+      .to_return(body: success_response.to_json, status: 200)
+
+    stub_request(:get, "#{search_query_parser_service_url}/tokens?q=")
+        .to_return(status: 400)
+  end
 
   describe '#get_tokens' do
-    let(:success_response) {
+    let(:success_response) do
       {
         entities: {
           noun_chunks: [
-            'aaa bbb'
+            'aaa bbb',
           ],
           tokens: {
             adjectives: [],
-            all: [
-              'aaa',
-              'bbb'
+            all: %w[
+              aaa
+              bbb
             ],
-            nouns: [
-              'bbb'
+            nouns: %w[
+              bbb
             ],
-            verbs: []
-          }
-        }
+            verbs: [],
+          },
+        },
       }
-    }
-
-    let(:pattern) {
-      {
-        success: true,
-        status: 200,
-        data: success_response
-      }
-    }
-
-    before do
-      stub_request(:get, 'http://www.example.com/api/search/tokens/aaa bbb').
-        to_return(body: success_response.to_json, status: 200)
     end
 
-    it 'returns a valid response' do
-      response = subject.get_tokens('aaa%20bbb')
+    let(:pattern) do
+      success_response
+    end
 
-      expect(response).to match_json_expression(pattern)
+    context 'when the query term is a valid string' do
+      let(:term) { 'aaa bbb' }
+
+      it 'returns a valid response' do
+        response = search_query_parser.get_tokens(term)
+
+        expect(response).to match_json_expression(pattern)
+      end
+    end
+
+    context 'when the query term is empty' do
+      let(:term) { '' }
+
+      it { expect { search_query_parser.get_tokens('') }.to raise_error(Faraday::BadRequestError) }
     end
   end
 end
