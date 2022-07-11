7cff9cbc3f9da41c04d0ba3b588e75090a8f249a
William Fish <william.michael.fish@gmail.com>
2022-07-11 11:25:18 +0100

    HOTT-1721: Tweaks the SearchQueryParser service
    This adds support for returning the result model and makes the service
    conform to the same api as all other services

--- app/services/search_query_parser.rb
+++ app/services/search_query_parser.rb
@@ -1,19 +1,20 @@
-require 'uri'
-
 class SearchQueryParser
-  API_URL = ENV['TARIFF_QUERY_SEARCH_PARSER_URL']
+  delegate :client, to: :class
 
-  def initialize
-    @client = Faraday.new(API_URL) do |conn|
-      conn.request :json
+  def initialize(search_query)
+    @search_query = search_query
+  end
 
+  def call
+    result_attributes = client.get('tokens', q: @search_query).body
+
+    Beta::Search::SearchQueryParserResult.build(result_attributes)
+  end
+
+  def self.client
+    @client ||= Faraday.new(TradeTariffBackend.search_query_parser_url) do |conn|
       conn.response :raise_error
       conn.response :json
     end
-  end
-
-  def get_tokens(search_query_term)
-    @client.get('tokens', q: search_query_term)
-           .body
   end
 end
