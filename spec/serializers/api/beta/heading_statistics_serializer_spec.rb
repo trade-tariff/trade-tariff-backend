1e00ea94aa90d28170a076081d1fd8ab1de2699b
William Fish <william.michael.fish@gmail.com>
2022-07-13 16:11:26 +0100

    HOTT-1743: Adds heading statistics serializer and specs

---
+++ spec/serializers/api/beta/heading_statistics_serializer_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe Api::Beta::HeadingStatisticsSerializer do
+  describe '#serializable_hash' do
+    subject(:serializable_hash) { described_class.new(serializable).serializable_hash }
+
+    let(:serializable) { build(:search_result, :single_hit, :generate_statistics).heading_statistics }
+
+    let(:expected) do
+      {
+        data: [
+          {
+            id: '0406',
+            type: :heading_statistic,
+            attributes: {
+              description: 'Cheese and curd',
+              chapter_id: '04',
+              chapter_description: "DAIRY PRODUCE; BIRDS' EGGS; NATURAL HONEY; EDIBLE PRODUCTS OF ANIMAL ORIGIN, NOT ELSEWHERE SPECIFIED OR INCLUDED",
+              score: 74.98428,
+              cnt: 1,
+              avg: 74.98428,
+              chapter_score: 74.98428,
+            },
+          },
+        ],
+      }
+    end
+
+    it { is_expected.to eq(expected) }
+  end
+end
