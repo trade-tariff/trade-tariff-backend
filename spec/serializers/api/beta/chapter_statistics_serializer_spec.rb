037817a5d5b7e293e1b92e4e8a011be4bac5e655
William Fish <william.michael.fish@gmail.com>
2022-07-13 16:11:07 +0100

    HOTT-1743: Adds chapter statistics serializer and specs

---
+++ spec/serializers/api/beta/chapter_statistics_serializer_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Api::Beta::ChapterStatisticsSerializer do
+  describe '#serializable_hash' do
+    subject(:serializable_hash) { described_class.new(serializable).serializable_hash }
+
+    let(:serializable) { build(:search_result, :single_hit, :generate_statistics).chapter_statistics }
+
+    let(:expected) do
+      {
+        data: [
+          {
+            id: '04',
+            type: :chapter_statistic,
+            attributes: {
+              description: "DAIRY PRODUCE; BIRDS' EGGS; NATURAL HONEY; EDIBLE PRODUCTS OF ANIMAL ORIGIN, NOT ELSEWHERE SPECIFIED OR INCLUDED",
+              cnt: 1,
+              score: 74.98428,
+              avg: 74.98428,
+            },
+          },
+        ],
+      }
+    end
+
+    it { is_expected.to eq(expected) }
+  end
+end
