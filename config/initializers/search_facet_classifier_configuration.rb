239f16cd3a81300781c02df1c0ff23fca2dbce01
William Fish <william.michael.fish@gmail.com>
2022-07-19 09:58:00 +0100

    HOTT-1764: Adds initializer to preload converted config

---
+++ config/initializers/search_facet_classifier_configuration.rb
@@ -0,0 +1,8 @@
+unless Rails.env.test?
+  Rails.application.reloader.to_prepare do
+    # trigger loading at boot
+    require_relative '../../app/lib/trade_tariff_backend'
+
+    TradeTariffBackend.search_facet_classifier_configuration
+  end
+end
