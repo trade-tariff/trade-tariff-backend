1800ae46d1fd0d9c496b19ec01cdbc91f3372b28
William Fish <william.michael.fish@gmail.com>
2022-07-19 09:56:11 +0100

    HOTT-1764: Adds classification configuration model

---
+++ spec/models/search_facet_classifier_configuration_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe SearchFacetClassifierConfiguration do
+  describe '.build' do
+    it { is_expected.to respond_to :text }
+    it { is_expected.to respond_to :url }
+  end
+
+  describe '.each_classification' do
+    context 'when passed a block' do
+      it { expect { |block| described_class.each_classification(&block) }.to yield_control.exactly(69).times }
+    end
+
+    context 'when not passed a block' do
+      subject(:each_classification) { described_class.each_classification }
+
+      it { is_expected.to be_nil }
+    end
+  end
+end
