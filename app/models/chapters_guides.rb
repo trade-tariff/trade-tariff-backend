7c313d21e1398302fdf86890fb27a9f87c602ede
William Fish <william.michael.fish@gmail.com>
2022-07-19 10:45:45 +0100

    HOTT-1728: Replace ChapterGuides with GuidesGoodsNomenclature
    Co-authored-by: Alexdesi <alexdesi@gmail.com>

--- app/models/chapters_guides.rb
+++
@@ -1,4 +0,0 @@
-class ChaptersGuides < Sequel::Model
-  # stub class so we can refer to ChaptersGuides,
-  # i.e.: `lib/tasks/chapters.rake`
-end
---
+++ app/models/guides_goods_nomenclature.rb
@@ -0,0 +1,2 @@
+class GuidesGoodsNomenclature < Sequel::Model
+end
