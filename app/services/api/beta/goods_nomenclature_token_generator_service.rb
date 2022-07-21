09e2ee3121f183832e974681058b5bc85bccabb9
William Fish <william.michael.fish@gmail.com>
2022-07-21 17:07:00 +0100

    HOTT-1768: Adds comment to token generator

--- app/services/api/beta/goods_nomenclature_token_generator_service.rb
+++ app/services/api/beta/goods_nomenclature_token_generator_service.rb
@@ -1,5 +1,11 @@
 module Api
   module Beta
+    # Generates tokens which are classified later to indicate facet categories the current
+    # goods nomenclature belongs too.
+    #
+    # The input goods nomenclatures include the initial goods nomenclature and its ancestors.
+    # The result is reversed because we accumulate classifications later and the lowest ancestor
+    # description needs to set the facet classification to be more price. This is especially true where chapters, headings and subheadings may have multiple classifications for a given facet.
     class GoodsNomenclatureTokenGeneratorService
       delegate :lemmatizer, :stop_words, to: TradeTariffBackend
 
