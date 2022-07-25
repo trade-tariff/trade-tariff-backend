83ce215910559839f4526718d2822fd0ee70e431
William Fish <william.michael.fish@gmail.com>
2022-07-25 17:53:06 +0100

    HOTT-1742: Adds facet stats to search result serializers

--- spec/serializers/api/beta/search_result_serializer_spec.rb
+++ spec/serializers/api/beta/search_result_serializer_spec.rb
@@ -2,25 +2,25 @@ RSpec.describe Api::Beta::SearchResultSerializer do
   describe '#serializable_hash' do
     subject(:serializable_hash) { described_class.new(serializable).serializable_hash }
 
-    let(:serializable) { build(:search_result, :clothing, :generate_statistics, :generate_guide_statistics) }
+    let(:serializable) do
+      build(
+        :search_result,
+        :clothing,
+        :generate_heading_and_chapter_statistics,
+        :generate_guide_statistics,
+        :generate_facet_statistics,
+      )
+    end
 
     let(:expected) do
       {
         data: {
           id: '6fc22ae4ee7f6fbe9b4988a4557dd3f9',
           type: :search_result,
-          attributes: {
-            took: 6,
-            timed_out: false,
-            max_score: 77.81122,
-            total_results: 10,
-          },
+          attributes: { took: 1, timed_out: false, max_score: 76.96534, total_results: 10 },
           relationships: {
             search_query_parser_result: {
-              data: {
-                id: '50cf19912960f65490b334ea9c196eea',
-                type: :search_query_parser_result,
-              },
+              data: { id: '50cf19912960f65490b334ea9c196eea', type: :search_query_parser_result },
             },
             hits: {
               data: [
@@ -45,10 +45,20 @@ RSpec.describe Api::Beta::SearchResultSerializer do
               ],
             },
             chapter_statistics: {
-              data: [{ id: '63', type: :chapter_statistic }, { id: '62', type: :chapter_statistic }],
+              data: [
+                { id: '63', type: :chapter_statistic },
+                { id: '62', type: :chapter_statistic },
+              ],
             },
             guide: {
               data: { id: '18', type: :guide },
+            },
+            facet_filter_statistics: {
+              data: [
+                { id: '2ecbb6c19ee6282b0c79dda2aeaf0192', type: :facet_filter_statistic },
+                { id: 'b24e66a126ad13c1521cf6cda4b2c502', type: :facet_filter_statistic },
+                { id: 'b030d559d41aee55d3cd439888aa5edf', type: :facet_filter_statistic },
+              ],
             },
           },
         },
