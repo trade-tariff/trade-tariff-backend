75baa873cca8098ceaea3829ff9b76c20d8611c2
William Fish <william.michael.fish@gmail.com>
2022-07-25 11:50:21 +0100

    HOTT-1774: Adds spec and handling for missing memberships

--- app/lib/cds_importer/entity_mapper/geographical_area_membership_mapper.rb
+++ app/lib/cds_importer/entity_mapper/geographical_area_membership_mapper.rb
@@ -13,25 +13,28 @@ class CdsImporter
         "#{mapping_path}.geographicalAreaSid" => :geographical_area_sid,
       ).freeze
 
+      # This mutates misnamed attributes on geogrphical area memberships and coerces
+      # them to be comprehensible to our importer.
       before_building_model do |xml_node|
-        unless xml_node['geographicalAreaMembership'].is_a?(Array)
-          xml_node['geographicalAreaMembership'] = [xml_node['geographicalAreaMembership']]
-        end
+        # Handle no membership XML node
+        if xml_node['geographicalAreaMembership'].present?
+          # Handle single membership XML node
+          xml_node['geographicalAreaMembership'] = Array.wrap(xml_node['geographicalAreaMembership'])
+          xml_node['geographicalAreaMembership'] = xml_node['geographicalAreaMembership'].each_with_object([]) do |geographical_area_membership, array|
+            unless geographical_area_membership.key?('geographicalAreaGroupSid')
+              message = "Skipping membership import due to missing geographical area group sid. hjid is #{geographical_area_membership['hjid']}\n"
 
-        xml_node['geographicalAreaMembership'] = xml_node['geographicalAreaMembership'].each_with_object([]) do |geographical_area_membership, array|
-          unless geographical_area_membership.key?('geographicalAreaGroupSid')
-            message = "Skipping membership import due to missing geographical area group sid. hjid is #{geographical_area_membership['hjid']}\n"
+              instrument_warning(message, xml_node)
+              next
+            end
 
-            instrument_warning(message, xml_node)
-            next
+            geographical_area = GeographicalArea[hjid: geographical_area_membership['geographicalAreaGroupSid']]
+
+            geographical_area_membership['geographicalAreaSid'] = geographical_area&.geographical_area_sid
+            geographical_area_membership['geographicalAreaGroupSid'] = xml_node['sid'].to_i
+
+            array << geographical_area_membership
           end
-
-          geographical_area = GeographicalArea[hjid: geographical_area_membership['geographicalAreaGroupSid']]
-
-          geographical_area_membership['geographicalAreaSid'] = geographical_area&.geographical_area_sid
-          geographical_area_membership['geographicalAreaGroupSid'] = xml_node['sid'].to_i
-
-          array << geographical_area_membership
         end
       end
     end
