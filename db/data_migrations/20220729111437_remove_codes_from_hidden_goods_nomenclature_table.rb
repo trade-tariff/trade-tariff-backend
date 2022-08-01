5cc197a90beccbb89e92c83af5122275ef5f9420
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2022-08-01 10:48:56 +0100

    rename code to goods_nomenclature_item_id

--- db/data_migrations/20220729111437_remove_codes_from_hidden_goods_nomenclature_table.rb
+++ db/data_migrations/20220729111437_remove_codes_from_hidden_goods_nomenclature_table.rb
@@ -3,19 +3,21 @@ Sequel.migration do
     codes = %w[9900000000 9905000000 9919000000 9919000010 9919000020]
 
     up do
-      codes.each do |code|
-        HiddenGoodsNomenclature.where(goods_nomenclature_item_id: code).delete
+      codes.each do |goods_nomenclature_item_id|
+        HiddenGoodsNomenclature.where(goods_nomenclature_item_id:).delete
       end
     end
 
     down do
-      codes.each do |code|
-        next unless HiddenGoodsNomenclature.where(goods_nomenclature_item_id: code).count.zero?
+      HiddenGoodsNomenclature.unrestrict_primary_key
 
-        HiddenGoodsNomenclature.unrestrict_primary_key
-        HiddenGoodsNomenclature.create(goods_nomenclature_item_id: code)
-        HiddenGoodsNomenclature.restrict_primary_key
+      codes.each do |goods_nomenclature_item_id|
+        next unless HiddenGoodsNomenclature.where(goods_nomenclature_item_id:).count.zero?
+
+        HiddenGoodsNomenclature.create(goods_nomenclature_item_id:)
       end
+
+      HiddenGoodsNomenclature.restrict_primary_key
     end
   end
 end
