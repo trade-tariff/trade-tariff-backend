2d6d7e559a39554e1a78c8802934cf4a7ce8d1f8
javier-transform <javier.fernandez@transformuk.com>
2022-08-11 12:51:50 +0500

    Adds conditions to check presence of both results

--- db/data_migrations/20220805304709_fix_broken_comm_code_descriptions.rb
+++ db/data_migrations/20220805304709_fix_broken_comm_code_descriptions.rb
@@ -1,9 +1,13 @@
 Sequel.migration do
   up do
     tunas = GoodsNomenclatureDescription.where(goods_nomenclature_description_period_sid: 154_585)
-    tunas.update(description: 'Tunas (of the genus Thunnus), skipjack tuna (stripe-bellied bonito) (Katsuwonus pelamis), excluding edible fish offal of subheadings 0302 91 to 0302 99')
+    if tunas.present?
+      tunas.update(description: 'Tunas (of the genus Thunnus), skipjack tuna (stripe-bellied bonito) (Katsuwonus pelamis), excluding edible fish offal of subheadings 0302 91 to 0302 99')
+    end
     engines = GoodsNomenclatureDescription.where(goods_nomenclature_description_period_sid: 157_423)
-    engines.update(description: 'Four-stroke petrol engines of a cylinder capacity of not more than 250 cm3 for use in the manufacture of garden equipment of heading 8432, 8433, 8436 or 8508')
+    if engines.present?
+      engines.update(description: 'Four-stroke petrol engines of a cylinder capacity of not more than 250 cm3 for use in the manufacture of garden equipment of heading 8432, 8433, 8436 or 8508')
+    end
   end
 
   down {}
