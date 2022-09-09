be173bd8c355c8b7eea070714cf13ba69e8f237f
William Fish <william.michael.fish@gmail.com>
2022-09-09 11:41:57 +0100

    HOTT-1658: Removes export refund nomenclature query

--- app/models/concerns/declarable.rb
+++ app/models/concerns/declarable.rb
@@ -20,33 +20,41 @@ module Declarable
       Measure.join(
         Measure.with_base_regulations
                .with_actual(BaseRegulation)
-               .where({ measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid) })
-               .where { Sequel.~(measures__measure_type_id: MeasureType.excluded_measure_types) }
+               .where(measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid))
+               .exclude(measures__measure_type_id: MeasureType.excluded_measure_types)
                .order(*COMMON_UNION_MEASURE_ORDER)
-               .tap! { |query|
-                 query.union(
-                   Measure.with_base_regulations
-                     .with_actual(BaseRegulation)
-                     .where({ measures__export_refund_nomenclature_sid: export_refund_uptree.map(&:export_refund_nomenclature_sid) })
-                     .where { Sequel.~(measures__measure_type_id: MeasureType.excluded_measure_types) }
-                     .order(*COMMON_UNION_MEASURE_ORDER),
-                 ) if export_refund_uptree.present?
-               }
        .union(
          Measure.with_modification_regulations
                 .with_actual(ModificationRegulation)
-                .where({ measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid) })
-                .where { Sequel.~(measures__measure_type_id: MeasureType.excluded_measure_types) }
-                .order(*COMMON_UNION_MEASURE_ORDER)
-                .tap! { |query|
-                  query.union(
-                    Measure.with_modification_regulations
-                           .with_actual(ModificationRegulation)
-                           .where({ measures__export_refund_nomenclature_sid: export_refund_uptree.map(&:export_refund_nomenclature_sid) })
-                           .where { Sequel.~(measures__measure_type_id: MeasureType.excluded_measure_types) }
-                           .order(*COMMON_UNION_MEASURE_ORDER),
-                  ) if export_refund_uptree.present?
-                },
+                .where(measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid))
+                .exclude(measures__measure_type_id: MeasureType.excluded_measure_types)
+                .order(*COMMON_UNION_MEASURE_ORDER),
+         alias: :measures,
+       )
+       .with_actual(Measure)
+       .order(Sequel.asc(:measures__geographical_area_id),
+              Sequel.asc(:measures__measure_type_id),
+              Sequel.asc(:measures__additional_code_type_id),
+              Sequel.asc(:measures__additional_code_id),
+              Sequel.asc(:measures__ordernumber),
+              Sequel.desc(:effective_start_date)),
+        t1__measure_sid: :measures__measure_sid,
+      )
+    }
+
+    one_to_many :measures, primary_key: {}, key: {}, dataset: -> {
+      Measure.join(
+        Measure.with_base_regulations
+               .with_actual(BaseRegulation)
+               .where(measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid))
+               .exclude(measures__measure_type_id: MeasureType.excluded_measure_types)
+               .order(*COMMON_UNION_MEASURE_ORDER)
+       .union(
+         Measure.with_modification_regulations
+                .with_actual(ModificationRegulation)
+                .where(measures__goods_nomenclature_sid: uptree.map(&:goods_nomenclature_sid))
+                .exclude(measures__measure_type_id: MeasureType.excluded_measure_types)
+                .order(*COMMON_UNION_MEASURE_ORDER),
          alias: :measures,
        )
        .with_actual(Measure)
@@ -78,10 +86,6 @@ module Declarable
                                using: :description
     custom_format :formatted_description, with: DescriptionFormatter,
                                    using: :description
-  end
-
-  def export_refund_uptree
-    @_export_refund_uptree ||= export_refund_nomenclatures.map(&:uptree).flatten
   end
 
   def chapter_id
