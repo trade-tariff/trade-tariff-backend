304aa8f1f4d92725d70f7d8e0882208f289542c5
William Fish <william.michael.fish@gmail.com>
2022-09-15 13:11:44 +0100

    HOTT-1968: Adds intercept formatted message method

--- app/models/beta/search/intercept_message.rb
+++ app/models/beta/search/intercept_message.rb
@@ -1,6 +1,37 @@
 module Beta
   module Search
     class InterceptMessage
+      CHAPTER_REGEX = /(?<type>chapter)s? (?<code>[0-9]{1,2})(?<terminator>[.,\s])/i
+      HEADING_REGEX = /(?<type>(?<!sub)heading)s? (?<code>[0-9]{4})(?<terminator>[.,\s])/i
+      SUBHEADING_REGEX = /(?<type>subheading)s? (?<code>[0-9]{6,8})(?<terminator>[.,\s])/i
+      COMMODITY_REGEX = /(?<type>commodity|commodities) (?<code>[0-9]{10})(?<terminator>[.,\s])/i
+
+      GOODS_NOMENCLATURE_LINK_TRANSFORMERS = {
+        CHAPTER_REGEX => lambda do |matched_text|
+          match = matched_text.match(CHAPTER_REGEX)
+
+          "(#{matched_text[0..-2]})[/chapters/#{match[:code].rjust(2, '0')}]#{match[:terminator]}"
+        end,
+
+        HEADING_REGEX => lambda do |matched_text|
+          match = matched_text.match(HEADING_REGEX)
+
+          "(#{matched_text[0..-2]})[/headings/#{match[:code]}]#{match[:terminator]}"
+        end,
+
+        SUBHEADING_REGEX => lambda do |matched_text|
+          match = matched_text.match(SUBHEADING_REGEX)
+
+          "(#{matched_text[0..-2]})[/subheadings/#{match[:code].ljust(10, '0')}-80]#{match[:terminator]}"
+        end,
+
+        COMMODITY_REGEX => lambda do |matched_text|
+          match = matched_text.match(COMMODITY_REGEX)
+
+          "(#{matched_text[0..-2]})[/commodities/#{match[:code]}]#{match[:terminator]}"
+        end,
+      }.freeze
+
       include ContentAddressableId
 
       content_addressable_fields :term, :message
@@ -18,6 +49,16 @@ module Beta
         result.message = I18n.t("#{query}.message")
 
         result
+      end
+
+      def formatted_message
+        return '' if message.blank?
+
+        GOODS_NOMENCLATURE_LINK_TRANSFORMERS.each_with_object(message.to_s) do |(regex, transformer), transformed_message|
+          transformed_message.gsub!(regex) do |matched_text|
+            transformer.call(matched_text)
+          end
+        end
       end
     end
   end
