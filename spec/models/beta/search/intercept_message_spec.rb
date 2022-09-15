8f89809618cb28b7c871df6b25c258762d4ca21e
William Fish <william.michael.fish@gmail.com>
2022-09-15 13:12:06 +0100

    HOTT-1968: Adds InterceptMessage#formatted_message specs

--- spec/models/beta/search/intercept_message_spec.rb
+++ spec/models/beta/search/intercept_message_spec.rb
@@ -9,4 +9,64 @@ RSpec.describe Beta::Search::InterceptMessage do
     it { expect(result).to respond_to(:term) }
     it { expect(result).to respond_to(:message) }
   end
+
+  describe '#formatted_message' do
+    context 'when there are a mixture of different goods nomenclature to generate links for' do
+      subject(:formatted_message) { build(:intercept_message, :with_mixture_of_goods_nomenclature_to_transform).formatted_message }
+
+      let(:expected_message) do
+        '(chapter 1)[/chapters/01], (heading 0101)[/headings/0101], (subheading 012012)[/subheadings/0120120000-80] and (commodity 0702000007)[/commodities/0702000007].'
+      end
+
+      it { is_expected.to eq(expected_message) }
+    end
+
+    context 'when there are chapters to generate links for' do
+      subject(:formatted_message) { build(:intercept_message, :with_chapters_to_transform).formatted_message }
+
+      let(:expected_message) do
+        'This should point to (ChaPter 99)[/chapters/99] and (chapters 32)[/chapters/32] and (chapters 1)[/chapters/01] but not chapter 19812321 but for (chapter 9)[/chapters/09].'
+      end
+
+      it { is_expected.to eq(expected_message) }
+    end
+
+    context 'when there are headings to generate links for' do
+      subject(:formatted_message) { build(:intercept_message, :with_headings_to_transform).formatted_message }
+
+      let(:expected_message) do
+        'This should point to (hEadIngs 0101)[/headings/0101] and (heading 0102)[/headings/0102] but not heading 2 or heading 012012 but for (heading 0105)[/headings/0105].'
+      end
+
+      it { is_expected.to eq(expected_message) }
+    end
+
+    context 'when there are subheadings to generate links for' do
+      subject(:formatted_message) { build(:intercept_message, :with_subheadings_to_transform).formatted_message }
+
+      let(:expected_message) do
+        'This should point to (subheadiNg 010511)[/subheadings/0105110000-80] and (subheadings 01051191)[/subheadings/0105119100-80] and never change subheading 1231 or subheading 1231312312 but for (subheading 010512)[/subheadings/0105120000-80].'
+      end
+
+      it { is_expected.to eq(expected_message) }
+    end
+
+    context 'when there are commodities to generate links for' do
+      subject(:formatted_message) { build(:intercept_message, :with_commodities_to_transform).formatted_message }
+
+      let(:expected_message) do
+        'This should point to (coMmodities 0105110000)[/commodities/0105110000] and cOmmodity 01051191 and never change commodity 1 or commodity 13112313123123 but for (commodity 0101210001)[/commodities/0101210001].'
+      end
+
+      it { is_expected.to eq(expected_message) }
+    end
+
+    context 'when there is no corresponding message' do
+      subject(:formatted_message) { build(:intercept_message, :without_message).formatted_message }
+
+      let(:expected_message) { '' }
+
+      it { is_expected.to eq(expected_message) }
+    end
+  end
 end
