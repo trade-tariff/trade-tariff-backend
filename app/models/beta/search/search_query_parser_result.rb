48ddf68a2353e9ed4a825c2ba3c01f35c99e0ebe
William Fish <william.michael.fish@gmail.com>
2022-10-11 12:14:22 +0100

    HOTT-2119: Support synonym parser result

--- app/models/beta/search/search_query_parser_result.rb
+++ app/models/beta/search/search_query_parser_result.rb
@@ -2,6 +2,9 @@ module Beta
   module Search
     class SearchQueryParserResult
       include ActiveModel::Model
+      include ContentAddressableId
+
+      content_addressable_fields :original_search_query
 
       attr_accessor :adjectives,
                     :nouns,
@@ -10,23 +13,36 @@ module Beta
                     :original_search_query,
                     :corrected_search_query
 
-      def self.build(attributes)
-        search_query_parser_result = new
+      class Standard
+        def self.build(attributes)
+          search_query_parser_result = SearchQueryParserResult.new
 
-        attributes = JSON.parse(attributes) if attributes.is_a?(String)
+          attributes = JSON.parse(attributes) if attributes.is_a?(String)
 
-        search_query_parser_result.original_search_query = attributes['original_search_query']
-        search_query_parser_result.corrected_search_query = attributes['corrected_search_query']
-        search_query_parser_result.adjectives = attributes.dig('tokens', 'adjectives')
-        search_query_parser_result.nouns = attributes.dig('tokens', 'nouns')
-        search_query_parser_result.verbs = attributes.dig('tokens', 'verbs')
-        search_query_parser_result.noun_chunks = attributes.dig('tokens', 'noun_chunks')
+          search_query_parser_result.original_search_query = attributes['original_search_query']
+          search_query_parser_result.corrected_search_query = attributes['corrected_search_query']
+          search_query_parser_result.adjectives = attributes.dig('tokens', 'adjectives')
+          search_query_parser_result.nouns = attributes.dig('tokens', 'nouns')
+          search_query_parser_result.verbs = attributes.dig('tokens', 'verbs')
+          search_query_parser_result.noun_chunks = attributes.dig('tokens', 'noun_chunks')
 
-        search_query_parser_result
+          search_query_parser_result
+        end
       end
 
-      def id
-        Digest::MD5.hexdigest(original_search_query)
+      class Synonym
+        def self.build(attributes)
+          search_query_parser_result = SearchQueryParserResult.new
+
+          search_query_parser_result.original_search_query = attributes['original_search_query']
+          search_query_parser_result.corrected_search_query = ''
+          search_query_parser_result.adjectives = []
+          search_query_parser_result.nouns = []
+          search_query_parser_result.verbs = []
+          search_query_parser_result.noun_chunks = [attributes['original_search_query']]
+
+          search_query_parser_result
+        end
       end
     end
   end
