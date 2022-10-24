d48c77b6682251db97bec919c85313520e0bf782
William Fish <william.michael.fish@gmail.com>
2022-10-24 12:29:03 +0100

    HOTT-2167: Adds serializers for quota order numbers

---
+++ app/serializers/api/admin/quota_order_numbers/quota_definition_serializer.rb
@@ -0,0 +1,19 @@
+module Api
+  module Admin
+    module QuotaOrderNumbers
+      class QuotaDefinitionSerializer
+        include JSONAPI::Serializer
+
+        set_type :quota_definition
+
+        set_id :quota_definition_sid
+
+        attributes :validity_start_date,
+                   :validity_end_date,
+                   :initial_volume
+
+        has_many :quota_balance_events, serializer: Api::Admin::QuotaOrderNumbers::QuotaBalanceEventSerializer
+      end
+    end
+  end
+end
---
+++ spec/serializers/api/admin/quota_order_numbers/quota_balance_event_serializer_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Api::Admin::QuotaOrderNumbers::QuotaBalanceEventSerializer do
+  describe '#serializable_hash' do
+    subject(:serialized) { described_class.new(serializable).serializable_hash }
+
+    let(:serializable) { build(:quota_balance_event) }
+
+    let(:expected) do
+      {
+        data: {
+          id: "#{serializable.quota_definition_sid}-#{serializable.occurrence_timestamp.iso8601}",
+          type: :quota_balance_event,
+          attributes: {
+            occurrence_timestamp: serializable.occurrence_timestamp,
+            new_balance: serializable.new_balance,
+            imported_amount: serializable.imported_amount,
+          },
+        },
+      }
+    end
+
+    it { is_expected.to eq(expected) }
+  end
+end
