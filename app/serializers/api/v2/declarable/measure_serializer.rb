5cc0f7f4bd7573bd58333395b47d8ad2e60c84cf
Alexdesi <alexdesi@gmail.com>
2022-11-07 09:28:00 +0000

    HOTT-2029 Refactor: remove duplication in declarable/measure_serializer and measure_serializer.

--- app/serializers/api/v2/declarable/measure_serializer.rb
+++ app/serializers/api/v2/declarable/measure_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V2
     module Declarable
-      class MeasureSerializer
+      class MeasureSerializer < Api::V2::BaseMeasureSerializer
         include JSONAPI::Serializer
 
         set_type :measure
@@ -9,11 +9,8 @@ module Api
         set_id :measure_sid
 
         attributes :id,
-                   :origin,
                    :effective_start_date,
                    :effective_end_date,
-                   :import,
-                   :export,
                    :excise,
                    :vat,
                    :reduction_indicator,
@@ -22,23 +19,15 @@ module Api
                    :universal_waiver_applies
 
         has_one :preference_code, serializer: Api::V2::PreferenceCodeSerializer
-        has_one :duty_expression, serializer: Api::V2::Measures::DutyExpressionSerializer
-        has_one :measure_type, serializer: Api::V2::Measures::MeasureTypeSerializer
-        has_many :legal_acts, serializer: Api::V2::Measures::MeasureLegalActSerializer
         has_one :suspending_regulation, key: :suspension_legal_act,
                                         record_type: :suspension_legal_act, serializer: Api::V2::Measures::MeasureSuspensionLegalActSerializer,
                                         if: proc { |measure| !measure.national && measure.suspended? }
-        has_many :measure_conditions, serializer: Api::V2::Measures::MeasureConditionSerializer
         has_many :measure_condition_permutation_groups, serializer: Api::V2::Measures::MeasureConditionPermutationGroupSerializer
-        has_many :measure_components, serializer: Api::V2::Measures::MeasureComponentSerializer
         has_many :resolved_measure_components, serializer: Api::V2::Measures::MeasureComponentSerializer
 
         has_many :national_measurement_units, serializer: Api::V2::Measures::NationalMeasurementUnitSerializer
-        has_one :geographical_area, serializer: Api::V2::Measures::GeographicalAreaSerializer
         has_many :excluded_countries, record_type: :geographical_area, serializer: Api::V2::GeographicalAreaSerializer
-        has_many :footnotes, serializer: Api::V2::Measures::FootnoteSerializer
         has_one :additional_code, if: proc { |measure| measure.additional_code.present? }, serializer: Api::V2::AdditionalCodeSerializer
-        has_one :order_number, serializer: Api::V2::Quotas::OrderNumber::QuotaOrderNumberSerializer
 
         meta do |measure|
           {
--- app/serializers/api/v2/measure_serializer.rb
+++ app/serializers/api/v2/measure_serializer.rb
@@ -1,17 +1,7 @@
 module Api
   module V2
     # Serializer used in MeasuresController#show
-    class MeasureSerializer
-      include JSONAPI::Serializer
-
-      set_type :measure
-
-      set_id :measure_sid
-
-      attributes :origin,
-                 :import,
-                 :export
-
+    class MeasureSerializer < Api::V2::BaseMeasureSerializer
       attribute :excise, &:excise?
       attribute :vat, &:vat?
 
@@ -26,16 +16,8 @@ module Api
       has_one :goods_nomenclature,
               serializer: proc { |record, _params| "Api::V2::Shared::#{record.goods_nomenclature_class}Serializer".constantize }
 
-      has_one :duty_expression, serializer: Api::V2::Measures::DutyExpressionSerializer
-      has_one :measure_type, serializer: Api::V2::Measures::MeasureTypeSerializer
-      has_many :legal_acts, serializer: Api::V2::Measures::MeasureLegalActSerializer
-      has_many :measure_conditions, serializer: Api::V2::Measures::MeasureConditionSerializer
-      has_many :measure_components, serializer: Api::V2::Measures::MeasureComponentSerializer
-      has_one :geographical_area, serializer: Api::V2::Measures::GeographicalAreaSerializer
       has_many :excluded_geographical_areas, serializer: Api::V2::GeographicalAreaSerializer
-      has_many :footnotes, serializer: Api::V2::Measures::FootnoteSerializer
       has_one :additional_code, serializer: Api::V2::AdditionalCodeSerializer
-      has_one :order_number, serializer: Api::V2::Quotas::OrderNumber::QuotaOrderNumberSerializer
     end
   end
 end
