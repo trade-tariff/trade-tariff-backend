e0c77700c43bdc27f29915eaec33050f87b6c121
William Fish <william.michael.fish@gmail.com>
2022-12-05 13:51:39 +0000

    HOTT-2315: Cleanup implicitly deleted data

---
+++ db/data_migrations/20221205133316_remove_broken_implicit_delete_quota_data.rb
@@ -0,0 +1,31 @@
+Sequel.migration do
+  # IMPORTANT! Data migrations should be Idempotent, they may get re-run as part
+  # of data rollbacks
+
+  up do
+    return unless TradeTariffBackend.uk?
+
+    origin = QuotaOrderNumberOrigin::Operation.new
+
+    origin.quota_order_number_origin_sid = 21096
+    origin.quota_order_number_sid = 20142
+    origin.geographical_area_id = 'IN'
+    origin.validity_start_date = '2021-01-01T00:00:00'
+    origin.validity_end_date = nil
+    origin.geographical_area_sid = '154'
+    origin.operation = Sequel::Plugins::Oplog::DESTROY_OPERATION
+    origin.operation_date = '2022-12-03'
+    origin.filename = 'tariff_dailyExtract_v1_20221202T235959.gzip'
+
+    origin.save
+  end
+
+  down do
+    return unless TradeTariffBackend.uk?
+
+    QuotaOrderNumberOrigin::Operation.find(
+      quota_order_number_origin_sid: 21096,
+      operation: Sequel::Plugins::Oplog::DESTROY_OPERATION,
+    ).delete
+  end
+end
