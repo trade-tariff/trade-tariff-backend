13cdcc5f1d023d16457e490840fa2f76a0fa6a4f
William Fish <william.michael.fish@gmail.com>
2022-12-13 13:01:13 +0000

    HOTT-2297: Adds tests transfer event serializer updates

--- app/serializers/api/v2/quotas/quota_closed_and_transferred_event_serializer.rb
+++ app/serializers/api/v2/quotas/quota_closed_and_transferred_event_serializer.rb
@@ -16,15 +16,24 @@ module Api
           event.target_quota_definition.try(:validity_start_date)
         end
 
-        attribute :target_quota_definition_validity_start_date do |event|
-          event.target_quota_definition.try(:validity_start_date)
+        attribute :target_quota_definition_validity_end_date do |event|
+          event.target_quota_definition.try(:validity_end_date)
+        end
+
+        attribute :target_quota_definition_measurement_unit do |event|
+          event.target_quota_definition.try(:formatted_measurement_unit)
         end
 
         attribute :quota_definition_validity_start_date do |event|
           event.quota_definition.try(:validity_start_date)
         end
+
         attribute :quota_definition_validity_end_date do |event|
           event.transferred_amount.try(:validity_start_date)
+        end
+
+        attribute :quota_definition_measurement_unit do |event|
+          event.target_quota_definition.try(:formatted_measurement_unit)
         end
       end
     end
