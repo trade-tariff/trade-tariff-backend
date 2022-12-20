0e275950bae865962057800c276e05263751e798
jebw <jeb@jdwilkins.co.uk>
2022-12-20 12:10:40 +0000

    HOTT-2210 Fix redlock with Sidekiq 7

--- config/initializers/redis_lock_db.rb
+++ config/initializers/redis_lock_db.rb
@@ -5,3 +5,29 @@ module RedisLockDb
     end
   end
 end
+
+module RedlockEvalShaFix
+  def lock(resource, val, ttl, allow_new_lock)
+    recover_from_script_flush do
+      @redis.with do |conn|
+        conn.evalsha Redlock::Scripts::LOCK_SCRIPT_SHA, [resource], [val, ttl, allow_new_lock]
+      end
+    end
+  rescue Redis::BaseConnectionError
+    false
+  end
+
+  # rubocop:disable Style/RescueStandardError
+  def unlock(resource, val)
+    recover_from_script_flush do
+      @redis.with do |conn|
+        conn.evalsha Redlock::Scripts::UNLOCK_SCRIPT_SHA, [resource], [val]
+      end
+    end
+  rescue
+    # Nothing to do, unlocking is just a best-effort attempt.
+  end
+  # rubocop:enable Style/RescueStandardError
+end
+
+Redlock::Client::RedisInstance.prepend RedlockEvalShaFix
