37ee5c6c1fd67002f2c89a9a44dea7622ba96117
William Fish <william.michael.fish@gmail.com>
2023-01-10 11:14:17 +0000

    HOTT-2372: Adds initial loader and specs

---
+++ spec/lib/spelling_corrector/loaders/initial_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe SpellingCorrector::Loaders::Initial do
+  include_context 'with a stubbed s3 bucket'
+
+  describe '#load' do
+    subject(:load) { described_class.new.load }
+
+    let(:expected_terms) do
+      {
+        'aardvark' => 2,
+        'aardvarks' => 1,
+        'aardwolf' => 1,
+        'aare' => 1,
+        'aarhus' => 1,
+        'aaron' => 3,
+        'aarp' => 1,
+        'aas' => 1,
+        'aat' => 1,
+      }
+    end
+
+    it { is_expected.to eq(expected_terms) }
+  end
+end
