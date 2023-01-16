08dc0d6e6359e614d51a495097c6cd431944b926
William Fish <william.michael.fish@gmail.com>
2023-01-16 17:30:00 +0000

    HOTT-2484: Adds requirement operator method to condition code

--- app/models/measure_condition_code.rb
+++ app/models/measure_condition_code.rb
@@ -1,5 +1,20 @@
 class MeasureConditionCode < Sequel::Model
   ENTRY_PRICE_SYSTEM_CODE = 'V'.freeze
+  REQUIREMENT_CONDITION_CODE_OPERATORS = {
+    "E": '=<',
+    "F": '=>',
+    "G": '=>',
+    "I": '=<',
+    "J": '>',
+    "L": '>',
+    "M": '=>',
+    "N": '=>',
+    "O": '>',
+    "R": '=>',
+    "U": '>',
+    "V": '=>',
+    "X": '>',
+  }.freeze
 
   plugin :time_machine
   plugin :oplog, primary_key: :condition_code
@@ -10,4 +25,8 @@ class MeasureConditionCode < Sequel::Model
                                                   primary_key: :condition_code
 
   delegate :description, to: :measure_condition_code_description
+
+  def requirement_operator
+    REQUIREMENT_CONDITION_CODE_OPERATORS[condition_code&.to_sym]
+  end
 end
