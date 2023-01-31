8b03b785788b25d33cd2c3074326eaa54890fda9
William Fish <william.michael.fish@gmail.com>
2023-01-31 14:57:37 +0000

    HOTT-2605: Lint what you can automatically

--- app/services/search_service/fuzzy_search.rb
+++ app/services/search_service/fuzzy_search.rb
@@ -2,20 +2,18 @@ class SearchService
   class FuzzySearch < BaseSearch
     def search!
       begin
-        @results ||= begin
-          FuzzySearchResult.new(query_string, date).each_with_object({}) do |(match_type, search_index, results), memo|
-            results.uniq! do |result|
-              if result['_source'].has_key?('reference')
-                result['_source']['reference']['id']
-              else
-                result
-              end
+        @results ||= FuzzySearchResult.new(query_string, date).each_with_object({}) do |(match_type, search_index, results), memo|
+          results.uniq! do |result|
+            if result['_source'].key?('reference')
+              result['_source']['reference']['id']
+            else
+              result
             end
-
-            memo.deep_merge!({
-              match_type => { search_index.type.pluralize => results }
-            })
           end
+
+          memo.deep_merge!({
+            match_type => { search_index.type.pluralize => results },
+          })
         end
       rescue OpenSearch::Transport::Transport::Error
         # rescue from malformed queries, return empty resultset in that case
