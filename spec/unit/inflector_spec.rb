968aa788bec8858fc98fdfa8bda33608816770e7
jebw <jeb@jdwilkins.co.uk>
2023-01-31 14:55:21 +0000

    HOTT-2566 Correctly pluralize RulesOfOrigin terms
    Needed by router if using resourceful routes

---
+++ spec/unit/inflector_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'ActiveSupport::Inflector' do
+  describe '#pluralize' do
+    it { expect('rule_of_origin'.pluralize).to eq 'rules_of_origin' }
+    it { expect('rules_of_origin'.pluralize).to eq 'rules_of_origin' }
+    it { expect('RuleOfOrigin'.pluralize).to eq 'RulesOfOrigin' }
+    it { expect('RulesOfOrigin'.pluralize).to eq 'RulesOfOrigin' }
+  end
+
+  describe '#singularize' do
+    it { expect('rule_of_origin'.singularize).to eq 'rule_of_origin' }
+    it { expect('rules_of_origin'.singularize).to eq 'rule_of_origin' }
+    it { expect('RuleOfOrigin'.singularize).to eq 'RuleOfOrigin' }
+    it { expect('RulesOfOrigin'.singularize).to eq 'RuleOfOrigin' }
+  end
+end
