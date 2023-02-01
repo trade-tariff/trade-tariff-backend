9868ecbc09013319744f61525c7dbc2dbdcabb3e
William Fish <william.michael.fish@gmail.com>
2023-02-01 15:11:58 +0000

    HOTT-2495: Fixes footnote controller specs

--- spec/controllers/api/v2/footnotes_controller_spec.rb
+++ spec/controllers/api/v2/footnotes_controller_spec.rb
@@ -2,108 +2,6 @@ RSpec.describe Api::V2::FootnotesController, type: :controller do
   subject(:do_response) { get :search, params: query, format: :json && response }
 
   let(:footnote) { create(:footnote, :with_description) }
-  let(:pattern) do
-    {
-      data: [
-        {
-          id: String,
-          type: 'footnote',
-          attributes: {
-            code: String,
-            footnote_type_id: String,
-            footnote_id: String,
-            description: String,
-            formatted_description: String,
-            extra_large_measures: false,
-          },
-          relationships: {
-            measures: {
-              data: [
-                {
-                  id: String,
-                  type: 'measure',
-                },
-              ],
-            },
-            goods_nomenclatures: {
-              data: [
-                {
-                  id: String,
-                  type: 'heading',
-                },
-                {
-                  id: String,
-                  type: 'chapter',
-                },
-              ],
-            },
-          },
-        },
-      ],
-      included: [
-        {
-          id: String,
-          type: 'chapter',
-          attributes: {
-            goods_nomenclature_item_id: String,
-            description: String,
-            formatted_description: String,
-            producline_suffix: String,
-          },
-        },
-        {
-          id: String,
-          type: 'heading',
-          attributes: {
-            goods_nomenclature_item_id: String,
-            description: String,
-            formatted_description: String,
-            producline_suffix: String,
-          },
-        },
-        {
-          id: String,
-          type: 'heading',
-          attributes: {
-            goods_nomenclature_item_id: String,
-            description: String,
-            formatted_description: String,
-            producline_suffix: String,
-          },
-        },
-        {
-          id: String,
-          type: 'measure',
-          attributes: {
-            goods_nomenclature_item_id: String,
-            validity_start_date: String,
-            validity_end_date: String,
-          },
-          relationships: {
-            goods_nomenclature: {
-              data: {
-                id: String,
-                type: 'heading',
-              },
-            },
-            geographical_area: {
-              data: {
-                id: String,
-                type: 'geographical_area',
-              },
-            },
-          },
-        },
-      ],
-      meta: {
-        pagination: {
-          page: Integer,
-          per_page: Integer,
-          total_count: Integer,
-        },
-      },
-    }
-  end
 
   before do
     measure_with_goods_nomenclature = create(:measure, :with_base_regulation, goods_nomenclature: create(:heading, :with_description))
@@ -125,19 +23,19 @@ RSpec.describe Api::V2::FootnotesController, type: :controller do
   context 'when searching by the code' do
     let(:query) { { code: footnote.footnote_id } }
 
-    it { expect(do_response.body).to match_json_expression pattern }
+    it_behaves_like 'a successful jsonapi response'
   end
 
   context 'when searching by type' do
     let(:query) { { type: footnote.footnote_type_id } }
 
-    it { expect(do_response.body).to match_json_expression pattern }
+    it_behaves_like 'a successful jsonapi response'
   end
 
   context 'when searching by description' do
     let(:query) { { description: footnote.description } }
 
-    it { expect(do_response.body).to match_json_expression pattern }
+    it_behaves_like 'a successful jsonapi response'
   end
 
   context 'when no footnotes are found' do
