81b63570a04e08c24842f76403fbef33d240a5c4
jebw <jeb@jdwilkins.co.uk>
2023-02-03 12:06:11 +0000

    HOTT-2566 Fetch rule sets alongside schemes

--- app/controllers/api/v2/rules_of_origin/product_specific_rules_controller.rb
+++ app/controllers/api/v2/rules_of_origin/product_specific_rules_controller.rb
@@ -3,15 +3,22 @@ module Api
     module RulesOfOrigin
       class ProductSpecificRulesController < ApiController
         def index
-          schemes = []
+          schemes = TradeTariffBackend.rules_of_origin.scheme_set.all_schemes
+          rule_sets = schemes.index_by(&:scheme_code)
+                             .transform_values(&method(:rule_sets_for_scheme))
 
-          render json: serializer(schemes).serializable_hash
+          presented_schemes = RulesOfOrigin::SchemePresenter.for_many(schemes, {}, rule_sets)
+          render json: serializer(presented_schemes).serializable_hash
         end
 
       private
 
         def serializer(schemes)
           Api::V2::RulesOfOrigin::SchemeSerializer.new(schemes)
+        end
+
+        def rule_sets_for_scheme(scheme)
+          scheme.rule_sets_for_subheading params[:commodity_code]
         end
       end
     end
