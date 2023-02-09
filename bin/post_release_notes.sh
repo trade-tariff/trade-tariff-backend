bf6c0d5c809c9e56125f0d945690824ead793c09
William Fish <william.michael.fish@gmail.com>
2023-02-09 09:57:31 +0000

    Add user emoji

--- bin/post_release_notes.sh
+++ bin/post_release_notes.sh
@@ -1,15 +1,9 @@
-
 #!/bin/bash
 
-# Get the SLACK_WEBHOOK and SLACK_CHANNEL environment variables
 webhook_url="$SLACK_WEBHOOK"
 channel="#$SLACK_CHANNEL"
 username="$SLACK_USERNAME"
-
-# Get the output of the release notes script
 message=$(bash bin/generate_release_notes.sh)
-
-# Define the message payload in JSON format
 payload="{
   \"channel\": \"$channel\",
   \"text\": \"$message\",
@@ -17,5 +11,4 @@ payload="{
   \"mrkdwn\": false
 }"
 
-# Use cURL to send the message to the Slack API
 curl -X POST -H "Content-type: application/json" --data "$payload" "$webhook_url"
