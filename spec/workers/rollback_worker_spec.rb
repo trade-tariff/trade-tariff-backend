80cad72f3166f75a8d740ed0ffd3c79cfdd79dfe
jebw <jeb@jdwilkins.co.uk>
2023-02-23 11:12:14 +0000

    HOTT-2639 Refresh view when doing data rollback

--- spec/workers/rollback_worker_spec.rb
+++ spec/workers/rollback_worker_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe RollbackWorker, type: :worker do
   let(:date) { '01-01-2020' }
 
+  before do
+    allow(GoodsNomenclatures::TreeNode).to receive(:refresh!).and_call_original
+  end
+
   describe '#perform' do
     context 'for all envs' do
       before do
@@ -11,6 +15,12 @@ RSpec.describe RollbackWorker, type: :worker do
         expect(TariffSynchronizer).to receive(:rollback).with(date, keep: false)
         expect(TariffSynchronizer).not_to receive(:rollback_cds)
         described_class.new.perform(date)
+      end
+
+      it 'refreshes materialized view' do
+        described_class.new.perform(date)
+
+        expect(GoodsNomenclatures::TreeNode).to have_received(:refresh!)
       end
     end
 
@@ -23,6 +33,12 @@ RSpec.describe RollbackWorker, type: :worker do
         expect(TariffSynchronizer).to receive(:rollback_cds).with(date, keep: false)
         expect(TariffSynchronizer).not_to receive(:rollback)
         described_class.new.perform(date)
+      end
+
+      it 'refreshes materialized view' do
+        described_class.new.perform(date)
+
+        expect(GoodsNomenclatures::TreeNode).to have_received(:refresh!)
       end
     end
   end
