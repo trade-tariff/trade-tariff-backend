64d9637bff3d89db1872cc6576d9f9913fa09e0e
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-03-02 14:28:36 +0000

    HOTT-2703: Created initialiser

---
+++ config/initializers/chief_cds_guidance_s3.rb
@@ -0,0 +1,10 @@
+s3_bucket_name = ENV['AWS_PERSISTENCE_BUCKET_NAME']
+credentials_loaded = ENV['AWS_PROFILE'].present? || (ENV['AWS_SECRET_ACCESS_KEY'].present? && ENV['AWS_ACCESS_KEY_ID'])
+
+s3_bucket = if Rails.env.test?
+                            Aws::S3::Resource.new(stub_responses: true).bucket(s3_bucket_name)
+                          elsif s3_bucket_name && credentials_loaded
+                            Aws::S3::Resource.new.bucket(s3_bucket_name)
+                          end
+
+Rails.application.config.chief_cds_guidance_bucket = s3_bucket
