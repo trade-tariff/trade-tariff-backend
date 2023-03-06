602e072694ade10bf44288844778e61cee6cbd58
William Fish <william.michael.fish@gmail.com>
2023-03-06 13:40:09 +0000

    HOTT-2703: Consolidate guidance intializer

--- config/initializers/chief_cds_guidance.rb
+++
@@ -1,8 +0,0 @@
-unless Rails.env.test?
-  Rails.application.reloader.to_prepare do
-    # trigger loading at boot
-    require_relative '../../app/lib/trade_tariff_backend'
-
-    TradeTariffBackend.chief_cds_guidance
-  end
-end
