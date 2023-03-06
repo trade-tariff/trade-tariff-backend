9eefd41937d54bc77a20d92e33276ca6b6a3ffa7
William Fish <william.michael.fish@gmail.com>
2023-03-06 13:39:09 +0000

    HOTT-2703: Adds populator service and spec

---
+++ app/services/appendix_5a_populator_service.rb
@@ -0,0 +1,92 @@
+class Appendix5aPopulatorService
+  def call
+    logger.info 'Populating Appendix 5a'
+
+    Appendix5a.unrestrict_primary_key
+    Appendix5a.db.transaction do
+      added_guidance.each(&:save)
+      changed_guidance.each(&:save)
+      removed_guidance.each(&:destroy)
+    end
+    Appendix5a.restrict_primary_key
+
+    notify
+
+    logger.info 'Finished populating Appendix 5a'
+  end
+
+  private
+
+  def removed_guidance
+    @removed_guidance ||= begin
+      removed_document_codes = existing_document_codes - new_guidance.keys
+
+      existing_guidance.select do |guidance|
+        removed_document_codes.include?(guidance.document_code)
+      end
+    end
+  end
+
+  def added_guidance
+    @added_guidance ||= begin
+      added_document_codes = new_guidance.keys - existing_document_codes
+
+      added_document_codes.map do |document_code|
+        guidance = new_guidance[document_code]
+
+        certificate_type_code = document_code[0]
+        certificate_code = document_code[1..]
+        cds_guidance = guidance['guidance_cds']
+        chief_guidance = guidance['guidance_chief']
+
+        Appendix5a.new(
+          certificate_type_code:,
+          certificate_code:,
+          cds_guidance:,
+          chief_guidance:,
+        )
+      end
+    end
+  end
+
+  def changed_guidance
+    @changed_guidance ||= begin
+      changed_document_codes = new_guidance.keys & existing_document_codes
+
+      changed_document_codes.each_with_object([]) do |document_code, acc|
+        existing_guidance.each do |guidance|
+          next unless guidance.document_code == document_code
+
+          guidance.cds_guidance = new_guidance[document_code]['guidance_cds']
+          guidance.chief_guidance = new_guidance[document_code]['guidance_chief']
+
+          if guidance.column_changes.any?
+            acc << guidance
+          end
+        end
+      end
+    end
+  end
+
+  def existing_document_codes
+    @existing_document_codes ||= existing_guidance.map(&:document_code)
+  end
+
+  def new_guidance
+    @new_guidance ||= Appendix5a.fetch_latest
+  end
+
+  def existing_guidance
+    @existing_guidance ||= Appendix5a.all
+  end
+
+  def notify
+    message = "Appendix 5a has been updated with #{added_guidance.count} new, "
+    message += "#{changed_guidance.count} changed and "
+    message += "#{removed_guidance.count} removed guidance documents"
+
+    logger.info message
+
+    SlackNotifierService.call(message)
+  end
+end
---
+++ spec/services/appendix_5a_populator_service_spec.rb
@@ -0,0 +1,111 @@
+RSpec.describe Appendix5aPopulatorService do
+  describe '#call' do
+    subject(:call) { described_class.new.call }
+
+    before do
+      allow(Appendix5a).to receive(:fetch_latest).and_return(new_guidance)
+      allow(SlackNotifierService).to receive(:call).and_call_original
+    end
+
+    let!(:existing_guidance) do
+      create(
+        :appendix_5a,
+        cds_guidance: 'foo',
+        chief_guidance: 'bar',
+      )
+    end
+
+    context 'when the latest guidance has changed' do
+      let(:new_guidance) do
+        {
+          '1123' => {
+            'guidance_cds' => 'foo',
+            'guidance_chief' => 'baz',
+          },
+        }
+      end
+
+      it 'refreshes the guidance' do
+        expect { call }
+          .to change { existing_guidance.reload.chief_guidance }
+          .from('bar')
+          .to('baz')
+      end
+
+      it 'notifies slack' do
+        call
+
+        expect(SlackNotifierService)
+          .to have_received(:call)
+          .with('Appendix 5a has been updated with 0 new, 1 changed and 0 removed guidance documents')
+      end
+    end
+
+    context 'when latest guidance has not changed' do
+      let(:new_guidance) do
+        {
+          '1123' => {
+            'guidance_cds' => 'foo',
+            'guidance_chief' => 'bar',
+          },
+        }
+      end
+
+      it 'does not refresh the guidance' do
+        expect { call }
+          .not_to change { existing_guidance.reload.values }
+      end
+
+      it 'notifies slack' do
+        call
+
+        expect(SlackNotifierService)
+          .to have_received(:call)
+          .with('Appendix 5a has been updated with 0 new, 0 changed and 0 removed guidance documents')
+      end
+    end
+
+    context 'when latest guidance removes old guidance' do
+      let(:new_guidance) { {} }
+
+      it 'removes old guidance' do
+        expect { call }.to change(Appendix5a, :count).by(-1)
+      end
+
+      it 'notifies slack' do
+        call
+
+        expect(SlackNotifierService)
+          .to have_received(:call)
+          .with('Appendix 5a has been updated with 0 new, 0 changed and 1 removed guidance documents')
+      end
+    end
+
+    context 'when latest guidance adds new guidance' do
+      let(:new_guidance) do
+        {
+          '1123' => {
+            'guidance_cds' => 'foo',
+            'guidance_chief' => 'bar',
+          },
+          '2123' => {
+            'guidance_cds' => 'foo',
+            'guidance_chief' => 'bar',
+          },
+        }
+      end
+
+      it 'adds new guidance' do
+        expect { call }.to change(Appendix5a, :count).by(1)
+      end
+
+      it 'notifies slack' do
+        call
+
+        expect(SlackNotifierService)
+          .to have_received(:call)
+          .with('Appendix 5a has been updated with 1 new, 0 changed and 0 removed guidance documents')
+      end
+    end
+  end
+end
