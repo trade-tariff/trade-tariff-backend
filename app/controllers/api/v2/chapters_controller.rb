3eb386f738887c73499a4547b4c5e012ce40a6b4
William Fish <william.michael.fish@gmail.com>
2023-03-15 17:03:24 +0000

    Revert "Fragment cache chapters"
    This reverts commit e59028f6078d304d648a1812933a1cc6209a9fe8.

--- app/controllers/api/v2/chapters_controller.rb
+++ app/controllers/api/v2/chapters_controller.rb
@@ -3,7 +3,6 @@ require 'goods_nomenclature_mapper'
 module Api
   module V2
     class ChaptersController < ApiController
-      CACHE_VERSION = 1
       before_action :find_chapter, only: %i[show changes headings]
 
       def index
@@ -22,20 +21,15 @@ module Api
       end
 
       def show
-        chapter_id = params[:id]
-        cache_key = "_chapter-v#{CACHE_VERSION}-#{chapter_id}-#{actual_date}"
+        root_headings = GoodsNomenclatureMapper.new(chapter.headings_dataset
+                                                      .eager(:goods_nomenclature_descriptions,
+                                                             :goods_nomenclature_indents)
+                                                      .all).root_entries
 
-        Rails.cache.fetch(cache_key, expires_at: actual_date.end_of_day) do
-          root_headings = GoodsNomenclatureMapper.new(chapter.headings_dataset
-            .eager(:goods_nomenclature_descriptions,
-                   :goods_nomenclature_indents)
-            .all).root_entries
-
-          options = { is_collection: false }
-          options[:include] = [:section, :guides, :headings, 'headings.children']
-          presenter = Api::V2::Chapters::ChapterPresenter.new(chapter, root_headings)
-          render json: Api::V2::Chapters::ChapterSerializer.new(presenter, options).serializable_hash
-        end
+        options = { is_collection: false }
+        options[:include] = [:section, :guides, :headings, 'headings.children']
+        presenter = Api::V2::Chapters::ChapterPresenter.new(chapter, root_headings)
+        render json: Api::V2::Chapters::ChapterSerializer.new(presenter, options).serializable_hash
       end
 
       def changes
@@ -82,8 +76,6 @@ module Api
       def chapter_id
         "#{params[:id]}00000000"
       end
-
-      def chapter_cache_key; end
     end
   end
 end
