58162592c5b7069e7501d83ff1ff17a6547dd9ec
William Fish <william.michael.fish@gmail.com>
2023-03-20 14:09:02 +0000

    HOTT-2897: Code review

--- db/data_migrations/20230320121959_drop_broken_memberships.rb
+++ db/data_migrations/20230320121959_drop_broken_memberships.rb
@@ -1,20 +1,22 @@
 Sequel.migration do
   up do
-    memberships = [
-      {
-        geographical_area_sid: 186,
-        geographical_area_group_sid: 520,
-        filename: 'tariff_dailyExtract_v1_20230224T235959.gzip',
-      },
-      {
-        geographical_area_sid: 248,
-        geographical_area_group_sid: 520,
-        filename: 'tariff_dailyExtract_v1_20230224T235959.gzip',
-      },
-    ]
+    if TradeTariffBackend.uk?
+      memberships = [
+        {
+          geographical_area_sid: 186,
+          geographical_area_group_sid: 520,
+          filename: 'tariff_dailyExtract_v1_20230224T235959.gzip',
+        },
+        {
+          geographical_area_sid: 248,
+          geographical_area_group_sid: 520,
+          filename: 'tariff_dailyExtract_v1_20230224T235959.gzip',
+        },
+      ]
 
-    memberships.each do |membership|
-      GeographicalAreaMembership.find(membership).destroy
+      memberships.each do |membership|
+        GeographicalAreaMembership.find(membership)&.destroy
+      end
     end
   end
 
