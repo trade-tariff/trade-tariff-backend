c72782afead1e495e681aee6e639e9d5694046b8
jebw <jeb@jdwilkins.co.uk>
2023-03-31 17:24:26 +0100

    HOTT-2643 Eager load heading data when using nested set

--- app/services/heading_service/serialization/ns_nondeclarable_service.rb
+++ app/services/heading_service/serialization/ns_nondeclarable_service.rb
@@ -16,6 +16,42 @@ module HeadingService
         ],
       }.freeze
 
+      MEASURES_EAGER_LOAD = {
+        measure_components: {
+          duty_expression: :duty_expression_description,
+          measurement_unit: %i[measurement_unit_description
+                               measurement_unit_abbreviations],
+          monetary_unit: :monetary_unit_description,
+          measurement_unit_qualifier: [],
+        },
+        measure_type: %i[measure_type_description
+                         measure_type_series
+                         measure_type_series_description],
+        additional_code: [],
+      }.freeze
+
+      HEADING_EAGER_LOAD = [
+        :goods_nomenclature_descriptions,
+        {
+          footnotes: :footnote_descriptions,
+          ns_overview_measures: MEASURES_EAGER_LOAD,
+          ns_ancestors: [
+            :goods_nomenclature_descriptions,
+            {
+              ns_overview_measures: MEASURES_EAGER_LOAD,
+              footnotes: :footnote_descriptions,
+            },
+          ],
+          ns_descendants: [
+            :goods_nomenclature_descriptions,
+            {
+              ns_overview_measures: MEASURES_EAGER_LOAD,
+              footnotes: :footnote_descriptions,
+            },
+          ],
+        },
+      ].freeze
+
       attr_reader :heading
 
       delegate :serializable_hash, to: :serializer
@@ -27,12 +63,22 @@ module HeadingService
     private
 
       def presented_heading
-        Api::V2::Headings::HeadingPresenter.new(heading)
+        Api::V2::Headings::HeadingPresenter.new(eager_loaded_heading)
       end
 
       def serializer
         Api::V2::Headings::HeadingSerializer
           .new(presented_heading, OPTIONS)
+      end
+
+      def eager_loaded_heading
+        Heading.actual
+               .non_hidden
+               .where(goods_nomenclature_sid: heading.goods_nomenclature_sid)
+               .eager(*HEADING_EAGER_LOAD)
+               .limit(1)
+               .all
+               .first
       end
     end
   end
