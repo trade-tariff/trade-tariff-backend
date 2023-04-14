5aeae092807a837e477840a533dd657bbffae5e3
jebw <jeb@jdwilkins.co.uk>
2023-04-14 13:44:32 +0100

    HOTT-2943 Add fields to Goods Nomenclature API

---
+++ spec/serializers/api/v2/goods_nomenclatures/goods_nomenclature_extended_serializer_spec.rb
@@ -0,0 +1,67 @@
+RSpec.describe Api::V2::GoodsNomenclatures::GoodsNomenclatureExtendedSerializer do
+  describe '#serializable_hash' do
+    subject(:serializable) { described_class.new(gn).serializable_hash[:data] }
+
+    let(:gn) do
+      create :commodity,
+             :with_description,
+             validity_start_date: 2.days.ago.beginning_of_day,
+             validity_end_date: 2.days.from_now.end_of_day
+    end
+
+    it { is_expected.to include type: :goods_nomenclature }
+    it { is_expected.to include id: gn.goods_nomenclature_sid.to_s }
+
+    describe 'attributes' do
+      subject { serializable[:attributes] }
+
+      let :attribute_keys do
+        %i[
+          goods_nomenclature_item_id
+          goods_nomenclature_sid
+          producline_suffix
+          description
+          number_indents
+          href
+          formatted_description
+          validity_start_date
+          validity_end_date
+          declarable
+        ]
+      end
+
+      it { is_expected.to have_attributes keys: attribute_keys }
+      it { is_expected.to include goods_nomenclature_item_id: gn.goods_nomenclature_item_id }
+      it { is_expected.to include goods_nomenclature_sid: gn.goods_nomenclature_sid }
+      it { is_expected.to include producline_suffix: gn.producline_suffix }
+      it { is_expected.to include description: gn.description }
+      it { is_expected.to include number_indents: gn.number_indents }
+      it { is_expected.to include href: "/api/v2/commodities/#{gn.goods_nomenclature_item_id}" }
+      it { is_expected.to include formatted_description: gn.formatted_description }
+      it { is_expected.to include validity_start_date: gn.validity_start_date }
+      it { is_expected.to include validity_end_date: gn.validity_end_date }
+      it { is_expected.to include declarable: true }
+
+      context 'with heading' do
+        let(:gn) { create :heading, :with_children }
+
+        it { is_expected.to include href: "/api/v2/headings/#{gn.short_code}" }
+        it { is_expected.to include declarable: false }
+      end
+
+      context 'with declarable heading' do
+        let(:gn) { create :heading }
+
+        it { is_expected.to include href: "/api/v2/headings/#{gn.short_code}" }
+        it { is_expected.to include declarable: true }
+      end
+
+      context 'with subheading' do
+        let(:gn) { create :subheading }
+
+        it { is_expected.to include href: "/api/v2/commodities/#{gn.goods_nomenclature_item_id}" }
+        it { is_expected.to include declarable: false }
+      end
+    end
+  end
+end
---
+++ spec/serializers/api/v2/goods_nomenclatures/goods_nomenclature_list_serializer_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe Api::V2::GoodsNomenclatures::GoodsNomenclatureListSerializer do
+  describe '#serializable_hash' do
+    subject(:serializable) { described_class.new(gn).serializable_hash[:data] }
+
+    let(:gn) { build :commodity }
+
+    it { is_expected.to include type: :goods_nomenclature }
+    it { is_expected.to include id: gn.goods_nomenclature_sid.to_s }
+
+    describe 'attributes' do
+      subject { serializable[:attributes] }
+
+      let :attribute_keys do
+        %i[
+          goods_nomenclature_item_id
+          goods_nomenclature_sid
+          producline_suffix
+          description
+          number_indents
+          href
+        ]
+      end
+
+      it { is_expected.to have_attributes keys: attribute_keys }
+      it { is_expected.to include goods_nomenclature_item_id: gn.goods_nomenclature_item_id }
+      it { is_expected.to include goods_nomenclature_sid: gn.goods_nomenclature_sid }
+      it { is_expected.to include producline_suffix: gn.producline_suffix }
+      it { is_expected.to include description: gn.description }
+      it { is_expected.to include number_indents: gn.number_indents }
+      it { is_expected.to include href: "/api/v2/commodities/#{gn.goods_nomenclature_item_id}" }
+    end
+  end
+end
