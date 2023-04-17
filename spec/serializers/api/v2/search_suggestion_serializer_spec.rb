aef00119ac7482349efb646787a429f2b14d43a0
William Fish <william.michael.fish@gmail.com>
2023-04-17 11:18:18 +0100

    HOTT-3031: Fixes broken specs

--- spec/serializers/api/v2/search_suggestion_serializer_spec.rb
+++ spec/serializers/api/v2/search_suggestion_serializer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Api::V2::SearchSuggestionSerializer do
   let(:search_suggestion) do
     create(:search_suggestion, value: 'aluminium wire')
 
-    SearchSuggestion.fuzzy_search('aluminum').first
+    SearchSuggestion.fuzzy_search('alu').first
   end
 
   it 'returns search suggestions' do
@@ -15,8 +15,8 @@ RSpec.describe Api::V2::SearchSuggestionSerializer do
           type: eq(:search_suggestion),
           attributes: {
             value: 'aluminium wire',
-            score: be_within(0.2).of(0.411),
-            query: 'aluminum',
+            score: be_within(0.2).of(0.18),
+            query: 'alu',
           },
         },
       },
