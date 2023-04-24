57b8a6e80bcb1340626fe09226bb1700804a8980
jebw <jeb@jdwilkins.co.uk>
2023-04-24 17:34:53 +0100

    HOTT-3064 Convert commodities CSV to use nested set

--- spec/serializers/api/v2/csv/commodity_serializer_spec.rb
+++ spec/serializers/api/v2/csv/commodity_serializer_spec.rb
@@ -1,31 +1,38 @@
 RSpec.describe Api::V2::Csv::CommoditySerializer do
   describe '#serializable_array' do
-    subject(:serializable_array) { described_class.new(serializable).serializable_array }
+    subject(:serializable) { described_class.new([commodity]).serializable_array }
 
-    let(:serializable) do
-      commodity = Hashie::TariffMash.new(
-        description: 'foo',
-        number_indents: 1,
-        goods_nomenclature_item_id: '0702000007',
-        declarable: true,
-        leaf: true,
-        goods_nomenclature_sid: 123,
-        formatted_description: "<span title='foo'>foo</span>",
-        description_plain: 'foo',
-        producline_suffix: 80,
-        parent_sid: 122,
-      )
+    let(:parent) { create :commodity, :with_description, :with_heading }
+    let(:commodity) { create :commodity, parent: }
 
-      [commodity]
+    it 'includes header row' do
+      expect(serializable[0]).to eq %i[
+        description
+        number_indents
+        goods_nomenclature_item_id
+        declarable
+        leaf
+        goods_nomenclature_sid
+        formatted_description
+        description_plain
+        producline_suffix
+        parent_sid
+      ]
     end
 
     it 'serializes correctly' do
-      expect(serializable_array).to eq(
-        [
-          %i[description number_indents goods_nomenclature_item_id declarable leaf goods_nomenclature_sid formatted_description description_plain producline_suffix parent_sid],
-          ['foo', 1, '0702000007', true, true, 123, "<span title='foo'>foo</span>", 'foo', 80, 122],
-        ],
-      )
+      expect(serializable[1]).to eq [
+        commodity.description,
+        commodity.number_indents,
+        commodity.goods_nomenclature_item_id,
+        true,
+        true,
+        commodity.goods_nomenclature_sid,
+        commodity.description,
+        commodity.description,
+        commodity.producline_suffix,
+        parent.goods_nomenclature_sid,
+      ]
     end
   end
 end
