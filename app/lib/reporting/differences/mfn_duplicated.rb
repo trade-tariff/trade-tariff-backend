57f78366af329e891f04ec84ff78f79dc3eb4524
William Fish <william.michael.fish@gmail.com>
2023-06-05 15:54:06 +0100

    HOTT-3259: Adds duplicated mfn worksheet

--- app/lib/reporting/differences/mfn_duplicated.rb
+++ app/lib/reporting/differences/mfn_duplicated.rb
@@ -1,6 +1,6 @@
 module Reporting
   class Differences
-    class MfnMissing
+    class MfnDuplicated
       delegate :workbook,
                :regular_style,
                :bold_style,
@@ -9,7 +9,15 @@ module Reporting
 
       HEADER_ROW = [
         'Commodity code',
-        'Description',
+        'Headline',
+        'MFN 1 - SID',
+        'MFN 1 - Add code',
+        'MFN 1 - Duty',
+        'MFN 1 - Measure type',
+        'MFN 2 - SID',
+        'MFN 2 - Add code',
+        'MFN 2 - Duty',
+        'MFN 2 - Measure type',
       ].freeze
 
       TAB_COLOR = '00ff00'.freeze
@@ -17,11 +25,19 @@ module Reporting
       CELL_TYPES = Array.new(HEADER_ROW.size, :string).freeze
 
       COLUMN_WIDTHS = [
-        25, # Commodity code (PLS)
-        80, # Description
+        25, # Commodity code
+        40, # Headline
+        20, # MFN 1 - SID
+        20, # MFN 1 - Add code
+        20, # MFN 1 - Duty
+        20, # MFN 1 - Measure type
+        20, # MFN 2 - SID
+        20, # MFN 2 - Add code
+        20, # MFN 2 - Duty
+        20, # MFN 2 - Measure type
       ].freeze
 
-      AUTOFILTER_CELL_RANGE = 'A1:B1'.freeze
+      AUTOFILTER_CELL_RANGE = 'A1:J1'.freeze
       FROZEN_VIEW_STARTING_CELL = 'A2'.freeze
 
       def initialize(name, report)
@@ -67,23 +83,38 @@ module Reporting
       end
 
       def each_declarable
-        each_chapter(eager: Differences::GOODS_NOMENCLATURE_OVERVIEW_MEASURE_EAGER) do |eager_chapter|
+        each_chapter(eager: Differences::GOODS_NOMENCLATURE_OVERVIEW_MEASURE_WITH_COMPONENTS_EAGER) do |eager_chapter|
           eager_chapter.ns_descendants.each do |chapter_descendant|
             next unless chapter_descendant.ns_declarable?
 
-            next if chapter_descendant.applicable_overview_measures.any? do |measure|
+            mfns = chapter_descendant.applicable_overview_measures.select do |measure|
               measure.measure_type_id.in?(MeasureType::THIRD_COUNTRY)
             end
 
-            yield chapter_descendant
+            yield chapter_descendant if mfns.size == 2
           end
         end
       end
 
       def build_row_for(declarable)
+        mfn_measures = declarable.applicable_overview_measures.select do |measure|
+          measure.measure_type_id.in?(MeasureType::THIRD_COUNTRY)
+        end
+
+        mfn_measure_1 = mfn_measures.first
+        mfn_measure_2 = mfn_measures.second
+
         [
           declarable.goods_nomenclature_item_id,
           declarable.description,
+          mfn_measure_1.measure_sid,
+          "#{mfn_measure_1.additional_code_type_id}#{mfn_measure_1.additional_code_id}",
+          mfn_measure_1.duty_expression,
+          mfn_measure_1.measure_type_id,
+          mfn_measure_2.measure_sid,
+          "#{mfn_measure_2.additional_code_type_id}#{mfn_measure_2.additional_code_id}",
+          mfn_measure_2.duty_expression,
+          mfn_measure_2.measure_type_id,
         ]
       end
     end
