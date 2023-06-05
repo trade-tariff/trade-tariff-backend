4d9e14c9368d3ecb559e4afce38f742e55aeda69
William Fish <william.michael.fish@gmail.com>
2023-06-05 16:06:07 +0100

    HOTT-3254: Adds hiearchy worksheet

---
+++ app/lib/reporting/differences/hierarchy.rb
@@ -0,0 +1,102 @@
+module Reporting
+  class Differences
+    class Hierarchy
+      delegate :workbook,
+               :regular_style,
+               :bold_style,
+               :centered_style,
+               :uk_goods_nomenclatures,
+               :xi_goods_nomenclatures,
+               to: :report
+
+      HEADER_ROW = [
+        'Commodity code (PLS)',
+        'UK hiearchy',
+        'EU hierarchy',
+      ].freeze
+
+      TAB_COLOR = 'cc0000'.freeze
+
+      CELL_TYPES = Array.new(HEADER_ROW.size, :string).freeze
+
+      COLUMN_WIDTHS = [
+        20, # Commodity code (PLS)
+        20, # UK hierarchy
+        20, # EU hierarchy
+      ].freeze
+
+      AUTOFILTER_CELL_RANGE = 'A1:C1'.freeze
+      FROZEN_VIEW_STARTING_CELL = 'A2'.freeze
+
+      def initialize(name, report)
+        @name = name
+        @report = report
+      end
+
+      def add_worksheet
+        workbook.add_worksheet(name:) do |sheet|
+          sheet.sheet_pr.tab_color = TAB_COLOR
+          sheet.add_row(HEADER_ROW, style: bold_style)
+          sheet.auto_filter = AUTOFILTER_CELL_RANGE
+          sheet.sheet_view.pane do |pane|
+            pane.top_left_cell = FROZEN_VIEW_STARTING_CELL
+            pane.state = :frozen
+            pane.y_split = 1
+          end
+
+          rows.compact.each do |row|
+            sheet.add_row(row, types: CELL_TYPES, style: regular_style)
+            sheet.rows.last.tap do |last_row|
+              last_row.cells[1].style = centered_style # UK endline status
+              last_row.cells[2].style = centered_style # EU endline status
+            end
+          end
+
+          sheet.column_widths(*COLUMN_WIDTHS)
+        end
+      end
+
+      private
+
+      attr_reader :name, :report
+
+      def rows
+        matching_goods_nomenclature = uk_goods_nomenclature_ids.keys & xi_goods_nomenclature_ids.keys
+
+        matching_goods_nomenclature.each_with_object([]) do |matching, acc|
+          row = build_row_for(matching)
+          acc << row unless row.nil?
+        end
+      end
+
+      def build_row_for(matching)
+        matching_uk_goods_nomenclature = uk_goods_nomenclature_ids[matching]
+        matching_xi_goods_nomenclature = xi_goods_nomenclature_ids[matching]
+
+        matching = matching_uk_goods_nomenclature['Hierarchy'] == matching_xi_goods_nomenclature['Hierarchy']
+
+        return nil if matching
+
+        item_id, pls = matching_uk_goods_nomenclature['ItemIDPlusPLS'].split('_')
+
+        [
+          "#{item_id} (#{pls})",
+          matching_uk_goods_nomenclature['Hierarchy'],
+          matching_xi_goods_nomenclature['Hierarchy'],
+        ]
+      end
+
+      def uk_goods_nomenclature_ids
+        @uk_goods_nomenclature_ids ||= uk_goods_nomenclatures.index_by do |goods_nomenclature|
+          goods_nomenclature['ItemIDPlusPLS']
+        end
+      end
+
+      def xi_goods_nomenclature_ids
+        @xi_goods_nomenclature_ids ||= xi_goods_nomenclatures.index_by do |goods_nomenclature|
+          goods_nomenclature['ItemIDPlusPLS']
+        end
+      end
+    end
+  end
+end
