445df81b1ccc9f29f0a1d9b8236c66fa870931a1
William Fish <william.michael.fish@gmail.com>
2023-06-16 09:22:59 +0100

    HOTT-3372: Reoves compression

--- test.sh
+++ test.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # POST request to enqueue a job and extract the job ID
-job_id=$(curl --compressed -s -X POST "http://localhost:3000/bulk_searches" -d @body.json -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Accept-Encoding: gzip' | jq -r '.data.id')
+job_id=$(curl -s -X POST "http://localhost:3000/bulk_searches" -d @body.json -H 'Content-Type: application/json' -H 'Accept: application/json' | jq -r '.data.id')
 
 echo "Job ID: $job_id"
 # Continually poll the status of the job until it's completed
 while true; do
     # GET request to check the status of the job
-    response=$(curl --compressed -s "http://localhost:3000/bulk_searches/${job_id}" -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Accept-Encoding: gzip')
-
+    response=$(curl -s "http://localhost:3000/bulk_searches/${job_id}" -H 'Content-Type: application/json' -H 'Accept: application/json' -w "\n%{http_code}\n")
+    body=$(echo "$response" | head -n -1)
+    status=$(echo "$response" | tail -n 1)
+    job_status=$(echo $body | jq -r '.data.attributes.status')
     # Extract the status from the response
-    status=$(echo "$response" | jq -r '.data.attributes.status')
-
-    echo "Status: $status"
+    echo "Status: $job_status ($status)"
 
     # Check if the status is "complete" or "error or "not_found"
-    if [ "$status" = "completed" ] || [ "$status" = "error" ] || [ "$status" = "not_found" ]; then
+    if [ "$status" = "200" ]; then
         # Output the completed job's response and break the loop
-        echo "$response" | jq
+        echo "$body" | jq
         break
     else
         # Sleep for a while before checking again
