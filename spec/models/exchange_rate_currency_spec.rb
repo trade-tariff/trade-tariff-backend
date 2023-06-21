168d02c5710f0a573da7267e6fc36644d24b7b3e
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-06-21 15:13:09 +0100

    HOTT-3384: Improve tests

--- spec/models/exchange_rate_currency_spec.rb
+++ spec/models/exchange_rate_currency_spec.rb
@@ -2,30 +2,24 @@ require 'csv'
 
 RSpec.describe ExchangeRateCurrency do
   let(:csv_file) { 'spec/fixtures/currency.csv' }
+  let(:aed) { described_class.where(currency_code: 'AED').take }
+  let(:aud) { described_class.where(currency_code: 'AUD').take }
 
   before do
     stub_const('ExchangeRateCurrency::CURRENCY_FILE', Rails.root.join('spec/fixtures/currency.csv'))
   end
 
   describe '.populate' do
-    it 'populates ExchangeRateCurrency records from the CSV file' do
-      expect(described_class).to receive(:unrestrict_primary_key)
+    before { described_class.populate }
 
-      expect(described_class).to receive(:create).with(
-        currency_code: 'AED',
-        currency_description: 'Dirham',
-        spot_rate_required: false,
-      )
+    it { expect(described_class.count).to eq(2) }
 
-      expect(described_class).to receive(:create).with(
-        currency_code: 'AUD',
-        currency_description: 'Dollar',
-        spot_rate_required: true,
-      )
+    it { expect(aed.currency_description).to eq('Dirham') }
 
-      expect(described_class).to receive(:restrict_primary_key)
+    it { expect(aed.spot_rate_required).to eq(false) }
 
-      described_class.populate
-    end
+    it { expect(aud.currency_description).to eq('Dollar') }
+
+    it { expect(aud.spot_rate_required).to eq(true) }
   end
 end
