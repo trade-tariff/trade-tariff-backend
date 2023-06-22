dce4f47aa0e21c8422c953a58874d05120d0d31d
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-06-22 14:22:12 +0100

    HOTT-3381: Created data structures

---
+++ db/migrate/20230619133359_create_exchange_rate_currency_rates.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table(:exchange_rate_currency_rates) do
+      primary_key :id
+      String :currency_code, size: 10, null: false
+      Date :validity_start_date
+      Date :validity_end_date
+      Float :rate
+      String :rate_type, size: 10
+
+      index :currency_code
+      index :validity_start_date
+      index :validity_end_date
+      index [:currency_code, :validity_start_date, :validity_end_date], unique: true
+    end
+  end
+end
