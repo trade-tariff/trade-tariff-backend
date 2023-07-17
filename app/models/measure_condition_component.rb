466479ab525565add6730ab491b7b3cd95042c48
William Fish <william.michael.fish@gmail.com>
2023-07-17 10:51:12 +0100

    HOTT-3641: Adds more standardisation for componentables

--- app/models/measure_condition_component.rb
+++ app/models/measure_condition_component.rb
@@ -4,23 +4,8 @@ class MeasureConditionComponent < Sequel::Model
 
   set_primary_key %i[measure_condition_sid duty_expression_id]
 
-  one_to_one :measure_condition, key: :measure_condition_sid,
-                                 primary_key: :measure_condition_sid
-
   include Componentable
 
-  def formatted_duty_expression
-    DutyExpressionFormatter.format(
-      duty_expression_id:,
-      duty_expression_description:,
-      duty_expression_abbreviation:,
-      duty_amount:,
-      monetary_unit: monetary_unit_code,
-      monetary_unit_abbreviation:,
-      measurement_unit:,
-      measurement_unit_qualifier:,
-      currency: TradeTariffBackend.currency,
-      formatted: true,
-    )
-  end
+  one_to_one :measure_condition, key: :measure_condition_sid,
+                                 primary_key: :measure_condition_sid
 end
