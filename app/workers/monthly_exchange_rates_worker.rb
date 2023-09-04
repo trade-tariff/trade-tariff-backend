8c373d4d64afc79a2ef41302b5532026cb44fb29
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-09-04 10:06:28 +0100

    HOTT-3892: Retry job later if XE connection fails

--- app/workers/monthly_exchange_rates_worker.rb
+++ app/workers/monthly_exchange_rates_worker.rb
@@ -1,7 +1,7 @@
 class MonthlyExchangeRatesWorker
   include Sidekiq::Worker
 
-  sidekiq_options retry: false
+  sidekiq_options retry: 1, retry_in: 1.hour
 
   def perform
     ExchangeRates::MonthlyExchangeRatesService.call
