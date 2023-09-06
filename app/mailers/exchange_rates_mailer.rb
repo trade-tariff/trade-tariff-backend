c0aff3b4dc2aac8d0e5dad59f00bd60f487df28c
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2023-09-06 16:44:06 +0100

    HOTT-3947: Lint

--- app/mailers/exchange_rates_mailer.rb
+++ app/mailers/exchange_rates_mailer.rb
@@ -5,7 +5,7 @@ class ExchangeRatesMailer < ApplicationMailer
 
   attr_reader :date
 
-  def monthly_files(date=Time.zone.today)
+  def monthly_files(date = Time.zone.today)
     @date = date
     @month_and_year = date.next_month.strftime('%B %Y')
     @csv_hmrc = ExchangeRateFile.where(type: 'monthly_csv_hmrc', period_month: month, period_year: year).take
