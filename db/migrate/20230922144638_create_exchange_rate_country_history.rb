8cd20c97352c7b0cfc8c47f1fe5ada4690f7ce4c
William Fish <william.michael.fish@gmail.com>
2023-10-04 13:20:20 +0100

    HOTT-4022: Exchange rate currency/country table tweaks

--- db/migrate/20230922144638_create_exchange_rate_country_history.rb
+++ db/migrate/20230922144638_create_exchange_rate_country_history.rb
@@ -1,18 +1,29 @@
-# frozen_string_literal: true
-
 Sequel.migration do
-  change do
-    create_table :exchange_rate_country_histories do
+  up do
+    create_table :exchange_rate_countries_currencies do
       primary_key :id
-      String :country, size: 200, unique: true, null: false
-      String :country_code, size: 10, unique: true, null: false
-      String :currency_code, size: 10, null: false
-      String :currency_description, size: 200, null: false
-      Date :start_date, null: false
-      Date :end_date
+      String :country_code, null: false # The ISO 3166-1 alpha-2 code of the country
+      String :country_description, null: false # The description of the country
+      String :currency_code, null: false # The ISO 4217 currency code for the country
+      String :currency_description, null: false # The description of the currency
+      Date :validity_start_date, null: false # The date the currency became valid for the country
+      Date :validity_end_date # The date the currency no longer applies for the country (a nil value here means the descriptions for the country and currency are still valid)
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
 
-      index :country, unique: true
-      index %i[country country_code], unique: true
+      index :currency_code # We join on this field
+      index :validity_start_date # We filter on this field
+      index :validity_end_date # We filter on this field
+      index %i[
+        currency_code
+        country_code
+        validity_start_date
+        validity_end_date
+      ], unique: true # We upsert on conflicts of these fields
     end
+  end
+
+  down do
+    drop_table :exchange_rate_countries_currencies
   end
 end
