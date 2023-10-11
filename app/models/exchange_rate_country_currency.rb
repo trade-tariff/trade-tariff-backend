40639bf98a0445ab3763161ffb2f19c497a2c272
William Fish <william.michael.fish@gmail.com>
2023-10-11 15:31:56 +0100

    BAU: Adds service to populate historical rates

--- app/models/exchange_rate_country_currency.rb
+++ app/models/exchange_rate_country_currency.rb
@@ -1,3 +1,24 @@
 class ExchangeRateCountryCurrency < Sequel::Model(:exchange_rate_countries_currencies)
   plugin :timestamps, update_on_create: true
+  plugin :auto_validations, not_null: :presence
+  plugin :dirty
+
+  def validate
+    super
+
+    validates_presence :country_description
+    validates_presence :currency_description
+    validates_presence :country_code
+    validates_presence :currency_code
+    validates_presence :validity_start_date
+  end
+
+  dataset_module do
+    def between(period_start_date, period_end_date)
+      where do |_query|
+        (Sequel[:exchange_rate_countries_currencies][:validity_start_date] <= period_start_date) &
+          (Sequel.function(:COALESCE, Sequel[:exchange_rate_countries_currencies][:validity_end_date], Sequel.lit("'infinity'")) >= period_end_date)
+      end
+    end
+  end
 end
