72a372f3d5d373ce2297c398650000ddcc5de002
William Fish <william.michael.fish@gmail.com>
2023-10-12 16:01:34 +0100

    BAU: Removes old exchange rates services

--- app/models/exchange_rate_country_currency.rb
+++ app/models/exchange_rate_country_currency.rb
@@ -1,7 +1,6 @@
 class ExchangeRateCountryCurrency < Sequel::Model(:exchange_rate_countries_currencies)
   plugin :timestamps, update_on_create: true
   plugin :auto_validations, not_null: :presence
-  plugin :dirty
 
   def validate
     super
@@ -11,14 +10,5 @@ class ExchangeRateCountryCurrency < Sequel::Model(:exchange_rate_countries_curre
     validates_presence :country_code
     validates_presence :currency_code
     validates_presence :validity_start_date
-  end
-
-  dataset_module do
-    def between(period_start_date, period_end_date)
-      where do |_query|
-        (Sequel[:exchange_rate_countries_currencies][:validity_start_date] <= period_start_date) &
-          (Sequel.function(:COALESCE, Sequel[:exchange_rate_countries_currencies][:validity_end_date], Sequel.lit("'infinity'")) >= period_end_date)
-      end
-    end
   end
 end
