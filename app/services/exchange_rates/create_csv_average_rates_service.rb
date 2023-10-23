14fab5377d5035e9c97dfcf9c52b2dd91df0750b
Sam Johnson <sh.johnson@me.com>
2023-10-23 18:01:08 +0100

    create avg rates file

---
+++ app/services/exchange_rates/create_csv_average_rates_service.rb
@@ -0,0 +1,45 @@
+module ExchangeRates
+  class CreateCsvAverageRatesService
+    HEADINGS = [
+      'Country',
+      'Unit Of Currency',
+      'Currency Code',
+      'Sterling value of Currency Unit £',
+      'Currency Units per £1',
+    ].freeze
+
+    def self.call(data)
+      new(data).call
+    end
+
+    def initialize(data)
+      @data = data
+    end
+
+    def call
+      CSV.generate do |csv|
+        csv << HEADINGS
+
+        @data.each do |rate|
+          csv << build_row(rate)
+        end
+      end
+    end
+
+    private
+
+    def build_row(rate)
+      [
+        rate.country_description,
+        rate.currency_description,
+        rate.currency_code,
+        sprintf('%.4f', 1 / rate.rate),
+        rate.presented_rate,
+      ]
+    end
+
+    def format_date(date)
+      date.strftime('%d/%b/%Y')
+    end
+  end
+end
