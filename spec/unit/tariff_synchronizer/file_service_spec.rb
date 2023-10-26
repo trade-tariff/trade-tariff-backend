f23a62bfc735a20b034507f0e922e50dea9a4ea9
Sam Johnson <sh.johnson@me.com>
2023-10-26 16:46:01 +0100

    fix file_service spec file offences

--- spec/unit/tariff_synchronizer/file_service_spec.rb
+++ spec/unit/tariff_synchronizer/file_service_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe TariffSynchronizer::FileService do
   let(:base_update) { create :base_update }
 
-  context 'in development' do
+  context 'when development' do
     describe '.write_file' do
       it 'Saves the file in the local filesystem', :aggregate_failures do
         FakeFS do
@@ -43,54 +43,56 @@ RSpec.describe TariffSynchronizer::FileService do
     end
   end
 
-  context 'in production' do
+  context 'when production' do
+    let(:aws_resource) { instance_double('Aws::S3::Resource') }
+    let(:aws_bucket) { instance_double('Aws::S3::Bucket') }
+    let(:aws_object) { instance_double('Aws::S3::Object') }
+
     before do
       string_inquirer = ActiveSupport::StringInquirer.new('production')
-      allow(Rails).to receive(:env).and_return(string_inquirer)
 
-      aws_resource = instance_double('Aws::S3::Resource')
-      @aws_bucket = instance_double('Aws::S3::Bucket')
-      @aws_object = instance_double('Aws::S3::Object')
-      expect(Aws::S3::Resource).to receive(:new).and_return(aws_resource)
-      expect(aws_resource).to receive(:bucket).with('trade-tariff-backend').and_return(@aws_bucket)
+      allow(Rails).to receive(:env).and_return(string_inquirer)
+      allow(Aws::S3::Resource).to receive(:new).and_return(aws_resource)
+      allow(aws_resource).to receive(:bucket).with('trade-tariff-backend').and_return(aws_bucket)
     end
 
     describe '.write_file' do
       it 'Saves the file to a S3 bucket in if is the production environment', :aggregate_failures do
-        expect(@aws_bucket).to receive(:object).with('data/some-file.txt').and_return(@aws_object)
-        expect(@aws_object).to receive(:put).with(body: 'Hello World')
-
         described_class.write_file('data/some-file.txt', 'Hello World')
+
+        expect(aws_bucket).to have_received(:object).with('data/some-file.txt').and_return(aws_object)
+        expect(aws_object).to have_received(:put).with(body: 'Hello World')
       end
     end
 
     describe '.file_exists?' do
       it 'Saves the file to a S3 bucket in if is the production environment', :aggregate_failures do
-        expect(@aws_bucket).to receive(:object).with('data/some-file.txt').and_return(@aws_object)
-        expect(@aws_object).to receive(:exists?)
-
         described_class.file_exists?('data/some-file.txt')
+
+        expect(aws_bucket).to have_receive(:object).with('data/some-file.txt').and_return(aws_object)
+        expect(aws_object).to have_receive(:exists?)
       end
     end
 
     describe '.file_size' do
       it 'returns the file size in the local filesystem', :aggregate_failures do
-        expect(@aws_bucket).to receive(:object).with('data/some-file.txt').and_return(@aws_object)
-        expect(@aws_object).to receive(:size)
-
         described_class.file_size('data/some-file.txt')
+
+        expect(aws_bucket).to have_received(:object).with('data/some-file.txt').and_return(aws_object)
+        expect(aws_object).to have_received(:size)
       end
     end
 
     describe '.file_as_stringio' do
       it 'calls amazon s3 to get the object with the same file_path and returns a string io', :aggregate_failures do
         allow(base_update).to receive(:file_path).and_return('data/some-file.txt')
+
         aws_object_output = instance_double('Aws::S3::Types::GetObjectOutput')
-        expect(@aws_bucket).to receive(:object).with('data/some-file.txt').and_return(@aws_object)
-        expect(@aws_object).to receive(:get).and_return(aws_object_output)
-        expect(aws_object_output).to receive(:body)
 
         described_class.file_as_stringio(base_update)
+        expect(aws_bucket).to have_received(:object).with('data/some-file.txt').and_return(aws_object)
+        expect(aws_object).to have_received(:get).and_return(aws_object_output)
+        expect(aws_object_output).to have_received(:body)
       end
     end
   end
