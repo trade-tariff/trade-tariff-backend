6a3b0cc71a9e0d02a0d8258d9aa2e25a7e4630f2
Amber Cronin <amber@amber.vision>
2023-10-30 10:31:23 +0000

    HOTT-4135: Backend Admin APIs
    + Adds backend admin API services
    + Updates pre-commit hooks

--- terraform/admin_xi.tf
+++ terraform/admin_xi.tf
@@ -1,14 +1,13 @@
-module "backend_uk" {
+module "backend_admin_xi" {
   source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.12.0"
 
-  service_name  = "${var.service_name}-uk"
+  service_name  = "backend-admin-xi"
   service_count = var.service_count
   region        = var.region
 
   cluster_name              = "trade-tariff-cluster-${var.environment}"
   subnet_ids                = data.aws_subnets.private.ids
   security_groups           = [data.aws_security_group.this.id]
-  target_group_arn          = data.aws_lb_target_group.this["backend-uk"].arn
   cloudwatch_log_group_name = "platform-logs-${var.environment}"
 
   min_capacity = var.min_capacity
@@ -26,16 +25,23 @@ module "backend_uk" {
 
   task_role_policy_arns = [
     aws_iam_policy.s3.arn,
-    aws_iam_policy.task_role_kms_keys.arn,
-    aws_iam_policy.emails.arn
+    aws_iam_policy.task_role_kms_keys.arn
   ]
 
   execution_role_policy_arns = [
-    aws_iam_policy.secrets.arn,
+    aws_iam_policy.secrets.arn
   ]
+
+  container_entrypoint = [""]
+  container_command    = local.worker_command
+
+  init_container            = true
+  init_container_entrypoint = [""]
+  init_container_command    = local.init_command
 
   service_environment_config = flatten([
     local.backend_common_vars,
+    local.backend_common_worker_vars,
     [
       {
         name  = "CDS"
@@ -43,29 +49,30 @@ module "backend_uk" {
       },
       {
         name  = "GOVUK_APP_DOMAIN"
-        value = "tariff-uk-backend-${var.environment}.apps.internal" # This is necessary for a GOVUK gem we're not using
+        value = "tariff-xi-admin-${var.environment}.apps.internal" # This is necessary for a GOVUK gem we're not using
+      },
+      {
+        name  = "NEW_RELIC_APP_NAME"
+        value = "tariff-xi-admin-${var.environment}"
       },
       {
         name  = "SERVICE"
-        value = "uk"
-      },
-      {
-        name  = "SLACK_USERNAME"
-        value = "UK Backend API ${title(var.environment)}"
+        value = "xi"
       },
       {
         name  = "TARIFF_FROM_EMAIL"
-        value = "Tariff UK [${title(var.environment)}] <${local.no_reply}>"
+        value = "Tariff XI [${title(var.environment)}] <${local.no_reply}>"
       },
       {
-        name  = "ENVIRONMENT"
-        value = var.environment
+        name  = "VCAP_APPLICATION"
+        value = "{}"
       }
     ]
   ])
 
   service_secrets_config = flatten([
     local.backend_common_secrets,
-    local.backend_uk_common_secrets
+    local.backend_xi_common_secrets,
+    local.backend_xi_worker_secrets,
   ])
 }
