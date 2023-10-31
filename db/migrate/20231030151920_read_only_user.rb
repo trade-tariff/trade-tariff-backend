557711bbc669418669f9d9377b918219b1279000
Amber Cronin <amber@amber.vision>
2023-10-31 11:08:34 +0000

    HOTT-4135: Sort out this huge diff
    [ci skip]

--- db/migrate/20231030151920_read_only_user.rb
+++ db/migrate/20231030151920_read_only_user.rb
@@ -1,17 +1,38 @@
 # frozen_string_literal: true
 
 user = 'tariff_read'
-db = Sequel::Model.db
 
 Sequel.migration do
   up do
-    # Create a user with SELECT on all tables, including tables created after this migration
-    db.run(sprintf('CREATE USER %s', user))
-    db.run(sprintf('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %s', user))
+    run %{
+        DO
+        $do$
+        BEGIN
+           IF EXISTS (
+              SELECT FROM pg_catalog.pg_roles
+              WHERE  rolname = '#{user}') THEN
+              RAISE NOTICE 'Role "#{user}" already exists. Skipping.';
+           ELSE
+              CREATE USER #{user};
+              ALTER DEFAULT PRIVILEGES IN SCHEMA uk GRANT SELECT ON TABLES TO #{user};
+              ALTER DEFAULT PRIVILEGES IN SCHEMA xi GRANT SELECT ON TABLES TO #{user};
+              ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO #{user};
+           END IF;
+        END
+        $do$;
+      }
   end
 
   down do
-    db.run(sprintf('ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON TABLES FROM %s', user))
-    db.run(sprintf('DROP USER IF EXISTS %s', user))
+    run %{
+      REVOKE ALL PRIVILEGES ON SCHEMA uk FROM #{user};
+      REVOKE ALL PRIVILEGES ON SCHEMA xi FROM #{user};
+      REVOKE ALL PRIVILEGES ON SCHEMA public FROM #{user};
+      ALTER DEFAULT PRIVILEGES IN SCHEMA uk REVOKE SELECT ON TABLES FROM #{user};
+      ALTER DEFAULT PRIVILEGES IN SCHEMA xi REVOKE SELECT ON TABLES FROM #{user};
+      ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON TABLES FROM #{user};
+      DROP OWNED BY #{user};
+      DROP USER IF EXISTS #{user};
+    }
   end
 end
