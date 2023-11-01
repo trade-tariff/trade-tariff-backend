48a3d501ff5fa6df216da17725905ab3b1f2d3af
William Fish <william.michael.fish@gmail.com>
2023-11-01 09:04:49 +0000

    HOTT-3359: Adds differences email configuration

--- spec/mailers/reports_mailer_spec.rb
+++ spec/mailers/reports_mailer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ReportsMailer, type: :mailer do
 
     let(:report) { Reporting::Differences.new }
 
-    it { expect(mail.subject).to eq('[HMRC Online Trade Tariff Support] UK tariff - potential issues report 2023-10-31') }
+    it { expect(mail.subject).to eq("[HMRC Online Trade Tariff Support] UK tariff - potential issues report #{Time.zone.today.iso8601}") }
     it { expect(mail.from).to eq(['no-reply@example.com']) }
     it { expect(mail.to).to eq(['differences@example.com']) }
     it { expect(mail.bcc).to eq(['user@example.com']) }
