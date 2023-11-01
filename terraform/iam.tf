d6eac0e39d049aeb459c53c66ad03aea2640e9af
William Fish <william.michael.fish@gmail.com>
2023-11-01 15:46:44 +0000

    HOTT-3359: Adds permissions to read secrets

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,6 +9,7 @@ data "aws_iam_policy_document" "secrets" {
     ]
     resources = [
       data.aws_secretsmanager_secret.database_connection_string.arn,
+      data.aws_secretsmanager_secret.differences_to_emails.arn,
       data.aws_secretsmanager_secret.oauth_id.arn,
       data.aws_secretsmanager_secret.oauth_secret.arn,
       data.aws_secretsmanager_secret.redis_uk_connection_string.arn,
