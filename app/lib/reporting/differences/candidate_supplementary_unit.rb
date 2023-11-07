d78c32034d6ff189653c87c51fcda6695d5a2277
William Fish <william.michael.fish@gmail.com>
2023-11-07 14:39:31 +0000

    HOTT-4323: Code review

--- app/lib/reporting/differences/candidate_supplementary_unit.rb
+++ app/lib/reporting/differences/candidate_supplementary_unit.rb
@@ -76,36 +76,54 @@ module Reporting
       attr_reader :report
 
       def each_row
+        each_declarable do |declarable|
+          units = candidate_units_for(declarable)
+          unit_codes = units.map do |unit|
+            "#{unit[:measurement_unit_code]}#{unit[:measurement_unit_qualifier_code]}"
+          end
+
+          next unless units.any?
+
+          yield [
+            declarable.goods_nomenclature_item_id,
+            unit_codes.uniq.sort.join(', '),
+          ]
+        end
+      end
+
+      # There are three rules for when a unit that is associated with a measure
+      # (and therefore a commodity) could potentially be a supplementary unit that
+      # should be declared:
+      #
+      # 1. The commodity has no supplementary units associated with it and there are units
+      #    associated with the commodities measures.
+      # 2. The commodity has supplementary units associated with it and there are units
+      #    of a different class/or type.
+      # 3. We have specific heuristics/business rules that tell us that a unit should be ignored
+      def candidate_units_for(declarable)
+        supplementary_unit_measures = declarable.applicable_measures.select(&:supplementary?)
+        relevant_measures = declarable.applicable_measures - supplementary_unit_measures
+        units = relevant_measures.flat_map(&:units)
+
+        if supplementary_unit_measures.any?
+          supplementary_unit_types = supplementary_unit_types_for(supplementary_unit_measures)
+
+          units.reject do |unit|
+            type = MeasurementUnit.type_for(
+              "#{unit[:measurement_unit_code]}#{unit[:measurement_unit_qualifier_code]}",
+            )
+
+            supplementary_unit_types.include?(type) || ignore_unit?(unit)
+          end
+        else
+          units.reject(&method(:ignore_unit?))
+        end
+      end
+
+      def each_declarable
         each_chapter(eager: Differences::GOODS_NOMENCLATURE_MEASURE_WITH_UNIT_EAGER) do |eager_chapter|
           eager_chapter.descendants.each do |chapter_descendant|
-            next unless chapter_descendant.declarable?
-
-            supplementary_unit_measures = chapter_descendant.applicable_measures.select(&:supplementary?)
-            relevant_measures = chapter_descendant.applicable_measures - supplementary_unit_measures
-            units = relevant_measures.flat_map(&:units)
-
-            units = if supplementary_unit_measures.any?
-                      supplementary_unit_types = supplementary_unit_types_for(supplementary_unit_measures)
-
-                      units.reject do |unit|
-                        type = MeasurementUnit.type_for(
-                          "#{unit[:measurement_unit_code]}#{unit[:measurement_unit_qualifier_code]}",
-                        )
-
-                        supplementary_unit_types.include?(type) || ignore_unit?(unit)
-                      end
-                    else
-                      units.reject(&method(:ignore_unit?))
-                    end
-
-            units = units.map { |unit| "#{unit[:measurement_unit_code]}#{unit[:measurement_unit_qualifier_code]}" }
-
-            next if units.blank?
-
-            yield [
-              chapter_descendant.goods_nomenclature_item_id,
-              units.uniq.join(', '),
-            ]
+            yield chapter_descendant if chapter_descendant.declarable?
           end
         end
       end
