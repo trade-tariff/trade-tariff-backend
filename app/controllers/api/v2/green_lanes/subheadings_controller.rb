2319e84982fa5f642021ef9065a20850271d68a6
jebw <jeb@jdwilkins.co.uk>
2023-11-22 16:44:12 +0000

    HOTT-4353 Include measures content in the serialized output

--- app/controllers/api/v2/green_lanes/subheadings_controller.rb
+++ app/controllers/api/v2/green_lanes/subheadings_controller.rb
@@ -3,9 +3,9 @@ module Api
     module GreenLanes
       class SubheadingsController < ApiController
         def show
-          subheading_measures = ::GreenLanes::FetchSubheadingsService.new(params[:id]).call
-          presented_subheading = SubheadingPresenter.new(subheading_measures)
-          serializer = Api::V2::GreenLanes::SubheadingMeasuresSerializer.new(presented_subheading)
+          subheading = ::GreenLanes::FetchSubheadingsService.new(params[:id]).call
+          presented_subheading = SubheadingPresenter.new(subheading)
+          serializer = Api::V2::GreenLanes::SubheadingSerializer.new(presented_subheading, include: ['applicable_measures'])
 
           render json: serializer.serializable_hash
         end
--- app/services/green_lanes/fetch_subheadings_service.rb
+++ app/services/green_lanes/fetch_subheadings_service.rb
@@ -6,12 +6,10 @@ module GreenLanes
     end
 
     def call
-      subheading = Subheading
+      Subheading
         .actual
         .where(goods_nomenclature_item_id: length_adjusted_digit_id, producline_suffix: '80')
         .take
-
-      SubheadingMeasures.new(subheading, subheading.applicable_measures)
     end
 
     private
