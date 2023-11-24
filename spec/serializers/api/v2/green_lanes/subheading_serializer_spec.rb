9bbe0e78615c3a400943b2d6d52a531d02c48a68
Alexdesi <alexdesi@gmail.com>
2023-11-24 10:33:20 +0000

    Refactor subheading_serializer_spec.

--- spec/serializers/api/v2/green_lanes/subheading_serializer_spec.rb
+++ spec/serializers/api/v2/green_lanes/subheading_serializer_spec.rb
@@ -1,22 +1,30 @@
 RSpec.describe Api::V2::GreenLanes::SubheadingSerializer do
-  subject(:serialized) { described_class.new(subheading).serializable_hash[:data] }
-
-  let(:subheading) { build :subheading }
-
-  describe '#serializable_hash' do
-    it { is_expected.to include type: :subheading }
-    it { is_expected.to include id: subheading.goods_nomenclature_sid.to_s }
-
-    context 'with attributes' do
-      subject { serialized[:attributes] }
-
-      it { is_expected.to include goods_nomenclature_item_id: subheading.goods_nomenclature_item_id.to_s }
-      it { is_expected.to include description: subheading.description }
-      it { is_expected.to include formatted_description: subheading.formatted_description }
-      it { is_expected.to include validity_start_date: subheading.validity_start_date }
-      it { is_expected.to include validity_end_date: subheading.validity_end_date }
-      it { is_expected.to include description_plain: subheading.description_plain }
-      it { is_expected.to include producline_suffix: subheading.producline_suffix }
-    end
+  subject(:serialized) do
+    described_class.new(subheading_presenter, include: %w[applicable_measures]).serializable_hash
   end
+
+  let(:subheading_presenter) { Api::V2::GreenLanes::SubheadingPresenter.new(subheading) }
+
+  let(:subheading) { create :subheading, :with_measures }
+
+  let(:expected_pattern) do
+    {
+      data: {
+        attributes: {
+          goods_nomenclature_item_id: subheading.goods_nomenclature_item_id,
+          goods_nomenclature_sid: subheading.goods_nomenclature_sid,
+          goods_nomenclature_item_id: subheading.goods_nomenclature_item_id.to_s,
+          description: be_a(String),
+          formatted_description: subheading.formatted_description,
+          validity_start_date: subheading.validity_start_date.iso8601,
+          validity_end_date: nil,
+          description_plain: subheading.description_plain,
+          producline_suffix: subheading.producline_suffix,
+        },
+      },
+      included: {},
+    }
+  end
+
+  it { is_expected.to include_json(expected_pattern) }
 end
