b941ac553477b560b48e3f5ed642e971c8350c71
Alexdesi <alexdesi@gmail.com>
2023-12-18 15:58:46 +0000

    HOTT-4628: add ErrorsController missing specs

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -22,6 +22,34 @@ RSpec.describe ApplicationController, type: :request do
       it { expect(do_response.body).to eq('{"error":"400 - Bad request: API documentation is available at https://api.trade-tariff.service.gov.uk/"}') }
     end
 
+    context 'when InvalidAuthenticityToken error is raised' do
+      let(:exception) { ActionController::InvalidAuthenticityToken }
+
+      it { is_expected.to have_http_status(:unprocessable_entity) }
+      it { expect(do_response.body).to eq('{"error":"422 - Unprocessable entity: API documentation is available at https://api.trade-tariff.service.gov.uk/"}') }
+    end
+
+    context 'when MethodNotAllowed error is raised' do
+      let(:exception) { ActionController::MethodNotAllowed }
+
+      it { is_expected.to have_http_status(:method_not_allowed) }
+      it { expect(do_response.body).to eq('{"error":"405 - Method Not Allowed: API documentation is available at https://api.trade-tariff.service.gov.uk/"}') }
+    end
+
+    context 'when NotImplemented error is raised' do
+      let(:exception) { ActionController::NotImplemented }
+
+      it { is_expected.to have_http_status(:not_implemented) }
+      it { expect(do_response.body).to eq('{"error":"501 - Not Implemented: API documentation is available at https://api.trade-tariff.service.gov.uk/"}') }
+    end
+
+    context 'when UnknownFormat error is raised' do
+      let(:exception) { ActionController::UnknownFormat }
+
+      it { is_expected.to have_http_status(:not_acceptable) }
+      it { expect(do_response.body).to eq('{"error":"406 - Not Acceptable: API documentation is available at https://api.trade-tariff.service.gov.uk/"}') }
+    end
+
     shared_examples_for 'a not found request' do |error|
       let(:exception) { error }
 
