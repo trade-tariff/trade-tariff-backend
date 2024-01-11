34af6a27193788cf55058ded410288e86dee7ebf
Alexdesi <alexdesi@gmail.com>
2024-01-11 16:15:44 +0000

    HOTT-4853: Fix various issues and zeitwerk error on data migration generator.

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,8 @@
-require_relative "boot"
+require_relative 'boot'
+require_relative '../lib/core_ext/object'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-# require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
-require "action_view/railtie"
-# require "action_cable/engine"
-# require "rails/test_unit/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,6 +10,29 @@ Bundler.require(*Rails.groups)
 
 module TradeTariffBackend
   class Application < Rails::Application
+    config.debug_exception_response_format = :default
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    config.generators do |g|
+      g.view_specs     false
+      g.helper_specs   false
+      g.test_framework false
+    end
+
+    config.time_zone = 'UTC'
+
+    config.intercept_messages = config_for(:intercept_messages)
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+
+
+  
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -40,5 +53,27 @@ module TradeTariffBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.sequel.schema_format = :sql
+    config.sequel.default_timezone = :utc
+
+    config.sequel.after_connect = proc do
+      Sequel::Model.plugin :take
+      Sequel::Model.plugin :validation_class_methods
+
+      Sequel::Model.db.extension :pagination
+      Sequel::Model.db.extension :server_block
+      Sequel::Model.db.extension :auto_literal_strings
+      Sequel::Model.db.extension :pg_array
+      Sequel::Model.db.extension :null_dataset
+    end
+
+    # Tells Rails to serve error pages from the app itself, rather than using static error pages in public/
+    config.exceptions_app = routes
+
+    config.sequel.allow_missing_migration_files = \
+      (ENV['ALLOW_MISSING_MIGRATION_FILES'].to_s == 'true')
   end
+
+  Rails.autoloaders.main.ignore(Rails.root.join('lib/core_ext'))
 end
