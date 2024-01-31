92a2fdd4cd8c1d237bb57d81674f362843094330
jebw <jeb@jdwilkins.co.uk>
2024-01-31 13:44:18 +0000

    HOTT-4996 Added GreenLanes Theme model

---
+++ spec/models/green_lanes/theme_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe GreenLanes::Theme do
+  describe 'attributes' do
+    it { is_expected.to respond_to :section }
+    it { is_expected.to respond_to :subsection }
+    it { is_expected.to respond_to :theme }
+    it { is_expected.to respond_to :description }
+    it { is_expected.to respond_to :category }
+    it { is_expected.to respond_to :created_at }
+    it { is_expected.to respond_to :updated_at }
+  end
+
+  describe 'validations' do
+    subject(:errors) { instance.tap(&:valid?).errors }
+
+    let(:instance) { described_class.new }
+
+    it { is_expected.to include section: ['is not present'] }
+    it { is_expected.to include subsection: ['is not present'] }
+    it { is_expected.to include theme: ['is not present'] }
+    it { is_expected.to include description: ['is not present'] }
+    it { is_expected.to include category: ['is not present'] }
+
+    context 'with blank theme' do
+      let(:instance) { described_class.new theme: '' }
+
+      it { is_expected.to include theme: ['is not present'] }
+    end
+
+    context 'with duplicate section and subsection' do
+      let(:existing) { create :green_lanes_theme }
+
+      let :instance do
+        described_class.new section: existing.section,
+                            subsection: existing.subsection
+      end
+
+      it { is_expected.to include %i[section subsection] => ['is already taken'] }
+    end
+  end
+
+  describe 'date fields' do
+    subject { create(:green_lanes_theme).reload }
+
+    it { is_expected.to have_attributes created_at: be_within(1.minute).of(Time.zone.now) }
+    it { is_expected.to have_attributes updated_at: be_within(1.minute).of(Time.zone.now) }
+  end
+end
