01aa1713adbeea8892f40f288f8b6378e1bfaf93
rasikasri <44141067+rasikasri@users.noreply.github.com>
2024-02-06 10:03:06 +0000

    HOTT-4995: Allow localhost access without auth token (#1700)
    * HOTT-4995: Allow localhost access without auth token

--- app/controllers/api/v2/green_lanes/base_controller.rb
+++ app/controllers/api/v2/green_lanes/base_controller.rb
@@ -15,9 +15,11 @@ module Api
         end
 
         def authenticate
-          authenticate_or_request_with_http_token do |provided_token, _options|
-            Rails.logger.debug provided_token
-            api_tokens.any? { |token| ActiveSupport::SecurityUtils.secure_compare(provided_token, token) }
+          unless Rails.env.development? && TradeTariffBackend.green_lanes_api_tokens.blank?
+            authenticate_or_request_with_http_token do |provided_token, _options|
+              Rails.logger.debug provided_token
+              api_tokens.any? { |token| ActiveSupport::SecurityUtils.secure_compare(provided_token, token) }
+            end
           end
         end
 
