537886fe9ba20377729966ed3ca27e6cc23fe9b5
jebw <jeb@jdwilkins.co.uk>
2024-02-13 14:14:41 +0000

    HOTT-5114 Avoid duplicate category assessments

--- app/services/green_lanes/find_category_assessments_service.rb
+++ app/services/green_lanes/find_category_assessments_service.rb
@@ -2,11 +2,11 @@ module GreenLanes
   class FindCategoryAssessmentsService
     class << self
       def call(goods_nomenclature:, geographical_area_id: nil)
-        goods_nomenclature.applicable_measures.flat_map do |measure|
+        goods_nomenclature.applicable_measures.flat_map { |measure|
           CategoryAssessment.filter(regulation_id: measure.measure_generating_regulation_id,
                                     measure_type_id: measure.measure_type_id,
                                     geographical_area: geographical_area_id)
-        end
+        }.uniq
       end
     end
   end
--- spec/services/green_lanes/find_category_assessments_service_spec.rb
+++ spec/services/green_lanes/find_category_assessments_service_spec.rb
@@ -1,19 +1,21 @@
 RSpec.describe GreenLanes::FindCategoryAssessmentsService do
   describe '#call' do
-    subject(:service) { described_class }
+    subject(:matches) { described_class.call goods_nomenclature: }
 
     before do
       GreenLanes::CategoryAssessment.load_from_string json_categorisations
 
-      allow(subheading).to receive(:applicable_measures).and_return measures
+      allow(goods_nomenclature).to receive(:applicable_measures).and_return measures
     end
 
-    let(:subheading) { create(:subheading) }
+    let(:goods_nomenclature) { create(:subheading) }
 
     let(:measures) do
-      [create(:measure, measure_generating_regulation_id: 'D0000001', measure_type_id: '400'),
-       create(:measure, measure_generating_regulation_id: 'D0000002', measure_type_id: '500'),
-       create(:measure, measure_generating_regulation_id: 'D0000003', measure_type_id: '713')]
+      [
+        create(:measure, measure_generating_regulation_id: 'D0000001', measure_type_id: '400'),
+        create(:measure, measure_generating_regulation_id: 'D0000002', measure_type_id: '500'),
+        create(:measure, measure_generating_regulation_id: 'D0000003', measure_type_id: '713'),
+      ]
     end
 
     let(:json_categorisations) do
@@ -43,17 +45,31 @@ RSpec.describe GreenLanes::FindCategoryAssessmentsService do
        }].to_json
     end
 
-    it 'returns categorisations based on applicable measures' do
-      result = service.call(goods_nomenclature: subheading)
-
-      expect(result).to have_attributes length: 4
+    context 'without origin filter' do
+      it { is_expected.to have_attributes length: 4 }
     end
 
     context 'when geographical_area_id is provided' do
-      it 'filter categorisations by geographical_area_id' do
-        result = service.call(goods_nomenclature: subheading, geographical_area_id: 'AU')
+      subject { described_class.call(goods_nomenclature:, geographical_area_id: 'AU') }
 
-        expect(result).to have_attributes length: 2
+      it { is_expected.to have_attributes length: 2 }
+    end
+
+    context 'with duplicate measures' do
+      let(:measures) do
+        [
+          create(:measure, measure_generating_regulation_id: 'D0000002', measure_type_id: '500'),
+          create(:measure, measure_generating_regulation_id: 'D0000003', measure_type_id: '713'),
+          create(:measure, measure_generating_regulation_id: 'D0000003', measure_type_id: '713'),
+        ]
+      end
+
+      let(:matched_regulation_ids) { matches.map(&:regulation_id) }
+
+      it { is_expected.to have_attributes length: 2 }
+
+      it 'includes expected category assessments' do
+        expect(matched_regulation_ids).to match_array %w[D0000002 D0000003]
       end
     end
   end
