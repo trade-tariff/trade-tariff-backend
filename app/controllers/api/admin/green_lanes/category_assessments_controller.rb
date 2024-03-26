3bd01284bffe52b5bdcceae21d1844b4cbb4a90d
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2024-03-26 16:39:47 +0000

    GL-102: Add Admin API to backend for CRUD of categorisation data

--- app/controllers/api/admin/green_lanes/category_assessments_controller.rb
+++ app/controllers/api/admin/green_lanes/category_assessments_controller.rb
@@ -10,7 +10,55 @@ module Api
           render json: serialize(category_assessments.to_a, pagination_meta)
         end
 
+        def show
+          ca = ::GreenLanes::CategoryAssessment.with_pk!(params[:id])
+          render json: serialize(ca)
+        end
+
+        def create
+          ca = ::GreenLanes::CategoryAssessment.new(ca_params)
+
+          if ca.valid? && ca.save
+            render json: serialize(ca),
+                   location: api_admin_category_assessment_url(ca.id),
+                   status: :created
+          else
+            render json: serialize_errors(ca),
+                   status: :unprocessable_entity
+          end
+        end
+
+        def update
+          ca = ::GreenLanes::CategoryAssessment.with_pk!(params[:id])
+          ca.set ca_params
+
+          if ca.valid? && ca.save
+            render json: serialize(ca),
+                   location: api_admin_category_assessment_url(ca.id),
+                   status: :ok
+          else
+            render json: serialize_errors(ca),
+                   status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          ca = ::GreenLanes::CategoryAssessment.with_pk!(params[:id])
+          ca.destroy
+
+          head :no_content
+        end
+
         private
+
+        def ca_params
+          params.require(:data).require(:attributes).permit(
+            :regulation_id,
+            :regulation_role,
+            :measure_type_id,
+            :theme_id,
+            )
+        end
 
         def record_count
           @category_assessments.pagination_record_count
@@ -22,6 +70,10 @@ module Api
 
         def serialize(*args)
           Api::Admin::GreenLanes::CategoryAssessmentSerializer.new(*args).serializable_hash
+        end
+
+        def serialize_errors(ca)
+          Api::Admin::ErrorSerializationService.new(ca).call
         end
 
         def check_service
