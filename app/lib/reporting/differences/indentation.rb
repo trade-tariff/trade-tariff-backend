471165a723093090409b56a0d6b718a70f82a41c
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-04-15 14:28:27 +0100

    Comparison bug fix

--- app/lib/reporting/differences/indentation.rb
+++ app/lib/reporting/differences/indentation.rb
@@ -91,7 +91,7 @@ module Reporting
         matching_uk_goods_nomenclature_for_comparison = uk_goods_nomenclature_ids_for_comparison[matching]
         matching_xi_goods_nomenclature_for_comparison = xi_goods_nomenclature_ids_for_comparison[matching]
 
-        new_issue = matching_uk_goods_nomenclature_for_comparison.nil? || matching_xi_goods_nomenclature_for_comparison.nil? || matching_uk_goods_nomenclature_for_comparison['Indentation'] != matching_xi_goods_nomenclature_for_comparison['Indentation']
+        new_issue = matching_uk_goods_nomenclature_for_comparison.nil? || matching_xi_goods_nomenclature_for_comparison.nil? || matching_uk_goods_nomenclature_for_comparison['Indentation'] == matching_xi_goods_nomenclature_for_comparison['Indentation']
 
         [
           "#{item_id} (#{pls})",
