9e2ac997f17815972f735b1869f8f4e880f85337
Hadleigh Wallenberg <127106895+HWallenberg@users.noreply.github.com>
2024-04-15 09:52:44 +0100

    Refactored new_issue assignment

--- app/lib/reporting/differences/indentation.rb
+++ app/lib/reporting/differences/indentation.rb
@@ -90,12 +90,8 @@ module Reporting
 
         matching_uk_goods_nomenclature_for_comparison = uk_goods_nomenclature_ids_for_comparison[matching]
         matching_xi_goods_nomenclature_for_comparison = xi_goods_nomenclature_ids_for_comparison[matching]
-
-        new_issue = if matching_uk_goods_nomenclature_for_comparison.nil? || matching_xi_goods_nomenclature_for_comparison.nil?
-                      true
-                    else
-                      matching_uk_goods_nomenclature_for_comparison['Indentation'] == matching_xi_goods_nomenclature_for_comparison['Indentation']
-                    end
+        
+        new_issue = matching_uk_goods_nomenclature_for_comparison.nil? || matching_xi_goods_nomenclature_for_comparison.nil? || matching_uk_goods_nomenclature_for_comparison['Indentation'] != matching_xi_goods_nomenclature_for_comparison['Indentation]
 
         [
           "#{item_id} (#{pls})",
