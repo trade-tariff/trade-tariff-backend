cd5f6e94be558006d2a835692c2360477283b1b0
rasika.abeyrathna <rasikasri@gmail.com>
2024-06-03 16:29:30 +0100

    Fixed pagination

--- spec/serializers/api/admin/green_lanes/goods_nomenclature_serializer_spec.rb
+++ spec/serializers/api/admin/green_lanes/goods_nomenclature_serializer_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Api::Admin::GreenLanes::GoodsNomenclatureSerializer do
         attributes: {
           description: serializable.description,
           goods_nomenclature_sid: serializable.goods_nomenclature_sid,
+          goods_nomenclature_item_id: serializable.goods_nomenclature_item_id,
         },
       },
     }
