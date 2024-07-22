d1cfafa86ee0e7b0694503af5a4348dbd0bd06e1
William Fish <william.michael.fish@gmail.com>
2024-07-22 13:56:19 +0100

    BAU: Add exec perms

--- terraform/backend_uk.tf
+++ terraform/backend_uk.tf
@@ -24,6 +24,7 @@ module "backend_uk" {
   memory = var.memory
 
   task_role_policy_arns = [
+    aws_iam_policy.exec.arn,
     aws_iam_policy.s3.arn,
     aws_iam_policy.task_role_kms_keys.arn,
     aws_iam_policy.emails.arn
