6e42b189467612aae8ab661e822a23c15020a3d9
rasika.abeyrathna <rasikasri@gmail.com>
2024-07-24 15:56:54 +0100

    GL-786: Enable notifications component to surface new data coming in

--- app/lib/green_lanes_updates_publisher/update_notifications_creator.rb
+++ app/lib/green_lanes_updates_publisher/update_notifications_creator.rb
@@ -8,9 +8,9 @@ module GreenLanesUpdatesPublisher
       if @updates.any?
         @updates.each do |update|
           notification = GreenLanes::UpdateNotification.new(regulation_id: update.regulation_id,
-                                                regulation_role: update.regulation_role,
-                                                measure_type_id: update.measure_type_id,
-                                                status: update.status)
+                                                            regulation_role: update.regulation_role,
+                                                            measure_type_id: update.measure_type_id,
+                                                            status: update.status)
 
           if notification.valid?
             notification.save
