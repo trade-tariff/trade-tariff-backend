775cb91f6b8b4f827949bbc5d7499ed96e9b7b86
Neil Middleton <neil.middleton@transformuk.com>
2024-09-10 11:44:25 +0100

    reinstate local change

--- app/lib/reporting.rb
+++ app/lib/reporting.rb
@@ -2,19 +2,19 @@ module Reporting
   extend Reportable
 
   def self.get(object_key)
-    # if Rails.env.production?
-    #   object(object_key).get.body.read
-    # else
+    if Rails.env.production?
+      object(object_key).get.body.read
+    else
 
-    File.open(object_key)
-    # end
+      File.open(object_key)
+    end
   end
 
   def self.get_link(object_key)
-    # if Rails.env.production?
-    #   File.join(TradeTariffBackend.reporting_cdn_host, object_key)
-    # else
-    object_key
-    # end
+    if Rails.env.production?
+      File.join(TradeTariffBackend.reporting_cdn_host, object_key)
+    else
+      object_key
+    end
   end
 end
