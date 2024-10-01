5a231b0eede4bd59c93e9a068bfa9d2ae9f3d407
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-10-01 14:39:54 +0100

    Altered tests to use CELEX explicit URLs

--- spec/presenters/api/v2/green_lanes/regulation_presenter_spec.rb
+++ spec/presenters/api/v2/green_lanes/regulation_presenter_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Api::V2::GreenLanes::RegulationPresenter do
   it { is_expected.to have_attributes base_regulation_id: regulation.base_regulation_id }
   it { is_expected.to have_attributes published_date: regulation.published_date }
   it { is_expected.to have_attributes regulation_code: %r{[A-Z0-9]{5}/[A-Z0-9]{2}} }
-  it { is_expected.to have_attributes regulation_url: %r{http://eur-lex.} }
+  it { is_expected.to have_attributes regulation_url: %r{https://eur-lex.} }
   it { is_expected.to have_attributes description: be_present }
 end
