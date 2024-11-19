ec172bc4a93ba7353357b5eb2444c2ee59e1a574
rasika.abeyrathna <rasikasri@gmail.com>
2024-11-19 12:20:08 +0000

    Added measures loader and changed to batch upload

--- app/lib/snapshot_importer.rb
+++ app/lib/snapshot_importer.rb
@@ -6,7 +6,7 @@ Dir[Rails.root.join('app/lib/snapshot/loaders/**/*.rb')].sort.each { |f| require
 class SnapshotImporter
   class ImportException < StandardError; end
 
-  FILE = '/Users/neil.middleton/Downloads/yearly.xml'.freeze
+  FILE = '/Users/rasika.abeyrathna/dev/HMRC/yearly.xml'.freeze
 
   def nodes
     Loaders.constants.dup.map(&:to_s).delete_if { |name| name == 'Base' }
@@ -20,16 +20,36 @@ class SnapshotImporter
     Object.const_get('AdditionalCodeType::Operation').truncate
     Object.const_get('AdditionalCodeTypeDescription::Operation').truncate
     Object.const_get('AdditionalCodeTypeMeasureType::Operation').truncate
+    Object.const_get('Measure::Operation').truncate
+    Object.const_get('MeasureComponent::Operation').truncate
 
+    file_name = FILE.split('/').last
     File.open(FILE) do |file|
+      current_node = ''
+      count = 0
+      batch = []
+
       Nokogiri::XML::Reader.from_io(file).each do |node|
         next unless nodes.include? node.name
 
-        attribs = Hash.from_xml(node.outer_xml)
-        next if attribs[node.name].nil?
+        if !batch.empty? && (current_node != node.name || count % 100 == 0)
+          puts "Loading #{current_node}"
+          loader = Object.const_get("Loaders::#{current_node}")
+          loader.load(file_name, batch)
+          batch.clear
+        end
 
-        puts "Loading #{node.name}" # rubocop:disable Rails/Output
-        Object.const_get("Loaders::#{node.name}").load(FILE.split('/').last, attribs)
+        # Process current node
+        attribs = Hash.from_xml(node.outer_xml)
+        batch << attribs if attribs[node.name]
+        current_node = node.name
+        count += 1
+      end
+
+      unless batch.empty?
+        puts "Loading #{current_node}"
+        loader = Object.const_get("Loaders::#{current_node}")
+        loader.load(file_name, batch)
       end
     end
     puts 'Done' # rubocop:disable Rails/Output
