f39a384975a220055d70e1b1857655deeb5a4cba
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-11-28 12:19:39 +0000

    Added env_var to toggle clean up Changes table in Cron job

--- app/workers/populate_changes_table_worker.rb
+++ app/workers/populate_changes_table_worker.rb
@@ -5,6 +5,10 @@ class PopulateChangesTableWorker
 
   def perform
     ChangesTablePopulator.populate
-    ChangesTablePopulator.cleanup_outdated
+    if TradeTariffBackend.execute_clean_up_changes_table?
+      ChangesTablePopulator.cleanup_outdated
+    else
+      logger.info 'Skipping cleanup of outdated changes, see env_var CLEAN_UP_CHANGES_TABLE'
+    end
   end
 end
