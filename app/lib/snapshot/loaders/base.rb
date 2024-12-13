2937155dda5d19483a1ac03a6e56c0b06f5d2149
rasika.abeyrathna <rasikasri@gmail.com>
2024-12-13 12:02:39 +0000

    rubocope errors

--- app/lib/snapshot/loaders/base.rb
+++
@@ -1,7 +0,0 @@
-module Loaders
-  class Base
-    def self.load(file, attributes)
-      # puts attributes[name.split('::').last].to_json
-    end
-  end
-end
---
+++ app/lib/snapshot_loaders/regulation_role_type.rb
@@ -0,0 +1,33 @@
+module SnapshotLoaders
+  class RegulationRoleType < Base
+    def self.load(file, batch)
+      regulation_groups = []
+      descriptions = []
+
+      batch.each do |attributes|
+        regulation_groups.push({
+          regulation_role_type_id: attributes.dig('RegulationRoleType', 'regulationRoleTypeId'),
+          # national: attributes.dig('RegulationRoleType',''),
+          validity_start_date: attributes.dig('RegulationRoleType', 'validityStartDate'),
+          validity_end_date: attributes.dig('RegulationRoleType', 'validityEndDate'),
+          operation: attributes.dig('RegulationRoleType', 'metainfo', 'opType'),
+          operation_date: attributes.dig('RegulationRoleType', 'metainfo', 'transactionDate'),
+          filename: file,
+        })
+
+        descriptions.push({
+          regulation_role_type_id: attributes.dig('RegulationRoleType', 'regulationRoleTypeId'),
+          language_id: attributes.dig('RegulationRoleType', 'regulationRoleTypeDescription', 'language', 'languageId'),
+          description: attributes.dig('RegulationRoleType', 'regulationRoleTypeDescription', 'description'),
+          # national: attributes.dig('RegulationRoleType', 'regulationRoleTypeDescription',''),
+          operation: attributes.dig('RegulationRoleType', 'regulationRoleTypeDescription', 'metainfo', 'opType'),
+          operation_date: attributes.dig('RegulationRoleType', 'regulationRoleTypeDescription', 'metainfo', 'transactionDate'),
+          filename: file,
+        })
+      end
+
+      Object.const_get('RegulationRoleType::Operation').multi_insert(regulation_groups)
+      Object.const_get('RegulationRoleTypeDescription::Operation').multi_insert(descriptions)
+    end
+  end
+end
