4315ec5795f95eea24c4a05b350bf7a7e25b30c9
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-01-08 17:14:49 +0000

    Added check for table exists in migration file

--- db/migrate/20241210100900_add_green_lanes_faq_feedback_table.rb
+++ db/migrate/20241210100900_add_green_lanes_faq_feedback_table.rb
@@ -2,14 +2,16 @@
 
 Sequel.migration do
   change do
-    create_table :green_lanes_faq_feedback do
-      primary_key :id
-      String :session_id, null: false
-      Integer :category_id, null: false
-      Integer :question_id, null: false
-      Boolean :useful, null: false
-      DateTime :created_at
-      DateTime :updated_at
+    unless table_exists?(:green_lanes_faq_feedback)
+      create_table :green_lanes_faq_feedback do
+        primary_key :id
+        String :session_id, null: false
+        Integer :category_id, null: false
+        Integer :question_id, null: false
+        Boolean :useful, null: false
+        DateTime :created_at
+        DateTime :updated_at
+      end
     end
 
     alter_table :green_lanes_faq_feedback do
