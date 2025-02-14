327555d8a146c7e41437dbaaeec97294673ba981
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-02-14 17:42:19 +0000

    Revert "Attempt to fix invalid SES issue by adding credentials"
    This reverts commit eabd8652d355ff18a6cd4c8c52d1b1a9cdc8fb13.
    
    To see if the terraform errors are related to the change.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,6 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :ses
 
-  config.action_mailer.ses_settings = {
-    region: 'eu-west-1',
-    credentials: Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'],
-      ENV['AWS_SECRET_ACCESS_KEY'],
-    ),
-  }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = [I18n.default_locale]
