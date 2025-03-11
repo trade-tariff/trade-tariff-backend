37df88b74aacaa41a130eaa889ab67332614ae3a
rasika.abeyrathna <rasikasri@gmail.com>
2025-03-11 12:57:21 +0000

    HMRC-738: Add Instrumentation for Search Result Logging in Search Suggestions and Search Controller

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -5,7 +5,7 @@ module Api
 
       def search
         results = SearchService.new(Api::V2::SearchSerializationService.new, params).to_json
-        log_search_results(params[:q], results)
+        SearchInstrumentationService.log_search_results(params[:q], results)
         render json: results
       end
 
@@ -15,7 +15,7 @@ module Api
                   else
                     Api::V2::SearchSuggestionSerializer.new(matching_suggestions).serializable_hash
                   end
-        log_search_suggestions_results(params[:q], results)
+        SearchInstrumentationService.log_search_suggestions_results(params[:q], results)
         render json: results
       end
 
@@ -27,57 +27,6 @@ module Api
         end
 
         []
-      end
-
-      def log_search_results(query, results)
-        results_type = results[:data][:type]
-        attributes = results[:data][:attributes]
-
-        commodity_score = attributes&.dig(:goods_nomenclature_match)&.dig('commodities')&.first&.dig('_score') || 0
-        chapter_score = attributes&.dig(:goods_nomenclature_match)&.dig('chapters')&.first&.dig('_score') || 0
-        heading_score = attributes&.dig(:goods_nomenclature_match)&.dig('headings')&.first&.dig('_score') || 0
-        section_score = attributes&.dig(:goods_nomenclature_match)&.dig('sections')&.first&.dig('_score') || 0
-
-        ref_commodity_score = attributes&.dig(:reference_match)&.dig('commodities')&.first&.dig('_score') || 0
-        ref_chapter_score = attributes&.dig(:reference_match)&.dig('chapters')&.first&.dig('_score') || 0
-        ref_heading_score = attributes&.dig(:reference_match)&.dig('headings')&.first&.dig('_score') || 0
-        ref_section_score = attributes&.dig(:reference_match)&.dig('sections')&.first&.dig('_score') || 0
-
-        # Find the maximum score
-        max_score = [
-          commodity_score,
-          chapter_score,
-          heading_score,
-          section_score,
-          ref_commodity_score,
-          ref_chapter_score,
-          ref_heading_score,
-          ref_section_score,
-        ].max
-
-        chapter_count = attributes&.dig(:goods_nomenclature_match)&.dig('chapters')&.size || 0
-        ref_chapter_count = attributes&.dig(:reference_match)&.dig('chapters')&.size || 0
-        commodity_count = attributes&.dig(:goods_nomenclature_match)&.dig('commodities')&.size || 0
-        attributes&.dig(:reference_match)&.dig('commodities')&.size || 0
-        heading_count = attributes&.dig(:goods_nomenclature_match)&.dig('headings')&.size || 0
-        ref_heading_count = attributes&.dig(:reference_match)&.dig('headings')&.size || 0
-        attributes&.dig(:goods_nomenclature_match)&.dig('sections')&.size || 0
-        attributes&.dig(:reference_match)&.dig('sections')&.size || 0
-        results_count = chapter_count + ref_chapter_count + heading_count + ref_heading_count + commodity_count
-
-        results_zero = results_count.zero?
-        query_length = query.present? ? query.length : 0
-        Rails.logger.info "Search Request : search_query=#{query}, query_length=#{query_length}, "\
-                            "result_type=#{results_type}, top_result_score=#{max_score}, "\
-                            "result_count=#{results_count}, result_zero=#{results_zero}"
-      end
-
-      def log_search_suggestions_results(query, results)
-        results_count = results[:data].size
-        results_zero = results_count.nil? || results_count.zero?
-        query_length = query.present? ? query.length : 0
-        Rails.logger.info "Search Suggestion Request : search_query=#{query}, query_length=#{query_length}, "\
-                            "result_count=#{results_count}, result_zero=#{results_zero}"
       end
     end
   end
---
+++ app/services/search_instrumentation_service.rb
@@ -0,0 +1,52 @@
+class SearchInstrumentationService
+  def self.log_search_suggestions_results(query, results)
+      results_count = results[:data].size
+      results_zero = results_count.nil? || results_count.zero?
+      query_length = query.present? ? query.length : 0
+      Rails.logger.info "Search Suggestion Request : search_query=#{query}, query_length=#{query_length}, "\
+                          "result_count=#{results_count}, result_zero=#{results_zero}"
+  end
+
+  def self.log_search_results(query, results)
+    results_type = results[:data][:type]
+    attributes = results[:data][:attributes]
+
+    commodity_score = attributes&.dig(:goods_nomenclature_match)&.dig('commodities')&.first&.dig('_score') || 0
+    chapter_score = attributes&.dig(:goods_nomenclature_match)&.dig('chapters')&.first&.dig('_score') || 0
+    heading_score = attributes&.dig(:goods_nomenclature_match)&.dig('headings')&.first&.dig('_score') || 0
+    section_score = attributes&.dig(:goods_nomenclature_match)&.dig('sections')&.first&.dig('_score') || 0
+
+    ref_commodity_score = attributes&.dig(:reference_match)&.dig('commodities')&.first&.dig('_score') || 0
+    ref_chapter_score = attributes&.dig(:reference_match)&.dig('chapters')&.first&.dig('_score') || 0
+    ref_heading_score = attributes&.dig(:reference_match)&.dig('headings')&.first&.dig('_score') || 0
+    ref_section_score = attributes&.dig(:reference_match)&.dig('sections')&.first&.dig('_score') || 0
+
+    # Find the maximum score
+    max_score = [
+      commodity_score,
+      chapter_score,
+      heading_score,
+      section_score,
+      ref_commodity_score,
+      ref_chapter_score,
+      ref_heading_score,
+      ref_section_score,
+    ].max
+
+    chapter_count = attributes&.dig(:goods_nomenclature_match)&.dig('chapters')&.size || 0
+    ref_chapter_count = attributes&.dig(:reference_match)&.dig('chapters')&.size || 0
+    commodity_count = attributes&.dig(:goods_nomenclature_match)&.dig('commodities')&.size || 0
+    attributes&.dig(:reference_match)&.dig('commodities')&.size || 0
+    heading_count = attributes&.dig(:goods_nomenclature_match)&.dig('headings')&.size || 0
+    ref_heading_count = attributes&.dig(:reference_match)&.dig('headings')&.size || 0
+    attributes&.dig(:goods_nomenclature_match)&.dig('sections')&.size || 0
+    attributes&.dig(:reference_match)&.dig('sections')&.size || 0
+    results_count = chapter_count + ref_chapter_count + heading_count + ref_heading_count + commodity_count
+
+    results_zero = results_count.zero?
+    query_length = query.present? ? query.length : 0
+    Rails.logger.info "Search Request : search_query=#{query}, query_length=#{query_length}, "\
+                        "result_type=#{results_type}, top_result_score=#{max_score}, "\
+                        "result_count=#{results_count}, result_zero=#{results_zero}"
+  end
+end
