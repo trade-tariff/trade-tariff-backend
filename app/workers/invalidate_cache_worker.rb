30f266beb65e77818d14831ec5db59e2f62c0468
Neil Middleton <neil.middleton@transformuk.com>
2025-03-26 11:55:59 +0000

    Default CDN name

--- app/workers/invalidate_cache_worker.rb
+++ app/workers/invalidate_cache_worker.rb
@@ -5,14 +5,11 @@ class InvalidateCacheWorker
     creds = Aws::ECSCredentials.new(retries: 3)
     client = Aws::CloudFront::Client.new(credentials: creds)
 
-    production_cdn = client.list_distributions.distribution_list.items.find { |d| d.comment = 'Production CDN' }
-    if production_cdn
-      distribution_id = production_cdn.id
-    end
+    production_cdn = client.list_distributions.distribution_list.items.find { |d| d.comment = ENV.fetch('CDN_DISTRIBUTION_NAME', 'Production CDN') }
 
-    if distro
+    if production_cdn
       client.create_invalidation({
-        distribution_id: distribution_id,
+        distribution_id: production_cdn.id,
         invalidation_batch: {
           paths: {
             quantity: 1,
