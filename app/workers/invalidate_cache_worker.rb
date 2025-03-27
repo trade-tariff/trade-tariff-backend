deb8c61d9a8b66db1987424bd3d64b2b55faf20a
Amber Cronin <amber@amber.vision>
2025-03-27 13:59:37 +0000

    HMRC-693: Use existing env var

--- app/workers/invalidate_cache_worker.rb
+++ app/workers/invalidate_cache_worker.rb
@@ -5,7 +5,7 @@ class InvalidateCacheWorker
     creds = Aws::ECSCredentials.new(retries: 3)
     client = Aws::CloudFront::Client.new(credentials: creds)
 
-    production_cdn = client.list_distributions.distribution_list.items.find { |d| d.comment = ENV.fetch('CDN_DISTRIBUTION_NAME', 'Production CDN') }
+    production_cdn = client.list_distributions.distribution_list.items.find { |d| d.comment = "#{ENV['ENVIRONMENT'].capitalize} CDN" }
 
     if production_cdn
       client.create_invalidation({
