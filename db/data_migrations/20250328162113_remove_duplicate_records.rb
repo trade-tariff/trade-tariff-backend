7ebb7a2e95ce7b41fe16c8c759b6de3221a81ff0
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-04-01 16:59:25 +0100

    Remove duplicates from quota_definitions_oplog
    Remove comment

--- db/data_migrations/20250328162113_remove_duplicate_records.rb
+++ db/data_migrations/20250328162113_remove_duplicate_records.rb
@@ -1,8 +1,5 @@
 Sequel.migration do
   up do
-    # Remove duplicate records from quota_balance_events_oplog
-    # based on the combination of quota_definition_sid and occurrence_timestamp.
-    # Keep the record with the maximum oid for each combination.
     run <<~SQL
       WITH max_oids AS (
         SELECT quota_definition_sid, occurrence_timestamp, MAX(oid) AS max_oid
@@ -14,6 +11,16 @@ Sequel.migration do
       WHERE quota_balance_events_oplog.quota_definition_sid = max_oids.quota_definition_sid
         AND quota_balance_events_oplog.occurrence_timestamp = max_oids.occurrence_timestamp
         AND quota_balance_events_oplog.oid != max_oids.max_oid;
+
+      WITH max_oids AS (
+        SELECT quota_definition_sid, MAX(oid) AS max_oid
+        FROM quota_definitions_oplog
+        GROUP BY quota_definition_sid
+      )
+      DELETE FROM quota_definitions_oplog
+      USING max_oids
+      WHERE quota_definitions_oplog.quota_definition_sid = max_oids.quota_definition_sid
+        AND quota_definitions_oplog.oid != max_oids.max_oid;
     SQL
   end
 
