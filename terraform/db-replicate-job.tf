25fe27456756b9ab166224e154ce80e06adef0db
rasika.abeyrathna <rasikasri@gmail.com>
2025-04-22 21:09:42 +0100

    HMRC-798: Github workflow to replicate databases into development/staging

--- terraform/db-replicate-job.tf
+++ terraform/db-replicate-job.tf
@@ -2,5 +2,16 @@ module "db-replicate-job" {
   source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.13.1"
 
   service_name = "db-replicate-job"
+  region       = var.region
+
+  cluster_name              = "trade-tariff-cluster-${var.environment}"
+  subnet_ids                = data.aws_subnets.private.ids
+  security_groups           = [data.aws_security_group.this.id]
+  cloudwatch_log_group_name = "platform-logs-${var.environment}"
+
   service_count = 0
+  max_capacity  = 1
+
+  docker_image = local.ecr_repo
+  docker_tag   = var.docker_tag
 }
