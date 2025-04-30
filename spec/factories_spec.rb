54de32148660e1f79850534e1c23b5c39e103968
Neil Middleton <neil.middleton@transformuk.com>
2025-04-30 11:33:09 +0100

    Remove pointless spec

--- spec/factories_spec.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.factories.map(&:name).each do |factory_name|
-  RSpec.describe "#{factory_name} factory" do
-    it 'is valid' do
-      factory = build factory_name
-      expect(factory).to be_valid if factory.respond_to?(:valid?)
-    end
-  end
-end
