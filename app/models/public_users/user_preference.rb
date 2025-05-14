49cb5095e7d941b25c7fc13ffc7e7401bacb4cfa
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-05-14 16:54:47 +0100

    Updated module from MyOtt to PublicUsers to make it more generic

--- app/models/public_users/user_preference.rb
+++ app/models/public_users/user_preference.rb
@@ -1,8 +1,6 @@
-module MyOtt
+module PublicUsers
   class UserPreference < Sequel::Model(Sequel[:user_preferences].qualify(:public))
     plugin :auto_validations
     plugin :timestamps, update_on_create: true
-
-    many_to_one :user_subscriptions, class: 'PublicUsers::Subscription', key: :user_id
   end
 end
