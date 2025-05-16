3740af13c44051d580adb510fb19a6d3ef8183c1
William Fish <william.michael.fish@gmail.com>
2025-05-16 12:02:46 +0100

    HMRC-1115: Adds handling for expired search reference search suggestions
    This adds handling for search references that were deleted from the
    search references table but still appear in the search suggestions table

--- app/services/search_suggestion_populator_service.rb
+++ app/services/search_suggestion_populator_service.rb
@@ -26,13 +26,18 @@ class SearchSuggestionPopulatorService
 
   private
 
+  def clear_old_suggestions
+    handle_expired_goods_nomenclature
+    handle_expired_search_references
+  end
+
   # A search suggestion always points to an associated goods nomenclature
   #
   # This goods nomenclature may expire or be replaced by a new goods nomenclature
   # and the suggestions point to now-expired goods nomenclatures
   #
   # This method clears out these expired suggestions
-  def clear_old_suggestions
+  def handle_expired_goods_nomenclature
     candidate_goods_nomenclature_sids = SearchSuggestion.goods_nomenclature_type.select_map(:id)
 
     all_goods_nomenclature_sids = GoodsNomenclature
@@ -51,6 +56,22 @@ class SearchSuggestionPopulatorService
     SearchSuggestion
       .where(id: expired_goods_nomenclature_sids.map(&:to_s))
       .goods_nomenclature_type
+      .delete
+  end
+
+  # NOTE: Search references get deleted when the classification team decides to move
+  # them to a different goods nomenclature. They also get deleted when goods_nomenclature that are associated with them are expired by the ClearInvalidSearchReferences job
+  #
+  # This method clears out search suggestions that are no longer valid
+  def handle_expired_search_references
+    current_suggestions = SearchSuggestion.search_reference_type.pluck(:id)
+    current_search_references = SearchReference.pluck(:id).map(&:to_s)
+    expired_search_references = current_suggestions - current_search_references
+
+    Rails.logger.info "Deleting #{expired_search_references.count} expired search references"
+
+    SearchSuggestion
+      .where(id: expired_search_references.map(&:to_s))
       .delete
   end
 end
