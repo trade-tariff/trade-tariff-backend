5ecd8ee39375411ee6a654ee2f902b698784cee6
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-05-16 16:13:15 +0100

    enable factories

--- spec/factories/public_users/subscription.rb
+++ spec/factories/public_users/subscription.rb
@@ -1,8 +1,7 @@
-# FactoryBot.define do
-#   factory :user_subscription, class: 'PublicUsers::Subscription' do
-#     user factory: :public_user
-#     subscription_type
-#     active { true }
-#     email { true }
-#   end
-# end
+FactoryBot.define do
+  factory :user_subscription, class: 'PublicUsers::Subscription' do
+    user_id { create(:public_user).id }
+    subscription_type_id { create(:subscription_type).id }
+    active { true }
+  end
+end
