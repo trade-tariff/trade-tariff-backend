83fca89af87c1c7060a915e75c1ef9f3c02b0acb
William Fish <william.michael.fish@gmail.com>
2025-05-28 12:13:17 +0100

    HMRC-794: Fixes data migration generator

--- lib/generators/data_migration_generator.rb
+++ lib/generators/data_migration_generator.rb
@@ -1,41 +1,39 @@
 require 'rails/generators'
 
-module Generators
-  class DataMigrationGenerator < Rails::Generators::NamedBase
-    class IllegalMigrationNameError < StandardError
-      def initialize(name)
-        super("Illegal name for migration file: #{name} (only lower case letters, numbers, and '_' allowed)")
-      end
+class DataMigrationGenerator < Rails::Generators::NamedBase
+  class IllegalMigrationNameError < StandardError
+    def initialize(name)
+      super("Illegal name for migration file: #{name} (only lower case letters, numbers, and '_' allowed)")
     end
+  end
 
-    def create_data_migration_file
-      validate_file_name!
+  def create_data_migration_file
+    validate_file_name!
 
-      create_file data_migration_file_name, <<~EODATAMIGRATION
-        Sequel.migration do
-          # IMPORTANT! Data migrations up block should be idempotent (reruns of up should produce the same effect)
-          # they may get re-run as part of data rollbacks but the rollback (down) function of the data migration will not get invoked
-          up do
+    create_file data_migration_file_name, <<~EODATAMIGRATION
+      Sequel.migration do
+        # IMPORTANT! Data migrations up block should be idempotent (reruns of up should produce the same effect)
+        # they may get re-run as part of data rollbacks but the rollback (down) function of the data migration will not get invoked
+        up do
 
-          end
-
-          down do
-
-          end
         end
-      EODATAMIGRATION
-    end
 
-    def data_migration_file_name
-      "db/data_migrations/#{timestamp}_#{file_name}.rb"
-    end
+        down do
 
-    def timestamp
-      Time.zone.now.utc.strftime('%Y%m%d%H%M%S')
-    end
+        end
+      end
+    EODATAMIGRATION
+  end
 
-    def validate_file_name!
-      raise IllegalMigrationNameError, file_name unless /^[_a-z0-9]+$/.match?(file_name)
-    end
+  def data_migration_file_name
+    "db/data_migrations/#{timestamp}_#{file_name}.rb"
+  end
+
+  def timestamp
+    Time.zone.now.utc.strftime('%Y%m%d%H%M%S')
+  end
+
+  def validate_file_name!
+    raise IllegalMigrationNameError, file_name unless /^[_a-z0-9]+$/.match?(file_name)
   end
 end
