757f401c2c9ac2c00134fb0bf722b2a8e0b9d15b
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-06-06 15:39:37 +0100

    Add action log with registered and subscribed actions

--- app/models/public_users/action_log.rb
+++ app/models/public_users/action_log.rb
@@ -1,8 +1,18 @@
 module PublicUsers
   class ActionLog < Sequel::Model(Sequel[:user_action_logs].qualify(:public))
+    REGISTERED = 'registered'.freeze
+    SUBSCRIBED = 'subscribed'.freeze
+
+    ALLOWED_ACTIONS = [REGISTERED, SUBSCRIBED].freeze
+
     plugin :auto_validations
     plugin :timestamps, update_on_create: true
 
     many_to_one :user, class: 'PublicUsers::User'
+
+    def validate
+      super
+      errors.add(:action, 'is not valid') unless ALLOWED_ACTIONS.include?(action)
+    end
   end
 end
