70b8197f0770386b469c469c9748cdfa274d2a00
rasika.abeyrathna <rasikasri@gmail.com>
2025-06-09 14:39:56 +0100

    HMRC-1141: Create Admin UI element to allow MTypes to be added for automation

--- db/migrate/20250527104818_create_green_lanes_identified_measure_type_category_assessments.rb
+++ db/migrate/20250527104818_create_green_lanes_identified_measure_type_category_assessments.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sequel.migration do
-  change do
+  up do
     create_table :green_lanes_identified_measure_type_category_assessments do
       primary_key :id
       String      :measure_type_id, size: 6, index: true, unique: true, null: false
@@ -9,5 +9,13 @@ Sequel.migration do
       Time        :created_at, null: false
       Time        :updated_at, null: false
     end
+  end
+
+  down do
+    alter_table :green_lanes_identified_measure_type_category_assessments do
+      drop_foreign_key :theme_id
+    end
+
+    drop_table :green_lanes_identified_measure_type_category_assessments
   end
 end
