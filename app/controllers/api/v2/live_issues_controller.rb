862d2163837da49abec77a6052ed09fdc53f684a
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-06-12 17:26:12 +0100

    Add filtering for v2 index

--- app/controllers/api/v2/live_issues_controller.rb
+++ app/controllers/api/v2/live_issues_controller.rb
@@ -2,17 +2,29 @@ module Api
   module V2
     class LiveIssuesController < ApiController
       def index
-        render json: serialize(live_issues)
+        if params[:filter].present?
+          render json: serialize(live_issues.where(permitted_filter).all) and return
+        end
+
+        render json: serialize(live_issues.all)
       end
 
       private
 
         def live_issues
-          @live_issues ||= LiveIssue.all
+          @live_issues ||= LiveIssue.dataset
         end
 
         def serialize(*args)
           Api::V2::LiveIssueSerializer.new(*args).serializable_hash
+        end
+
+        def serialize_errors(live_issue)
+          Api::V2::ErrorSerializationService.new(live_issue).call
+        end
+
+        def permitted_filter
+          params.require(:filter).permit(:status).to_h.symbolize_keys
         end
     end
   end
