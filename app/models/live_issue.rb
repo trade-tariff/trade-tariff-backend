57a7ca472f4d18f4fa7ddd1df82fad4f78fa8c15
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-06-20 16:50:34 +0100

    Remove redundant validation

--- app/models/live_issue.rb
+++ app/models/live_issue.rb
@@ -5,8 +5,6 @@ class LiveIssue < Sequel::Model(Sequel[:live_issues].qualify(:public))
   def validate
     super
 
-    validates_presence %i[title status commodities date_discovered]
-    validates_max_length 256, %i[description suggested_action]
     validates_includes %w[Active Resolved], :status
   end
 end
