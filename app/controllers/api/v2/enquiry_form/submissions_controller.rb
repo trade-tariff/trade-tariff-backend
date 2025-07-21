6377c7b71b2fb2c9247c4e379d0f7fb81754f2bc
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-07-21 13:36:20 +0100

    Split actions into admin and v2

---
+++ app/controllers/api/v2/enquiry_form/submissions_controller.rb
@@ -0,0 +1,22 @@
+class Api::V2::EnquiryForm::SubmissionsController < ApiController
+  def create
+    @submission = EnquiryForm::Submission.new(submission_params)
+    csv_data = EnquiryForm::CsvGeneratorService.new(submission_params).generate
+
+    EnquiryForm::CsvUploaderService.new(@submission, csv_data).upload
+    # EnquiryForm::SubmissionMailer.send_email(@submission).deliver_later
+
+
+    if @submission.valid? && @submission.save
+      render json: @submission.reference_number, status: :created
+    else
+      render json: @submission.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:data)
+  end
+end
