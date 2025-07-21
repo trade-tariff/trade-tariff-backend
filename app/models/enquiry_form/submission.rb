dbf0ccf1c23f7011a3ec4c04375ca5947caa2291
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-07-21 12:35:51 +0100

    Update submission model and add spec

--- app/models/enquiry_form/submission.rb
+++ app/models/enquiry_form/submission.rb
@@ -1,20 +1,21 @@
-class EnquiryForm::Submission < Sequel::Model(Sequel[:enquiry_forms].qualify(:public))
+class EnquiryForm::Submission < Sequel::Model(Sequel[:enquiry_form_submissions].qualify(:public))
   plugin :timestamps, update_on_create: true
   plugin :auto_validations, not_null: :presence
 
   # Exlude O and I as they're usually avoided in GOV.UK reference numbers
   CHARSET = ('A'..'Z').to_a + (0..9).to_a.map(&:to_s) - ['O', 'I']
 
+  def before_validation
+    super
+
+    self.reference_number ||= create_reference_number
+    self.email_status ||= 'Pending'
+  end
+
   def validate
     super
 
     validates_includes %w[Pending Sent Failed], :email_status
-  end
-
-  def before_create
-    super
-
-    self.reference_number ||= create_reference_number
   end
 
   def before_save
@@ -28,7 +29,9 @@ class EnquiryForm::Submission < Sequel::Model(Sequel[:enquiry_forms].qualify(:pu
   def create_reference_number(length = 8)
     loop do
       reference_number = CHARSET.sample(length).join
-      unless EnquiryForm::Submission.where(reference_number: reference_number).exists?
+
+      # returns nil if no record
+      unless EnquiryForm::Submission.where(reference_number: reference_number).first
         return reference_number
       end
     end
