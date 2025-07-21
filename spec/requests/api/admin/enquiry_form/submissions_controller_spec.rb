4133c1eefdb123200424c33d05ed2ce6aa1ef224
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-07-21 13:36:20 +0100

    Split actions into admin and v2

--- spec/requests/api/admin/enquiry_form/submissions_controller_spec.rb
+++ spec/requests/api/admin/enquiry_form/submissions_controller_spec.rb
@@ -1,19 +1,11 @@
 RSpec.describe Api::Admin::EnquiryForm::SubmissionsController, type: :request do
-  let(:form_submission_data) do
-    {
-      name: "John Johnson",
-      company_name: "Company LTD",
-      job_title: "Partnerships Manager",
-      email: "john.johnson@example.com",
-      enquiry_category: "Quotas",
-      enquiry_description: "How much quota do you have left for this commodity code?",
-    }
-  end
-  let(:enquiry_form_submission) { create(:enquiry_form_submission, enquiry_form: enquiry_form) }
+let(:enquiry_form_submission) { create('enquiry_form_submission') }
 
   describe "GET index" do
     it "returns a successful 200 response" do
-      get "/admin/enquiry_forms/submissions"
+      enquiry_form_submission
+
+      authenticated_get "/admin/enquiry_form/submissions"
 
       expect(response).to have_http_status(:ok)
     end
