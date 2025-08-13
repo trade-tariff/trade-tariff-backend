235e9f4e1f7fabef2c3661d50e36976fb6949bda
rasika.abeyrathna <rasikasri@gmail.com>
2025-08-13 11:31:00 +0100

    HMRC-1404: Basic auth to admin backends when auth disabled - add bearer token auth for admin api call

--- app/controllers/api/admin/admin_controller.rb
+++ app/controllers/api/admin/admin_controller.rb
@@ -3,6 +3,27 @@ module Api
     class AdminController < ApiController
       include GDS::SSO::ControllerMethods
       include AdminApi.routes.url_helpers
+      include ActionController::HttpAuthentication::Token::ControllerMethods
+
+      def authenticate_user!
+        if TradeTariffBackend.disable_admin_api_authentication?
+          if TradeTariffBackend.admin_api_bearer_token.blank?
+            render json: { error: 'Admin API Key is not valid' }, status: :internal_server_error
+            return
+          end
+          authenticate_with_bearer_token
+        else
+          super
+        end
+      end
+
+      def authenticate_with_bearer_token
+        authenticate_or_request_with_http_token do |provided_token, _options|
+          ActiveSupport::SecurityUtils.secure_compare(provided_token, TradeTariffBackend.admin_api_bearer_token)
+        end
+
+        true
+      end
 
       no_caching
     end
