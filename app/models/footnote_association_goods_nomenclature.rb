524034f3c27a0bfc3e1223eac9d4d0854535112c
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-09-03 12:46:06 +0100

    Add and fix association links

--- app/models/footnote_association_goods_nomenclature.rb
+++ app/models/footnote_association_goods_nomenclature.rb
@@ -5,4 +5,9 @@ class FootnoteAssociationGoodsNomenclature < Sequel::Model
                                  goods_nomenclature_sid]
 
   set_primary_key %i[footnote_id footnote_type goods_nomenclature_sid]
+
+  one_to_one :footnote, key: %i[footnote_id footnote_type_id],
+                        primary_key: %i[footnote_id footnote_type]
+  one_to_one :goods_nomenclature, key: :goods_nomenclature_sid,
+                                  primary_key: :goods_nomenclature_sid
 end
