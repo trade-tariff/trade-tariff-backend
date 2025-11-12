f56d6449c5695d6eb0887783e3a48e92d191ae08
William Fish <william.michael.fish@gmail.com>
2025-11-12 11:41:45 +0000

    HMRC-1742: Adds report generation for category assessments
    Includes some useful helpers for zipping data and instrumentation for
    N+1s in the reporting modules

--- app/lib/reporting/reportable.rb
+++ app/lib/reporting/reportable.rb
@@ -25,5 +25,32 @@ module Reporting
     def now
       Time.zone.today
     end
+
+    def zip(data, filename)
+      buffer = Zip::OutputStream.write_buffer { |out|
+        out.put_next_entry(filename)
+        out.write data
+      }.tap(&:rewind)
+
+      {
+        data: buffer.read,
+        filename: filename.gsub(File.extname(filename), '.zip'),
+        content_type: 'application/zip',
+      }
+    end
+
+    def instrument(label = "#{self.class.name}##{__method__}")
+      ::SequelRails::Railties::LogSubscriber.reset_runtime
+      ::SequelRails::Railties::LogSubscriber.reset_count
+      ::SequelRails::Railties::LogSubscriber.reset_tables
+      start_time = Time.zone.now
+      yield if block_given?
+    ensure
+      end_time = Time.zone.now
+      duration = end_time - start_time
+      Rails.logger.info("#{label} Total Time: #{duration.round(2)} seconds")
+      Rails.logger.info("#{label} SQL Queries: #{::SequelRails::Railties::LogSubscriber.count}, Total SQL Time: #{::SequelRails::Railties::LogSubscriber.runtime.round(2)} ms")
+      Rails.logger.info("#{label} SQL Tables: #{::SequelRails::Railties::LogSubscriber.tables}")
+    end
   end
 end
