79692079091234b060d3137aae0518daa805fa58
rasika.abeyrathna <rasikasri@gmail.com>
2025-11-17 17:49:28 +0000

    BAU_: HMRC needs to receive cds update email for empty cds files as well

--- app/workers/cds_update_notification_worker.rb
+++ app/workers/cds_update_notification_worker.rb
@@ -6,7 +6,9 @@ class CdsUpdateNotificationWorker
   def perform(notification_id)
     if TradeTariffBackend.uk?
       notification = CdsUpdateNotification.find(id: notification_id)
-      CdsImporter.new(notification.cds_update, handler_classes: [CdsImporter::ExcelWriter]).import
+      if notification.present?
+        CdsImporter.new(notification.cds_update, handler_classes: [CdsImporter::ExcelWriter]).import
+      end
     end
   end
 end
