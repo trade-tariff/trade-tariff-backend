c5fad0300cbeeb5677ed55b36469e765da7cdfc9
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-11-26 17:05:20 +0000

    Add API for commodity changes

--- app/services/api/user/commodity_changes_service.rb
+++ app/services/api/user/commodity_changes_service.rb
@@ -1,51 +1,65 @@
 module Api
   module User
     class CommodityChangesService
-      attr_reader :user, :date
+      attr_reader :user, :id, :date
 
-      def initialize(user, date = Time.zone.yesterday)
+      ALLOWED_IDS = %w[ending classification].freeze
+
+      def initialize(user, id = nil, date = Time.zone.yesterday)
         @user = user
+        @id = id
         @date = date
       end
 
       def call
-        [
-          commodity_endings,
-          classification_changes,
-        ].compact
+        if id.present? && ALLOWED_IDS.include?(id)
+          send(id, with_records: true)
+        else
+          all_changes
+        end
       end
 
       private
 
-      def commodity_endings
-        count = TariffChange.commodities
-                            .where(operation_date: date)
-                            .where(goods_nomenclature_sid: user_commodity_code_sids)
-                            .where(action: TariffChangesService::BaseChanges::ENDING)
-                            .count
+      def all_changes
+        [
+          ending,
+          classification,
+        ].compact
+      end
+
+      def ending(with_records: false)
+        changes = TariffChange.commodities
+                              .where(operation_date: date)
+                              .where(goods_nomenclature_sid: user_commodity_code_sids)
+                              .where(action: TariffChangesService::BaseChanges::ENDING)
+
+        count = changes.count
 
         return if count.zero?
 
-        OpenStruct.new({
-          id: 'commodity_endings',
+        TariffChanges::GroupedCommodityChange.new({
+          id: 'ending',
           description: 'Changes to end date',
           count: count,
+          tariff_changes: with_records ? changes.all : nil,
         })
       end
 
-      def classification_changes
-        count = TariffChange.commodity_descriptions
-                            .where(operation_date: date)
-                            .where(goods_nomenclature_sid: user_commodity_code_sids)
-                            .where(action: TariffChangesService::BaseChanges::UPDATE)
-                            .count
+      def classification(with_records: false)
+        changes = TariffChange.commodity_descriptions
+                              .where(operation_date: date)
+                              .where(goods_nomenclature_sid: user_commodity_code_sids)
+                              .where(action: TariffChangesService::BaseChanges::UPDATE)
+        count = changes.count
 
         return if count.zero?
 
-        OpenStruct.new({
-          id: 'classification_changes',
+        TariffChanges::GroupedCommodityChange.new({
+          id: 'classification',
           description: 'Changes to classification',
           count: count,
+          tariff_changes: with_records ? changes.all : nil,
         })
       end
 
