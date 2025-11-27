ac2855bf3e2f4afd98dc41ad9fb4ba7e8cd20764
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-11-27 13:04:01 +0000

    Changed SubscriptionsTarget Show to Index, created TargetsFilterService from refactored controller code and moved batcher services into the User modules.

--- app/controllers/api/user/subscription_targets_controller.rb
+++ app/controllers/api/user/subscription_targets_controller.rb
@@ -8,49 +8,19 @@ module Api
       before_action :authenticate!
       before_action :find_subscription
 
-      def show
+      def index
         return render json: { message: 'No token was provided' }, status: :unauthorized if @subscription.nil?
 
-        render json: serialize(targets_with_commodities)
+        filtered_targets = targets_filter_service.new(@subscription,
+                                                      filter_params[:active_commodities_type]&.to_sym,
+                                                      current_page, per_page).call
+
+        render json: serialize(filtered_targets)
+      rescue ArgumentError => e
+        render json: serialize_errors({ error: e.message }), status: :bad_request
       end
 
       private
-
-      def targets_with_commodities
-        type = filter_params[:active_commodities_type]&.to_sym
-
-        return [@subscription.subscription_targets.map, @subscription.subscription_targets.size] if type.blank?
-
-        TimeMachine.now do
-          service = Api::User::ActiveCommoditiesService.new(@subscription)
-          commodities, total =
-            if service.respond_to?("#{type}_commodities")
-              service.public_send("#{type}_commodities", page: current_page, per_page: per_page)
-            else
-              [[], 0]
-            end
-          targets = apply_commodities_to_subscription_targets(commodities)
-          [targets, total]
-        end
-      end
-
-      def apply_commodities_to_subscription_targets(commodities)
-        commodities.map do |commodity|
-          subscription_target_id =
-            find_target_id_by_goods_nomenclature_sid(commodity.goods_nomenclature_sid)
-
-          target = PublicUsers::SubscriptionTarget.new
-          target.virtual_id = subscription_target_id
-          target.target_type = 'commodity'
-          target.commodity = commodity
-          target
-        end
-      end
-
-      def find_target_id_by_goods_nomenclature_sid(sid)
-        subscription_target = @subscription.subscription_targets_dataset.where(target_id: sid).first
-        subscription_target&.id
-      end
 
       def filter_params
         params.fetch(:filter, {}).permit(:active_commodities_type)
@@ -58,7 +28,6 @@ module Api
 
       def serialize(targets_and_total)
         targets, total = targets_and_total
-
         serialized_targets = Api::User::SubscriptionTargetSerializer.new(targets).serializable_hash
 
         {
@@ -73,13 +42,24 @@ module Api
         }
       end
 
+      def serialize_errors(errors)
+        Api::User::ErrorSerializationService.new.serialized_errors(errors)
+      end
+
       def find_subscription
         @subscription = @current_user.subscriptions_dataset.where(uuid: subscription_id).first
         render json: { message: 'No subscription ID was provided' }, status: :unauthorized if @subscription.nil?
       end
 
       def subscription_id
-        params[:id]
+        params[:subscription_id]
+      end
+
+      def targets_filter_service
+        subscription_type_name = @subscription.subscription_type.name
+        "Api::User::TargetsFilterService::#{subscription_type_name.camelize}TargetsFilterService".constantize
+      rescue NameError
+        raise ArgumentError, "Unsupported subscription type for targets filtering: #{subscription_type_name}"
       end
     end
   end
--- spec/controllers/api/user/subscription_targets_controller_spec.rb
+++ spec/controllers/api/user/subscription_targets_controller_spec.rb
@@ -1,137 +1,92 @@
 RSpec.describe Api::User::SubscriptionTargetsController do
   routes { UserApi.routes }
 
-  let(:user_token) { 'Bearer tariff-api-test-token' }
-  let(:user_id) { 'user123' }
-  let(:user) { create(:public_user, external_id: user_id) }
-  let(:user_hash) { { 'sub' => user_id, 'email' => 'test@example.com' } }
-  let(:subscription) { create(:user_subscription, user: user) }
+  let(:user) { create(:public_user) }
+  let(:user_hash) { { 'sub' => user.external_id } }
+  let(:subscription) { create(:user_subscription, user: user, subscription_type: Subscriptions::Type.my_commodities) }
+
   let(:valid_subscription_id) { subscription.uuid }
-  let(:invalid_subscription_id) { SecureRandom.uuid }
 
   before do
-    request.headers['Authorization'] = user_token
+    request.headers['Authorization'] = 'Bearer token'
     allow(CognitoTokenVerifier).to receive(:verify_id_token).and_return(user_hash)
   end
 
-  describe 'GET #show' do
-    context 'with a valid subscription and no filter' do
-      let!(:subscription_targets) do
+  describe 'GET #index' do
+    context 'when service returns targets' do
+      let(:subscription_targets) do
         [
           create(:subscription_target, user_subscriptions_uuid: subscription.uuid, target_id: '123', target_type: 'commodity'),
           create(:subscription_target, user_subscriptions_uuid: subscription.uuid, target_id: '456', target_type: 'commodity'),
         ]
       end
+      let(:service_instance) { instance_double(Api::User::TargetsFilterService::MyCommoditiesTargetsFilterService) }
 
       before do
-        get :show, params: { id: valid_subscription_id }
+        allow(Api::User::TargetsFilterService::MyCommoditiesTargetsFilterService)
+          .to receive(:new)
+          .with(subscription, nil, 1, 10)
+          .and_return(service_instance)
+
+        allow(service_instance)
+          .to receive(:call)
+          .and_return([subscription_targets, 2])
+
+        get :index, params: { subscription_id: valid_subscription_id, page: 1, per_page: 10 }
       end
 
-      it 'returns a successful response' do
+      it 'returns status 200' do
         expect(response).to have_http_status(:ok)
       end
 
-      it 'returns all subscription targets without commodities' do
-        expected_targets = subscription_targets.map do |st|
-          target = PublicUsers::SubscriptionTarget.new
-          target.virtual_id = st.id
-          target.target_type = st.target_type
-          target.commodity = nil
-          target
-        end
+      it 'returns serialized targets with pagination metadata' do
+        json = JSON.parse(response.body)
 
-        serialized_data = Api::User::SubscriptionTargetSerializer.new(expected_targets).serializable_hash
+        serializer_output =
+          Api::User::SubscriptionTargetSerializer
+            .new(subscription_targets)
+            .serializable_hash
 
-        expected_response = {
-          data: serialized_data[:data],
-          meta: {
-            pagination: {
-              page: 1,
-              per_page: 20,
-              total_count: subscription_targets.size,
-            },
+        expect(json['data']).to eq(serializer_output[:data].as_json)
+
+        expect(json['meta']).to eq(
+          'pagination' => {
+            'page' => 1,
+            'per_page' => 10,
+            'total_count' => 2,
           },
-        }
-
-        expect(JSON.parse(response.body)).to eq(JSON.parse(expected_response.to_json))
+        )
       end
     end
 
-    context 'with active_commodities_type filter and pagination' do
-      let!(:commodity_1) { create(:commodity, goods_nomenclature_sid: 789, goods_nomenclature_item_id: '1234567890', producline_suffix: '80') }
-      let!(:commodity_2) { create(:commodity, goods_nomenclature_sid: 101, goods_nomenclature_item_id: '1234567891', producline_suffix: '80') }
-
-      let(:active_targets) do
-        [
-          create(:subscription_target, user_subscriptions_uuid: subscription.uuid, target_id: commodity_1.goods_nomenclature_sid, target_type: 'commodity'),
-          create(:subscription_target, user_subscriptions_uuid: subscription.uuid, target_id: commodity_2.goods_nomenclature_sid, target_type: 'commodity'),
-        ]
-      end
+    context 'when target filter service is not implemented' do
+      let(:subscription) { create(:user_subscription, user: user, subscription_type: Subscriptions::Type.stop_press) }
 
       before do
-        active_targets
-        service_instance = instance_double(Api::User::ActiveCommoditiesService)
-        allow(Api::User::ActiveCommoditiesService).to receive(:new).with(subscription).and_return(service_instance)
-        allow(service_instance).to receive(:respond_to?).with('active_commodities').and_return(true)
-        allow(service_instance).to receive(:active_commodities).and_return([[commodity_1, commodity_2], 2])
-
-        get :show, params: { id: valid_subscription_id, filter: { active_commodities_type: 'active' }, page: 1, per_page: 1 }
+        get :index, params: { subscription_id: valid_subscription_id }
       end
 
-      it 'returns paginated results' do
-        json = JSON.parse(response.body)
-        expect(json['meta']['pagination']).to include('page' => 1, 'per_page' => 1, 'total_count' => 2)
+      it 'returns a bad request response' do
+        expect(response).to have_http_status(:bad_request)
       end
 
-      it 'returns only commodities with producline_suffix 80' do
-        json = JSON.parse(response.body)
-        suffixes = json['data'].map { |d| d['attributes']['producline_suffix'] }
-        expect(suffixes).to all(eq('80'))
-      end
-
-      it 'calls ActiveCommoditiesService with the subscription' do
-        expect(Api::User::ActiveCommoditiesService).to have_received(:new).with(subscription)
+      it 'renders an error message' do
+        expect(response.body).to eq({ errors: [{ detail: 'Unsupported subscription type for targets filtering: stop_press' }] }.to_json)
       end
     end
 
-    context 'with unknown filter value' do
-      before do
-        service_instance = instance_double(Api::User::ActiveCommoditiesService)
-        allow(Api::User::ActiveCommoditiesService).to receive(:new).with(subscription).and_return(service_instance)
-        allow(service_instance).to receive(:call).and_return({})
-
-        get :show, params: { id: valid_subscription_id, filter: { active_commodities_type: 'inactive' } }
-      end
-
-      it 'returns an empty data array' do
-        expect(JSON.parse(response.body)).to eq({ 'data' => [], 'meta' => { 'pagination' => { 'page' => 1, 'per_page' => 20, 'total_count' => 0 } } })
-      end
-    end
-
-    context 'with an invalid subscription ID' do
-      before { get :show, params: { id: invalid_subscription_id } }
-
+    context 'when subscription is missing' do
       it 'returns unauthorized' do
+        get :index, params: { subscription_id: SecureRandom.uuid }
         expect(response).to have_http_status(:unauthorized)
       end
-
-      it 'renders error message' do
-        expect(response.body).to eq({ message: 'No subscription ID was provided' }.to_json)
-      end
     end
 
-    context 'without authorization token' do
-      before do
+    context 'when no auth token provided' do
+      it 'returns unauthorized' do
         request.headers['Authorization'] = nil
-        get :show, params: { id: valid_subscription_id }
-      end
-
-      it 'returns unauthorized' do
+        get :index, params: { subscription_id: valid_subscription_id }
         expect(response).to have_http_status(:unauthorized)
-      end
-
-      it 'renders error message' do
-        expect(response.body).to eq({ message: 'No bearer token was provided' }.to_json)
       end
     end
   end
