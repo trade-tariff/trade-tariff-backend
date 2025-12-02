385ecf6b45160e7880cd934be3173ab68cc86453
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-12-02 16:12:06 +0000

    Make NullCommodity conform to Commodity

--- app/models/public_users/null_commodity.rb
+++ app/models/public_users/null_commodity.rb
@@ -1,14 +1,29 @@
 module PublicUsers
   class NullCommodity
-    attr_reader :goods_nomenclature_item_id, :invalid
+    attr_reader :goods_nomenclature_item_id
 
-    def initialize(goods_nomenclature_item_id:, invalid: true)
+    def initialize(goods_nomenclature_item_id:)
       @goods_nomenclature_item_id = goods_nomenclature_item_id
-      @invalid = invalid
     end
 
     def id
-      goods_nomenclature_item_id
+      "null_#{goods_nomenclature_item_id}"
+    end
+
+    def classification_description
+      ''
+    end
+
+    def validity_end_date
+      nil
+    end
+
+    def chapter_short_code
+      nil
+    end
+
+    def heading
+      nil
     end
   end
 end
--- app/serializers/api/user/subscription_target_serializer.rb
+++ app/serializers/api/user/subscription_target_serializer.rb
@@ -8,7 +8,7 @@ module Api
       belongs_to :target_object,
                  record_type: :commodity,
                  serializer: Api::User::SubscriptionTarget::CommoditySerializer do |target|
-        target.commodity if target.commodity && !target.commodity.is_a?(PublicUsers::NullCommodity)
+        target.commodity if target.commodity
       end
     end
   end
