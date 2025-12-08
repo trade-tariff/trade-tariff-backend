096015b5d40c4eed9915c3e4469838b94aa3bd41
William Fish <william.michael.fish@gmail.com>
2025-12-08 13:02:09 +0000

    BAU: Log json in sidekiq
    Currently we do coloured terminal logging in production and its
    hard/impossible to use cloudwatch insights with this

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,6 +7,7 @@ Sidekiq.strict_args!
 Sidekiq.configure_server do |config|
   config.redis = TradeTariffBackend.redis_config
   config[:job_logger] = ::CustomJobLogger
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new if Rails.env.production?
 end
 
 Sidekiq.configure_client do |config|
