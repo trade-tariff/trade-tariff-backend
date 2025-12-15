a7e1b12a30a102906c199a98798d09462640dc90
rasika.abeyrathna <rasikasri@gmail.com>
2025-12-15 13:18:53 +0000

    HMRC-1684: Stop run-task job after completion

--- terraform/backend_xi.tf
+++ terraform/backend_xi.tf
@@ -1,5 +1,5 @@
 module "backend_xi" {
-  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.18.2"
+  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-snapshot-v1.19.0"
 
   region = var.region
 
