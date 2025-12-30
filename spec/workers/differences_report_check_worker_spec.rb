55218d8847d8c1f1d150269f01d60e9577d14bad
William Fish <william.michael.fish@gmail.com>
2025-12-30 09:31:26 +0000

    BAU: Refactor DifferencesReportCheckWorker
    - Inline small private methods for better readability.
    - Update spec to correctly mock TradeTariffBackend environment checks.
    - Make sure we're skipping checks in the XI service

--- spec/workers/differences_report_check_worker_spec.rb
+++ spec/workers/differences_report_check_worker_spec.rb
@@ -2,8 +2,11 @@ RSpec.describe DifferencesReportCheckWorker, type: :worker do
   subject(:worker) { described_class.new }
 
   describe '#perform' do
+    let(:environment) { ActiveSupport::StringInquirer.new('production') }
+
     before do
       allow(SlackNotifierService).to receive(:call).and_call_original
+      allow(TradeTariffBackend).to receive_messages(environment: environment, uk?: true)
     end
 
     it 'is happy if the differences report has run this week' do
@@ -13,7 +16,6 @@ RSpec.describe DifferencesReportCheckWorker, type: :worker do
     end
 
     it 'is unhappy if the differences report has not run this week' do
-      allow(ENV).to receive(:fetch).and_return('production')
       DifferencesLog.create(date: 10.days.ago, key: 'foo', value: 'foo')
       worker.perform
       expect(SlackNotifierService).to have_received(:call)
