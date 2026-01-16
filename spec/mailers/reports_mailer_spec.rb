0d1e058298d1bda4ded449b910e329dcb286a150
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2026-01-16 15:38:17 +0000

    Remove delta report service

--- spec/mailers/reports_mailer_spec.rb
+++ spec/mailers/reports_mailer_spec.rb
@@ -10,33 +10,6 @@ RSpec.describe ReportsMailer, type: :mailer do
     it { expect(mail.bcc).to eq(['user@example.com']) }
   end
 
-  describe '#delta' do
-    subject(:mail) { described_class.delta(report).tap(&:deliver_now) }
-
-    let(:start_date) { Time.zone.today.strftime('%Y_%m_%d') }
-    let(:report) do
-      {
-        dates: start_date,
-        total_records: 2,
-        commodity_changes: [
-          { commodity_code: '0101000000', type: 'Measure' },
-          { commodity_code: '0102000000', type: 'GoodsNomenclature' },
-        ],
-        package: Axlsx::Package.new,
-      }
-    end
-
-    before do
-      allow(TradeTariffBackend).to receive(:delta_report_to_emails).and_return('delta@example.com')
-      allow(DeltaReportService).to receive(:generate).and_return(report)
-    end
-
-    it { expect(mail.subject).to eq("[HMRC Online Trade Tariff] - UK tariff changes report #{start_date}") }
-    it { expect(mail.from).to eq(['no-reply@example.com']) }
-    it { expect(mail.to).to eq(['delta@example.com']) }
-    it { expect(mail.bcc).to be_nil }
-  end
-
   describe '#commodity_watchlist' do
     subject(:mail) { described_class.commodity_watchlist(date, package).tap(&:deliver_now) }
 
