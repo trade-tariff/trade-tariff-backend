7706582ee8cc202fc4a2323363c2ed0ca57c6980
William Fish <william.michael.fish@gmail.com>
2026-02-10 17:05:50 +0000

    AI-148: Accept search request_id via header to avoid cache key pollution

--- app/controllers/concerns/search_result_tracking.rb
+++ app/controllers/concerns/search_result_tracking.rb
@@ -1,15 +1,22 @@
 module SearchResultTracking
   extend ActiveSupport::Concern
 
+  SEARCH_REQUEST_ID_HEADER = 'X-Search-Request-Id'.freeze
+
   private
 
   def track_result_selected
-    return if params[:request_id].blank?
+    rid = search_request_id
+    return if rid.blank?
 
     ::Search::Instrumentation.result_selected(
-      request_id: params[:request_id],
+      request_id: rid,
       goods_nomenclature_item_id: params[:id],
       goods_nomenclature_class: controller_name.classify,
     )
+  end
+
+  def search_request_id
+    request.headers[SEARCH_REQUEST_ID_HEADER].presence || params[:request_id].presence
   end
 end
