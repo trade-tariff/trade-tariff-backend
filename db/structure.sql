b64772184dfc26fbbacfc05a5c00a66de15be6e0
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2026-03-10 12:57:44 +0000

    Add pgcrypto extension

--- db/structure.sql
+++ db/structure.sql
@@ -41,6 +41,20 @@ CREATE SCHEMA uk;
 
 
 --
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
+--
 -- Name: citext; Type: EXTENSION; Schema: -; Owner: -
 --
 
