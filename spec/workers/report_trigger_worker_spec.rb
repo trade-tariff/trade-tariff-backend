a7fd2a3f9f8b419619f395f38ed1fc74c30fddbd
William Fish <william.michael.fish@gmail.com>
2026-03-19 16:45:06 +0000

    HMRC-2126: Queue manual report triggers within one hour

--- spec/workers/report_trigger_worker_spec.rb
+++ spec/workers/report_trigger_worker_spec.rb
@@ -1,6 +1,12 @@
 RSpec.describe ReportTriggerWorker, type: :worker do
   subject(:worker) { described_class.new }
 
+  describe 'sidekiq configuration' do
+    it 'uses the within_1_hour queue' do
+      expect(described_class.get_sidekiq_options['queue']).to eq(:within_1_hour)
+    end
+  end
+
   describe '#perform' do
     before do
       allow(TradeTariffBackend).to receive(:service).and_return('uk')
