4361348cbf1d4b6d237085d13467c1b2830bff21
William Fish <william.michael.fish@gmail.com>
2026-03-20 14:34:55 +0000

    BAU: Scope legacy table cleanup to current service schema

--- db/migrate/20260317130000_remove_unused_legacy_tables.rb
+++ db/migrate/20260317130000_remove_unused_legacy_tables.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 Sequel.migration do
+  LEGACY_TABLES = %i[
+    chapters_guides
+    clear_caches
+    exchange_rate_countries
+    exchange_rate_currencies
+    tariff_update_conformance_errors
+    users
+  ].freeze
+
   up do
-    %i[
-      chapters_guides
-      clear_caches
-      exchange_rate_countries
-      exchange_rate_currencies
-      tariff_update_conformance_errors
-      users
-    ].each do |table|
-      drop_table?(table, cascade: true)
+    # These are legacy service-schema tables only.
+    # Keep public.users, which backs the current public user/subscription models.
+    service_schema = TradeTariffBackend.service.to_sym
+
+    LEGACY_TABLES.each do |table|
+      drop_table?(Sequel[table].qualify(service_schema), cascade: true)
     end
   end
 
