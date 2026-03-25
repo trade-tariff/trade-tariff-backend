42c0f08c5a17fcdf67b613f5623034a9e144a2ab
William Fish <william.michael.fish@gmail.com>
2026-03-25 10:29:23 +0000

    HMRC-2136: Duck-type export refund nomenclature for metadata

--- spec/models/export_refund_nomenclature_spec.rb
+++ spec/models/export_refund_nomenclature_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe ExportRefundNomenclature do
       ).to eq "#{export_refund_nomenclature.additional_code_type}#{export_refund_nomenclature.export_refund_code}"
     end
   end
+
+  describe '#applicable?' do
+    subject(:export_refund_nomenclature) { build :export_refund_nomenclature }
+
+    it { is_expected.to be_applicable }
+  end
+
+  describe '#type' do
+    subject(:export_refund_nomenclature) { build :export_refund_nomenclature }
+
+    it 'returns a placeholder type for additional code consumers' do
+      expect(export_refund_nomenclature.type).to eq('export_refund_nomenclature')
+    end
+  end
 end
