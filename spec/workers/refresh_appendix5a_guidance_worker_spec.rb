7bdbb5e49c2fb1f5c7485b1298420aad9f1d5a3b
William Fish <william.michael.fish@gmail.com>
2026-04-02 09:35:04 +0100

    BAU: Fixes failing test

--- spec/workers/refresh_appendix5a_guidance_worker_spec.rb
+++ spec/workers/refresh_appendix5a_guidance_worker_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe RefreshAppendix5aGuidanceWorker, type: :worker do
   let(:worker) { described_class.new }
+  let(:service) { instance_double(Appendix5aPopulatorService, call: true) }
 
   describe '#perform' do
     include_context 'with a stubbed appendix 5a guidance s3 bucket'
 
     before do
-      allow(Appendix5aPopulatorService).to receive(:new).and_call_original
-      allow(Appendix5aMailer).to receive_message_chain(:appendix5a_notify_message, :deliver_now)
+      allow(Appendix5aPopulatorService).to receive(:new).and_return(service)
     end
 
     it 'calls the service' do
       worker.perform
 
       expect(Appendix5aPopulatorService).to have_received(:new)
+      expect(service).to have_received(:call)
     end
   end
 end
