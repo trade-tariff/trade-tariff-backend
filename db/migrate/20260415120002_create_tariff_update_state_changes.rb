eca6f719b5ddeb3bd2951d734352c455425c68f4
Neil Middleton <neil.middleton@transformuk.com>
2026-04-15 17:25:47 +0100

    Make create_tariff_update_state_changes idempotent with create_table?

--- db/migrate/20260415120002_create_tariff_update_state_changes.rb
+++ db/migrate/20260415120002_create_tariff_update_state_changes.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   change do
-    create_table :tariff_update_state_changes do
+    create_table?(:tariff_update_state_changes) do
       primary_key :id
       String :tariff_update_filename, null: false
       String :from_state, size: 1
