8408a96a1c43903cf059f3608d838c6ac51d9c95
William Fish <william.michael.fish@gmail.com>
2026-07-08 20:42:59 +0100

    fix(rubocop): shorten auth concern blocks
    Issue: No ticket/issue

--- app/controllers/concerns/public_user_authenticatable.rb
+++ app/controllers/concerns/public_user_authenticatable.rb
@@ -9,57 +9,56 @@ module PublicUserAuthenticatable
     missing_jwks_keys: 'Unable to verify token',
   }.freeze
 
-  included do
-  private
+  attr_reader :current_user
 
-    attr_reader :current_user
-
-    def authenticate!
-      if Rails.env.development?
-        @current_user ||= Api::User::DummyUserService.find_or_create
-        return
-      end
-
-      if user_token.present?
-        result = Api::User::UserService.find(user_token)
-
-        if result.is_a?(CognitoTokenVerifier::Result)
-          return render_authentication_error(result.reason)
-        end
-
-        if result.nil?
-          return render_not_found_error
-        end
-
-        @current_user = result
-      else
-        render_authentication_error(:missing_token)
-      end
+  def authenticate!
+    if Rails.env.development?
+      @current_user ||= Api::User::DummyUserService.find_or_create
+      return
     end
 
-    def render_authentication_error(reason)
-      error_detail = AUTHENTICATION_ERROR_MESSAGES[reason] || 'Authentication failed'
-
-      render json: serialize_authentication_error(error_detail, reason), status: :unauthorized
+    if user_token.present?
+      authenticate_user_token
+    else
+      render_authentication_error(:missing_token)
     end
+  end
 
-    def render_not_found_error
-      render json: serialize_authentication_error('User not found', :not_found), status: :not_found
-    end
+  private :authenticate!, :current_user
 
-    def serialize_authentication_error(detail, reason)
-      {
-        errors: [{
-          detail:,
-          code: reason.to_s,
-        }],
-      }.to_json
-    end
+private
 
-    def user_token
-      pattern = /^Bearer /
-      header = request.headers['Authorization']
-      header.gsub(pattern, '') if header&.match(pattern)
-    end
+  def authenticate_user_token
+    result = Api::User::UserService.find(user_token)
+
+    return render_authentication_error(result.reason) if result.is_a?(CognitoTokenVerifier::Result)
+    return render_not_found_error if result.nil?
+
+    @current_user = result
+  end
+
+  def render_authentication_error(reason)
+    error_detail = AUTHENTICATION_ERROR_MESSAGES[reason] || 'Authentication failed'
+
+    render json: serialize_authentication_error(error_detail, reason), status: :unauthorized
+  end
+
+  def render_not_found_error
+    render json: serialize_authentication_error('User not found', :not_found), status: :not_found
+  end
+
+  def serialize_authentication_error(detail, reason)
+    {
+      errors: [{
+        detail:,
+        code: reason.to_s,
+      }],
+    }.to_json
+  end
+
+  def user_token
+    pattern = /^Bearer /
+    header = request.headers['Authorization']
+    header.gsub(pattern, '') if header&.match(pattern)
   end
 end
