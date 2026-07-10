d479fb687b01c841afc7dd8dd318a4e46855ea3e
William Fish <william.michael.fish@gmail.com>
2026-07-10 14:37:16 +0100

    test(oplog): fix write_oplog_operation! example attributes
    Jira: HMRC-2435

--- spec/lib/sequel/plugins/oplog_spec.rb
+++ spec/lib/sequel/plugins/oplog_spec.rb
@@ -387,14 +387,9 @@ RSpec.describe Sequel::Plugins::Oplog do
 
   describe '#write_oplog_operation!' do
     it 'appends create, update, and destroy rows without requiring a projection hit', :aggregate_failures do
-      create_model = TestRecord.new(id: 7, name: 'Seven', description: 'created', operation_date: Time.zone.today)
-      create_model.write_oplog_operation!(:create)
-
-      update_model = TestRecord.new(id: 7, name: 'Seven updated', description: 'updated', operation_date: Time.zone.today)
-      update_model.write_oplog_operation!(:update)
-
-      destroy_model = TestRecord.new(id: 7, name: 'Seven updated', description: 'updated', operation_date: Time.zone.today)
-      destroy_model.write_oplog_operation!(:destroy)
+      TestRecord.new(id: 7, name: 'Seven', description: 'created').write_oplog_operation!(:create)
+      TestRecord.new(id: 7, name: 'Seven updated', description: 'updated').write_oplog_operation!(:update)
+      TestRecord.new(id: 7, name: 'Seven updated', description: 'updated').write_oplog_operation!(:destroy)
 
       expect(TestRecord::Operation.where(id: 7).order(:oid).select_map(:operation)).to eq(%w[C U D])
       expect(TestRecord::Operation.where(id: 7, operation: 'U').first.name).to eq('Seven updated')
