a86180f517d51872d58c919cbe2336095eadb882
Neil Middleton <neil.middleton@transformuk.com>
2026-07-16 07:43:27 +0100

    Fix quota_order_numbers spec: create quota_definition so with_quota_definitions join returns rows

--- spec/requests/api/v3/quota_order_numbers_spec.rb
+++ spec/requests/api/v3/quota_order_numbers_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe 'GET /uk/api/v3/quota_order_numbers', type: :request do
   let(:headers) { { 'Accept' => 'application/vnd.hmrc.3.0+json' } }
 
   context 'with quota order numbers in the database' do
-    before { create(:quota_order_number) }
+    before do
+      quota_order_number = create(:quota_order_number)
+      create(:quota_definition, :actual,
+             quota_order_number_sid: quota_order_number.quota_order_number_sid)
+    end
 
     it 'returns 200 with flat collection' do
       get '/uk/api/v3/quota_order_numbers', headers: headers
