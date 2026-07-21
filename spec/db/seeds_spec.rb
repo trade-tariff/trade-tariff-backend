402037d3fa41ded7d7396d080d89ecbea2722846
William Fish <william.michael.fish@gmail.com>
2026-07-21 16:17:34 +0100

    chore(rubocop): defer seeds suppression cleanup
    Issue: No ticket/issue

--- spec/db/seeds_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'db/seeds.rb' do
-  it 'refreshes the materialized views' do
-    allow(MaterializeViewHelper).to receive(:refresh_materialized_view)
-
-    load Rails.root.join('db/seeds.rb')
-
-    expect(MaterializeViewHelper).to have_received(:refresh_materialized_view)
-  end
-end
