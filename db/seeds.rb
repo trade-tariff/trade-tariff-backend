e7c78a901ebe163da29306a98722b8c4c407783f
William Fish <william.michael.fish@gmail.com>
2026-07-21 16:17:34 +0100

    chore(rubocop): defer seeds suppression cleanup
    Issue: No ticket/issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require_relative '../app/helpers/materialize_view_helper'
+# rubocop:disable Style/MixinUsage
+include MaterializeViewHelper
+# rubocop:enable Style/MixinUsage
 
 # Populate materialized views after a db:structure:load. If any view was
 # created WITH NO DATA, refresh! will detect "has not been populated" and
 # fall back to a blocking refresh automatically.
-MaterializeViewHelper.refresh_materialized_view
+refresh_materialized_view
