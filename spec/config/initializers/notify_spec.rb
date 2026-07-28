b337c377ed8470d86559fe178c7a4d732b83e53b
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2026-07-28 15:00:39 +0100

    Fix notify spec with new UUIDs

--- spec/config/initializers/notify_spec.rb
+++ spec/config/initializers/notify_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Notify configuration' do
         104e74e3-8f43-4642-a594-4d4ef931b121
         5db33f13-7235-4ed8-b704-e3fddc01ee09
         61e19d5e-4fae-4b7e-aa2e-cd05a87f4cf8
-        PENDING_APPENDIX5A_PRODUCTION_TEMPLATE_UUID
+        c35e387b-a2b8-4308-997c-06e1f3b36900
       ])
     end
   end
@@ -44,7 +44,7 @@ RSpec.describe 'Notify configuration' do
         6033e45a-7029-4c5a-b4d3-e52ba111c9b4
         53c88c0c-69be-4375-829f-c6fbb1b9e2ef
         ed4f4168-e8c5-4b80-94b9-050c86a40f0f
-        PENDING_APPENDIX5A_STAGING_TEMPLATE_UUID
+        943fd08f-d9f0-47e0-b9dd-40284f308414
       ])
     end
   end
