95df86934cbc7875238776d00441c857e9fc54dd
punitcse <jainpunit786@gmail.com>
2026-08-27 17:39:02 +0100

    add template id for notify

--- config/initializers/notify.rb
+++ config/initializers/notify.rb
@@ -13,6 +13,9 @@ NOTIFY_CONFIGURATION =
           appendix5a: 'c35e387b-a2b8-4308-997c-06e1f3b36900',
           customs_tariff_update: '02da9ea8-4919-45af-a9d4-68c908f49c9c',
         },
+        search_references: {
+          invalidation_alert: 'TODO-add-production-notify-template-id',
+        },
       },
       reply_to: {
         tariff_management: '61e19d5e-4fae-4b7e-aa2e-cd05a87f4cf8',
@@ -32,6 +35,9 @@ NOTIFY_CONFIGURATION =
           appendix5a: '943fd08f-d9f0-47e0-b9dd-40284f308414',
           customs_tariff_update: 'eae01fd6-3e73-4a21-83ed-175eec3701c5',
         },
+        search_references: {
+          invalidation_alert: 'TODO-add-staging-notify-template-id',
+        },
       },
       reply_to: {
         tariff_management: 'ed4f4168-e8c5-4b80-94b9-050c86a40f0f',
@@ -50,6 +56,9 @@ NOTIFY_CONFIGURATION =
         notifications: {
           appendix5a: '7b53d787-2659-4cd2-9e45-afe93ad61eec',
           customs_tariff_update: 'b99d0cef-0dce-414f-b3e2-28cf25075a43',
+        },
+        search_references: {
+          invalidation_alert: 'f7a4a307-938c-4ce8-9ec5-e1e05df5195b',
         },
       },
       reply_to: {
