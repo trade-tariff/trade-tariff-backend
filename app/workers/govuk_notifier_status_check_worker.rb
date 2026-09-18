56af7ec3a25adb328f0edfef89414098255c9ea8
Punit Jain <jainpunit786@gmail.com>
2026-09-18 17:08:02 +0100

    Stop Slack alerting on my_ott GOV.UK Notify failures that are already handled automatically (#3774)

--- app/workers/govuk_notifier_status_check_worker.rb
+++ app/workers/govuk_notifier_status_check_worker.rb
@@ -2,6 +2,7 @@ class GovukNotifierStatusCheckWorker
   include Sidekiq::Worker
 
   CHECK_DELAY = 10.minutes
+  SLACK_FAILURE_STATUSES = [GovukNotifier::TECHNICAL_FAILURE].freeze
 
   # Cap retries: Notify status checks must not use Sidekiq's default (25).
   sidekiq_options queue: :default, retry: 3
@@ -18,6 +19,7 @@ class GovukNotifierStatusCheckWorker
       notification_id,
       pipeline: 'my_ott',
       identifier: user_id,
+      slack_failure_statuses: SLACK_FAILURE_STATUSES,
     ).call
 
     user.invalidate! if status == GovukNotifier::PERMANENT_FAILURE
